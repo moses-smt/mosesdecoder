f773298d9567a476a84680435bbdf8ede493209d
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2006-07-14 21:55:22 +0000

    fixed  trans Opt scoring
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@124 1f5c12ca-751b-0410-a591-d2e778427230

--- trunk/moses/src/Manager.h
+++ trunk/moses/src/Manager.h
@@ -33,7 +33,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include "LatticePathList.h"
 #include "SquareMatrix.h"
 #include "WordsBitmap.h"
-#include "UnknownWordHandler.h"
+//#include "UnknownWordHandler.h"
 
 class LatticePath;
 
