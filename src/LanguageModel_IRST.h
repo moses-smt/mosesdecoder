6ac5a32b1322bc10855e7fd33dc9faadff007cc8
redpony <redpony@1f5c12ca-751b-0410-a591-d2e778427230>
2006-07-17 12:57:53 +0000

    add language model classes
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk/moses@143 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ src/LanguageModel_IRST.h
@@ -0,0 +1,76 @@
+// $Id$
+
+/***********************************************************************
+Moses - factored phrase-based language decoder
+Copyright (C) 2006 University of Edinburgh
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+***********************************************************************/
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include "NGramNode.h"
+#include "Factor.h"
+#include "TypeDef.h"
+#include "Util.h"
+
+class FactorCollection;
+class Factor;
+class Phrase;
+
+class lmtable;  // irst lm table
+
+class LanguageModel_IRST : public LanguageModel
+{
+public:
+	
+	LanguageModel_IRST();
+	void Load(size_t id
+					, const std::string &fileName
+					, FactorCollection &factorCollection
+					, FactorType factorType
+					, float weight
+					, size_t nGramOrder);
+	
+protected:
+	lmtable* m_lmtb;
+
+	float GetValue(LmId wordId, VocabIndex *context) const
+	{
+		LanguageModel *lm = const_cast<LanguageModel*>(this);	// hack. not sure if supposed to cast this
+		float p = lm->m_srilmModel.wordProb( wordId.sri, context );
+		return FloorSRIScore(TransformSRIScore(p));	 // log10->log
+	}
+
+	LmId GetLmID( const Factor *factor )  const
+	{
+		return GetLmID(factor->GetString());
+	}
+	void CreateFactors(FactorCollection &factorCollection);
+public:
+	static const LmId UNKNOWN_LM_ID;
+	
+	LmId GetLmID( const std::string &str ) const
+	{
+		LanguageModel *lm = const_cast<LanguageModel*>(this);	// hack. not sure if supposed to cast this
+		LmId res;
+    res.sri = lm->m_srilmVocab.getIndex( str.c_str(), lm->m_srilmVocab.unkIndex() );
+    return res;
+	}
+	
+};
+
