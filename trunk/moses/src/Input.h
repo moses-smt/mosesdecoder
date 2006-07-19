b4ca1e7f4082f91b788e8a3a2db2c9120fecd185
zens <zens@1f5c12ca-751b-0410-a591-d2e778427230>
2006-07-19 00:24:55 +0000

    *** empty log message ***
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@185 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ trunk/moses/src/Input.h
@@ -0,0 +1,37 @@
+// $Id$
+#ifndef INPUT_H_
+#define INPUT_H_
+#include <string>
+#include "TypeDef.h"
+#include "Phrase.h"
+class WordsRange;
+class Factor;
+
+// base class for sentences and confusion networks
+class InputType 
+{
+protected:
+	long m_translationId;
+ public:
+
+	InputType(long translationId=0);
+	//Input();
+	virtual ~InputType();
+
+	// for db stuff
+	long GetTranslationId()
+	{
+		return m_translationId;
+	}
+	void SetTranslationId(long translationId)
+	{	// for db stuff;
+		m_translationId = translationId;
+	}
+	virtual size_t GetSize() const=0;
+
+
+	// these functions are not (yet) well-defined for confusion networks
+	virtual Phrase GetSubString(const WordsRange&) const =0;
+	virtual const Factor* GetFactor(size_t pos, FactorType factorType) const=0;
+};
+#endif
