b2c9294c5300083cac79f9dd3e464b2b61548be9
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2006-07-19 17:20:39 +0000

    no message
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@192 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ branches/HIEUS_BRANCH/moses/src/LMList.cpp
@@ -0,0 +1,29 @@
+
+#include "LMList.h"
+#include "Phrase.h"
+#include "ScoreColl.h"
+
+using namespace std;
+
+void LMList::SetScore(const Phrase &phrase, float &retFullScore, float &retNGramScore, ScoreColl &ngramComponent) const
+{ 
+	const_iterator lmIter;
+	for (lmIter = begin(); lmIter != end(); ++lmIter)
+	{
+		const LanguageModel &lm = **lmIter;
+		const float weightLM = lm.GetWeight();
+
+		float fullScore, nGramScore;
+
+		#ifdef N_BEST
+				lm.CalcScore(phrase, fullScore, nGramScore, &ngramComponent);
+		#else
+		    // this is really, really ugly (a reference to an object at NULL
+		    // is asking for trouble). TODO
+				lm.CalcScore(*this, fullScore, nGramScore, NULL);
+		#endif
+
+		retFullScore   += fullScore * weightLM;
+		retNGramScore	+= nGramScore * weightLM;
+	}	
+}
