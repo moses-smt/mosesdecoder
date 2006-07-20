99b4a57210767086d605a50b8a0456a934d567bc
redpony <redpony@1f5c12ca-751b-0410-a591-d2e778427230>
2006-07-20 18:09:35 +0000

    move some headers around, preparation for enabling bzip2 compression (MUCH higher compression than gzip)
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk/moses@223 1f5c12ca-751b-0410-a591-d2e778427230

--- src/InputFileStream.cpp
+++ src/InputFileStream.cpp
@@ -19,4 +19,41 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ***********************************************************************/
 
+#include <boost/iostreams/filter/gzip.hpp>
+
+#if HAVE_BZIP2
+#include <boost/iostreams/filter/bzip2.hpp>
+#endif
+
 #include "InputFileStream.h"
+
+InputFileStream::InputFileStream(const std::string &filePath)
+{
+  using namespace boost::iostreams;
+
+  if (filePath.size() > 3 &&
+      filePath.substr(filePath.size() - 3, 3) == ".gz")
+  {
+    m_file.open(filePath.c_str(), ios_base::in | ios_base::binary);
+    push(gzip_decompressor());
+  }
+#if HAVE_BZIP2
+  else if (filePath.size() > 4 &&
+      filePath.substr(filePath.size() - 4, 4) == ".bz2")
+  {
+    m_file.open(filePath.c_str(), ios_base::in | ios_base::binary);
+    push(bzip2_decompressor());
+  }
+#endif
+  else
+  {
+    m_file.open(filePath.c_str());
+  }
+  push(m_file);
+}
+
+void InputFileStream::Close()
+{
+  pop();
+}
+
