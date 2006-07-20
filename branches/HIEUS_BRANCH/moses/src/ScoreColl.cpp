dd5faef56d9276dfbae6920ac9d14d92a8808c19
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2006-07-20 19:58:37 +0000

    no message
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@227 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ branches/HIEUS_BRANCH/moses/src/ScoreColl.cpp
@@ -0,0 +1,16 @@
+
+#include "ScoreColl.h"
+
+void ScoreColl::Combine(const ScoreColl &other)
+{
+	const_iterator iter;
+	for (iter = other.begin() ; iter != other.end() ; iter++)
+	{
+		float value = iter->second;
+		iterator iterThis = find(iter->first);
+		assert (iterThis != end());
+		
+		// score component for dictionary exists. add numbers
+		iterThis->second = value;
+	}
+}
