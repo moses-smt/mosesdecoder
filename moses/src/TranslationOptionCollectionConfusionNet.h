2a4cb09a6c194c3ce04ce657206e0e4deb93de86
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2006-07-28 03:20:09 +0000

    can set max trans option per coverage turned off profiling
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@350 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/TranslationOptionCollectionConfusionNet.h
+++ moses/src/TranslationOptionCollectionConfusionNet.h
@@ -7,7 +7,7 @@ class ConfusionNet;
 
 class TranslationOptionCollectionConfusionNet : public TranslationOptionCollection {
  public:
-	TranslationOptionCollectionConfusionNet(const ConfusionNet &source);
+	TranslationOptionCollectionConfusionNet(const ConfusionNet &source, size_t maxNoTransOptPerCoverage);
 
 	void ProcessUnknownWord(		size_t sourcePos
 															, int dropUnknown
