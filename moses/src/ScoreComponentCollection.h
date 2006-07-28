146aaca89d1b9fc88b683b76f10d9a1d9a12f102
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2006-07-28 15:01:34 +0000

    Visual Studio issues
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@355 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/ScoreComponentCollection.h
+++ moses/src/ScoreComponentCollection.h
@@ -22,6 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #pragma once
 
 #include <numeric>
+#include <assert.h>
 #include "ScoreProducer.h"
 #include "ScoreIndexManager.h"
 
