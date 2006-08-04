b947327d8d983f88eb61567a27664de782cf58e0
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2006-08-04 20:45:19 +0000

    added chunking language model
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk/moses@498 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ src/LanguageModel_Chunking.cpp
@@ -0,0 +1,73 @@
+
+// $Id$
+
+/***********************************************************************
+Moses - factored phrase-based language decoder
+Copyright (C) 2006 University of Edinburgh
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+***********************************************************************/
+
+#include "LanguageModel_Chunking.h"
+#include "Phrase.h"
+
+LanguageModel_Chunking::LanguageModel_Chunking()
+{
+}
+LanguageModel_Chunking::~LanguageModel_Chunking()
+{
+}
+
+void LanguageModel_Chunking::Load(const std::string &fileName
+					, FactorCollection &factorCollection
+					, FactorType factorType
+					, float weight
+					, size_t nGramOrder
+					, size_t maxNGramOrder)
+{
+	MyBase::Load(fileName, factorCollection, factorType, weight, nGramOrder);
+	m_maxNGramOrder = maxNGramOrder;
+}
+
+void LanguageModel_Chunking::CalcScore(const Phrase &phrase
+							, float &fullScore
+							, float &ngramScore) const
+{
+	size_t currOrder = 0;
+	vector<const Factor*> factors;
+	for (int currPos = (int)phrase.GetSize() - 1 ; currPos >= 0 ; currPos++ )
+	{
+		const Factor *factor = phrase.GetFactor(currPos, m_factorType);
+		if (factor->GetString().substr(0, 2) == "I-")
+		{
+			factors.push_back(factor);
+			if (++currOrder >= m_nGramOrder)
+				break;
+		}
+	}
+
+	// create a phrase with only those factors
+	Phrase phraseCalc(phrase.GetDirection());
+	for (int currPos = (int)factors.size() - 1 ; currPos >= 0 ; currPos++ )
+	{
+		FactorArray &factorArray = phraseCalc.AddWord();
+		factorArray[m_factorType] = factors[currPos];
+	}
+
+	// calc score on that phrase
+	MyBase::CalcScore(phraseCalc, fullScore, ngramScore);
+}
+
+
---
+++ src/LanguageModel_Chunking.h
@@ -0,0 +1,45 @@
+// $Id$
+
+/***********************************************************************
+Moses - factored phrase-based language decoder
+Copyright (C) 2006 University of Edinburgh
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+***********************************************************************/
+
+#include "LanguageModel_SRI.h"
+
+class LanguageModel_Chunking : public LanguageModel_SRI
+{
+	typedef LanguageModel_SRI MyBase;
+	
+protected:
+	size_t m_maxNGramOrder;
+public:
+	LanguageModel_Chunking();
+	~LanguageModel_Chunking();
+	void Load(const std::string &fileName
+					, FactorCollection &factorCollection
+					, FactorType factorType
+					, float weight
+					, size_t nGramOrder
+					, size_t maxNGramOrder);
+	void CalcScore(const Phrase &phrase
+							, float &fullScore
+							, float &ngramScore) const;
+	
+};
+
+
