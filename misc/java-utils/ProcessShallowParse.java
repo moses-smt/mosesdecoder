f0dc2ffcf72f060ff6f4a3a8d689469da736b98f
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2006-08-08 00:19:40 +0000

    create data for chunking LM
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@553 1f5c12ca-751b-0410-a591-d2e778427230

--- misc/java-utils/ProcessShallowParse.java
+++ misc/java-utils/ProcessShallowParse.java
@@ -13,7 +13,7 @@ public class ProcessShallowParse
 		InputStreamReader inStream = args.length > 0 ? new FileReader(args[0]) : new InputStreamReader(System.in); 
 		OutputStreamWriter outStream = args.length > 1 ?  new FileWriter(args[1]) : new OutputStreamWriter(System.out); 
 		
-		new ProcessShallowParse(inStream, outStream);
+		new ProcessShallowParse2(inStream, outStream);
 		
 		System.err.println("End...");
 	}
@@ -39,3 +39,44 @@ public class ProcessShallowParse
 	}
 }
 
+class ProcessShallowParse2
+{ // factored sentence
+	public ProcessShallowParse2(Reader inStream, Writer outStream) throws Exception
+	{		
+		BufferedReader inFile = new BufferedReader(inStream); 
+		BufferedWriter outFile = new BufferedWriter(outStream); 
+		
+		// tokenise
+		String inLine;
+		while ((inLine = inFile.readLine()) != null)
+		{
+			StringTokenizer st = new StringTokenizer(inLine);
+		     while (st.hasMoreTokens()) 
+		     {
+		    	 String factoredWord = st.nextToken();
+		    	 Output(factoredWord, outFile);
+		     }
+		     outFile.write("\n");
+		}		
+	}
+	
+	protected void Output(String factoredWord, BufferedWriter outStream) throws Exception
+	{
+		StringTokenizer st = new StringTokenizer(factoredWord, "|");
+    	st.nextToken();
+    	String pos = st.nextToken();
+    	String morph = st.nextToken();
+
+    	int lastPos = pos.lastIndexOf('-');
+    	
+    	if (pos.indexOf("ART") == 0
+    		|| pos.indexOf("P") == 0
+    		|| pos.indexOf("V") == 0
+    		|| pos.indexOf("$,") == 0
+    		|| pos.indexOf("$.") == 0
+    		)
+    	{
+    		outStream.write(pos + "|" + morph + " ");
+    	}
+	}
+}
