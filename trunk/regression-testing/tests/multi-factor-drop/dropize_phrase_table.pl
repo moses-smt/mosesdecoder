c4839e441fb95c920d425520497bb04cdfb7194d
redpony <redpony@1f5c12ca-751b-0410-a591-d2e778427230>
2006-08-08 15:54:30 +0000

    check in regression tests to CVS
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@557 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ trunk/regression-testing/tests/multi-factor-drop/dropize_phrase_table.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+#add_empties_to_phrase_table: go through an old-style pharaoh phrase table (no empty target sources) and add one such line for each single-word source phrase in the table,
+#complete with factors (note the number and type of factors are hardcoded here);
+#also add deletion-cost factors as necessary to all lines
+
+#usage: aetpt INPUT_PTABLE OUTPUT_PTABLE
+
+my ($inputFile, $outputFile) = @ARGV;
+my $DELETIONCOST = 2.718; #weight for an individual deletion
+
+open(INFILE, "<$inputFile") or die "couldn't open '$inputFile' for read\n";
+open(OUTFILE, ">$outputFile") or die "couldn't open '$outputFile' for write\n";
+my ($lastSrcPhrase, $srcPhrase);
+while(my $line = <INFILE>)
+{
+	chop($line);
+	$lastSrcPhrase = $srcPhrase;
+	my @tokens = split(/\|\|\|/, $line);
+	$srcPhrase = $tokens[0];
+	if($srcPhrase ne $lastSrcPhrase && $srcPhrase =~ /^\s*\S+\s*$/) #new source phrase of a single word; add deletion line
+	{
+		print OUTFILE "$srcPhrase |||  ||| 1 1 1 1 2.718 $DELETIONCOST\n";
+	}
+	print OUTFILE "$line 1\n";
+}
+close(INFILE);
+close(OUTFILE);
