400dac79d9ef45c157018613b6466402a547267d
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2006-08-10 22:51:54 +0000

    forgot to wrap header file
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@632 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/LanguageModelMultiFactor.h
+++ moses/src/LanguageModelMultiFactor.h
@@ -19,6 +19,8 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ***********************************************************************/
 
+#pragma once
+
 #include <vector>
 #include <string>
 #include "LanguageModel.h"
