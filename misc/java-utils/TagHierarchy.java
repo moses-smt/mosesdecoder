36f9cd2fb9924b0f9848d446df1516d417b056a5
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2006-08-11 02:10:51 +0000

    change chunking extraction according to advice from phi
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@637 1f5c12ca-751b-0410-a591-d2e778427230

--- misc/java-utils/TagHierarchy.java
+++ misc/java-utils/TagHierarchy.java
@@ -27,10 +27,10 @@ class TagHierarchy
 		while ((inLine = inFile.readLine()) != null)
 		{
 			if (inLine.compareTo("null") != 0)
-				OutputHierarchy(inLine, outFile);
+				OutputHierarchy2(inLine, outFile);
 		}
 	}
-	
+
 	public void OutputHierarchy(String inLine, BufferedWriter outFile) throws Exception
 	{
 		int level = 0;
@@ -60,16 +60,52 @@ class TagHierarchy
 	     }
 	     outFile.write('\n');
 	}
-}
 
-class Node
-{
-	public String tag, word;
-	
-	public Node(String tag, String word)
+	public void OutputHierarchy2(String inLine, BufferedWriter outFile) throws Exception
 	{
-		this.tag = tag;
-		this.word = word;
+		int level = 0;
+		Stack prevTags = new Stack();
+		
+		StringTokenizer st = new StringTokenizer(inLine);
+		
+	    while (st.hasMoreTokens()) 
+	    {
+	    	String parsed = st.nextToken();
+	    	if (parsed.substring(0, 1).compareTo("(") == 0)
+	    	{ // start of new node
+	    		outFile.write('\n');
+	    		for (int currLevel = 0 ; currLevel < level ; currLevel++)
+	    		{
+	    			outFile.write(' ');
+	    		}
+	    		String tag = parsed.substring(1, parsed.length());
+	    		outFile.write(tag);
+	    		prevTags.push(tag);
+	    		level++;
+	    	}
+	    	else
+	    	{ // closing nodes
+	    		
+	    		String parentTag = (String) prevTags.get(prevTags.size() - 2)
+	    				, currTag = (String) prevTags.get(prevTags.size() - 1);
+	    		if (currTag.equals("NN-NK") && parentTag.equals("NP-SB"))
+	    			outFile.write("_" + parentTag);
+
+	    		int firstBracket = parsed.indexOf(')');
+	    		int noBracket = parsed.length() - firstBracket;
+	    		String word = parsed.substring(0, firstBracket);
+	    		outFile.write(" == " + word);
+	    		
+	    		level -= noBracket;
+	    		
+	    		
+	    		// pop the rest
+	    		for (int i = 0 ; i < noBracket ; ++i)
+	    		{
+	    			prevTags.pop();
+	    		}
+	    	}
+	    }
+	    outFile.write('\n');
 	}
 }
-
