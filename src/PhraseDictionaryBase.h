3ee731073983efab2a03e88eded9d7fd0a024b1e
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2006-08-14 15:53:49 +0000

    implemented new phrase dictionary pruning
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk/moses@722 1f5c12ca-751b-0410-a591-d2e778427230

--- src/PhraseDictionaryBase.h
+++ src/PhraseDictionaryBase.h
@@ -41,7 +41,6 @@ class PhraseDictionaryBase : public Dictionary, public ScoreProducer
 	size_t m_tableLimit;
 	std::string m_filename;    // just for debugging purposes
 
-	virtual void SortTargetPhraseCollection() = 0;
  public:
 	PhraseDictionaryBase(size_t noScoreComponent);
 	virtual ~PhraseDictionaryBase();
