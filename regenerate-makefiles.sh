b57e2d38e8fbf3e4f0b2dd29dc463bd28e890bac
redpony <redpony@1f5c12ca-751b-0410-a591-d2e778427230>
2006-08-20 17:27:17 +0000

    add suggestion to use --enable-maintainer-mode
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk/moses@783 1f5c12ca-751b-0410-a591-d2e778427230

--- regenerate-makefiles.sh
+++ regenerate-makefiles.sh
@@ -9,6 +9,6 @@ automake
 
 echo
 echo "You should now be able to configure and build:"
-echo "   ./configure --with-srilm=/path/to/srilm"
+echo "   ./configure --enable-maintainer-mode --with-srilm=/path/to/srilm"
 echo "   make -j 4"
 echo
