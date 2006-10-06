cef863c4a7db97bd7b71ffe8977b2c6b970bbfae
bojar <bojar@1f5c12ca-751b-0410-a591-d2e778427230>
2006-10-06 18:38:21 +0000

    keep a copy of the scripts, so that we don't miss them in previous revisions
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@869 1f5c12ca-751b-0410-a591-d2e778427230

--- validate_more_revisions.sh
+++ validate_more_revisions.sh
@@ -17,9 +17,24 @@ fi
 
 mkdir -p $logdir
 
+tmpdir=/tmp/validate-more-revisions-tmp
+if [ -e $tmpdir ]; then
+  echo "$0 seems to be already running!"
+  echo "If this is a false alarm, remove our temp directory:"
+  echo "   rm -rf $tmpdir"
+  exit 1;
+fi
+
+mkdir -p $tmpdir
+
+# need to save a copy of the helper script validate_revision.sh, 
+# because previous releases might have missed it
+cp ./validate_revision.sh $tmpdir/
+
 for i in `seq $from $to`; do
   echo "Validating $i...";
-  ./validate_revision.sh $i > $logdir/$i.log 2>&1
+  $tmpdir/validate_revision.sh $i > $logdir/$i.log 2>&1
   tail -1 $logdir/$i.log | sed 's/^/   /'
 done
-echo "Finished validating"
+rm -rf $tmpdir
+echo "Finished validating, now at revision $to"
