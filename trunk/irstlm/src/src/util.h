6e9e38b289772828a4f503f0bcb10c0b11ad770e
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2006-11-05 13:02:33 +0000

    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@950 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ trunk/irstlm/src/src/util.h
@@ -0,0 +1,25 @@
+#ifndef IRSTLM_UTIL_H
+#define IRSTLM_UTIL_H
+
+#include <string>
+#include <fstream>
+#include "gzfilebuf.h"
+
+std::string gettempfolder();
+void createtempfile(std::ofstream  &fileStream, std::string &filePath, std::ios_base::openmode flags);
+void removefile(const std::string &filePath);
+
+class inputfilestream : public std::istream
+{
+protected:
+	std::streambuf *m_streambuf;
+public:
+  
+	inputfilestream(const std::string &filePath);
+	~inputfilestream();
+  
+	void close();
+};
+
+#endif
+
