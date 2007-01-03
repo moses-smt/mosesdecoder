b7f5cc2f96d6044e7e8fda884e1e4b8d259f920a
mfederico <mfederico@1f5c12ca-751b-0410-a591-d2e778427230>
2007-01-03 23:25:53 +0000

    a much more efficient version of quantize-lm
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@1109 1f5c12ca-751b-0410-a591-d2e778427230

--- trunk/irstlm/src/util.h
+++ trunk/irstlm/src/util.h
@@ -13,13 +13,15 @@ class inputfilestream : public std::istream
 {
 protected:
 	std::streambuf *m_streambuf;
+  bool _good;
 public:
-  
-	inputfilestream(const std::string &filePath);
+    
+    inputfilestream(const std::string &filePath);
 	~inputfilestream();
-  
+  bool good(){return _good;}
 	void close();
 };
+
 
 void *MMap(int	fd, int	access, off_t	offset, size_t	len, off_t	*gap);
 int Munmap(void	*p,size_t	len,int	sync);
