6f6597d9ac104ded002ab6ee185449e0870522bf
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2007-02-21 20:05:27 +0000

    minor gcc compile error. also, no longer use IRSTLM as a subsitutute for SRILM, and vice versa. They don't give identitcal results - avoids confusion.
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@1229 1f5c12ca-751b-0410-a591-d2e778427230

--- trunk/moses/src/LanguageModelSRI.cpp
+++ trunk/moses/src/LanguageModelSRI.cpp
@@ -33,8 +33,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include "Phrase.h"
 using namespace std;
 
-LanguageModelSRI::LanguageModelSRI(bool registerScore)
-:LanguageModelSingleFactor(registerScore)
+LanguageModelSRI::LanguageModelSRI(bool registerScore, ScoreIndexManager &scoreIndexManager)
+:LanguageModelSingleFactor(registerScore, scoreIndexManager)
 , m_srilmVocab(0)
 , m_srilmModel(0)
 {
