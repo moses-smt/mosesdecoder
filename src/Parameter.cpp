8fbe9e263e3a41b34eeffc13724c3a05ed9946eb
abarun <abarun@1f5c12ca-751b-0410-a591-d2e778427230>
2007-05-15 21:34:21 +0000

    Code for MBR decoding
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk/moses@1384 1f5c12ca-751b-0410-a591-d2e778427230

--- src/Parameter.cpp
+++ src/Parameter.cpp
@@ -73,6 +73,8 @@ Parameter::Parameter()
 	AddParam("distortion-limit", "dl", "distortion (reordering) limit in maximum number of words");	
 	AddParam("distortion-file", "source factors (0 if table independent of source), target factors, location of the factorized/lexicalized reordering tables");
  	AddParam("distortion", "configurations for each factorized/lexicalized reordering model.");
+ 	AddParam("mbr-scale", "scaling factor to convert log linear score into a probability.");
+ 	AddParam("decoder-type", "MAP/MBR decoder (default=MAP=0)");
 }
 
 Parameter::~Parameter()
