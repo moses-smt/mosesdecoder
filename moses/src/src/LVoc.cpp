24922dc741c7d1d0802e5234ca7475e46ee0e011
evahasler <evahasler@1f5c12ca-751b-0410-a591-d2e778427230>
2007-12-10 19:58:42 +0000

    delete src
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/eva_cube_pruning@1525 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/src/LVoc.cpp
+++
@@ -1,7 +0,0 @@
-#include<limits>
-#include "LVoc.h"
-
-//rather pointless file because LVoc is template all wee need here is the definitions of consts
-
-const LabelId InvalidLabelId = std::numeric_limits<LabelId>::max();
-const LabelId Epsilon        = InvalidLabelId-1;
