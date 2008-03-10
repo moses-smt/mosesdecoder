a62611c4c5ea3dd70186e91bfeb6851039341cfe
redpony <redpony@1f5c12ca-751b-0410-a591-d2e778427230>
2008-03-10 02:20:14 +0000

    fix bug
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@1587 1f5c12ca-751b-0410-a591-d2e778427230

--- trunk/moses/src/StaticData.cpp
+++ trunk/moses/src/StaticData.cpp
@@ -73,7 +73,7 @@ StaticData::StaticData()
 ,m_onlyDistinctNBest(false)
 ,m_computeLMBackoffStats(false)
 ,m_factorDelimiter("|") // default delimiter between factors
-,m_isAlwaysCreateDirectTranslationOption(true)
+,m_isAlwaysCreateDirectTranslationOption(false)
 {
   m_maxFactorIdx[0] = 0;  // source side
   m_maxFactorIdx[1] = 0;  // target side
