97c2a8fdf48637fffcbca00a504c2d8ae147353f
jdschroeder <jdschroeder@1f5c12ca-751b-0410-a591-d2e778427230>
2008-05-16 14:46:08 +0000

    PBCandidateGenerator first try
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@1741 1f5c12ca-751b-0410-a591-d2e778427230

--- hypergraph-prototype/attempt1/org/statmt/hg4/Decoder.java
+++ hypergraph-prototype/attempt1/org/statmt/hg4/Decoder.java
@@ -50,28 +50,6 @@ public class Decoder {
 		public GNode getRoot() { return root; } 
 	}
 	
-	static class PBCandidateGenerator extends PassiveCandidateGenerator {
-		ArrayList<ArrayList<Vertex>> lattice;
-		public PBCandidateGenerator(ArrayList<ArrayList<ArrayList<Phrase>>> l, Hypergraph derivation) {
-			assert(derivation.sourceVertices.size() == 1);
-			Vertex v = derivation.sourceVertices.iterator().next();
-			lattice = new ArrayList<ArrayList<ArrayList<Vertex>>>();
-			for (ArrayList<ArrayList<Phrase>> cols : l) {
-				lattice.add(new ArrayList<ArrayList<Vertex>>()));
-			}
-		}
-		public Collection<Vertex> retrieveCominableNodesForActiveVertex(Vertex activeVertex,
-				Map<String, Object> state, Phrase sentence) {
-			int c = (Integer)state.get("COVERAGE");
-			ArrayList<Vertex> transOpts = new ArrayList<Vertex>();
-			for (int e = c; e < sentence.size(); e++) {
-				lattice.get(c);
-				ArrayList<Vertex> opts = lattice.get(c).get(e-c);
-				transOpts.addAll(opts);
-			}
-			return transOpts;
-		}
-	}
 
 	public static void main(String[] args) {
 		SGrammar g = new SGrammar();
