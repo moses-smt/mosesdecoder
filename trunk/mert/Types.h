da83b439a8fcffb552c025fb261e7011d94c3f3a
jfouet <jfouet@1f5c12ca-751b-0410-a591-d2e778427230>
2008-05-20 12:57:32 +0000

    add type threshold
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@1785 1f5c12ca-751b-0410-a591-d2e778427230

--- trunk/mert/Types.h
+++ trunk/mert/Types.h
@@ -1,12 +1,13 @@
 #ifndef TYPE_H
 #define TYPE_H
 #include<vector>
+#include <map>
 
 using namespace std;
 typedef float parameter_t;
 //typedef vector<parameter_t> parameters_t;confusing; use vector<parameter_t>
-
 typedef vector<pair<unsigned int, unsigned int> > diff_t;
+typedef pair<float,diff_t > threshold;
 typedef vector<diff_t> diffs_t;
 typedef vector<unsigned int> candidates_t;
 typedef float statscore_t;
