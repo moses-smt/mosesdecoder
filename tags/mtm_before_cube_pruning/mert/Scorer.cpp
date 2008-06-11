d38854c8ed36861e6c525966e2d246b5747b016a
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2008-06-11 10:45:57 +0000

    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@1846 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ tags/mtm_before_cube_pruning/mert/Scorer.cpp
@@ -0,0 +1,50 @@
+#include "Scorer.h"
+
+void  StatisticsBasedScorer::score(const candidates_t& candidates, const diffs_t& diffs,
+            statscores_t& scores) {
+	if (!_scoreData) {
+		throw runtime_error("Score data not loaded");
+	}
+    //calculate the score for the candidates
+    if (_scoreData->size() == 0) {
+        throw runtime_error("Score data is empty");
+    }
+    if (candidates.size() == 0) {
+        throw runtime_error("No candidates supplied");
+    }
+    int numCounts = _scoreData->get(0,candidates[0]).size();
+	vector<int> totals(numCounts);
+	for (size_t i = 0; i < candidates.size(); ++i) {
+		ScoreStats stats = _scoreData->get(i,candidates[i]);
+		if (stats.size() != totals.size()) {
+			stringstream msg;
+			msg << "Statistics for (" << "," << candidates[i] << ") have incorrect "
+				<< "number of fields. Found: " << stats.size() << " Expected: " 
+				<< totals.size();
+			throw runtime_error(msg.str());
+		}
+		for (size_t k = 0; k < totals.size(); ++k) {
+			totals[k] += stats.get(k);	
+		}
+	}
+    scores.push_back(calculateScore(totals));
+
+    candidates_t last_candidates(candidates);
+    //apply each of the diffs, and get new scores
+    for (size_t i = 0; i < diffs.size(); ++i) {
+        for (size_t j = 0; j < diffs[i].size(); ++j) {
+            size_t sid = diffs[i][j].first;
+            size_t nid = diffs[i][j].second;
+            size_t last_nid = last_candidates[sid];
+            for (size_t k  = 0; k < totals.size(); ++k) {
+                int diff = _scoreData->get(sid,nid).get(k)
+                    - _scoreData->get(sid,last_nid).get(k);
+                totals[k] += diff;
+            }
+            last_candidates[sid] = nid;
+        }
+        scores.push_back(calculateScore(totals));
+    }
+
+}
+
---
+++ tags/mtm_before_cube_pruning/mert/Scorer.h
@@ -0,0 +1,167 @@
+#ifndef __SCORER_H__
+#define __SCORER_H__
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include "Types.h"
+#include "ScoreData.h"
+
+using namespace std;
+
+class ScoreStats;
+
+/**
+  * Superclass of all scorers and dummy implementation. In order to add a new
+  * scorer it should be sufficient to override prepareStats(), setReferenceFiles()
+  * and score() (or calculateScore()). 
+**/
+class Scorer {
+  private:
+	  string _name;
+	
+  public:
+		
+	Scorer(const string& name): _name(name), _scoreData(0),_preserveCase(false){ cerr << "I AM A SCORER" << std::endl;};
+		virtual ~Scorer(){};
+		
+		/**
+			* returns the number of statistics needed for the computation of the score
+			**/
+		virtual size_t NumberOfScores(){ cerr << "Scorer: 0" << endl; return 0; };
+		
+		/**
+		  * set the reference files. This must be called before prepareStats.
+		  **/
+		virtual void setReferenceFiles(const vector<string>& referenceFiles) {
+			//do nothing
+		}
+
+		/**
+		 * Process the given guessed text, corresponding to the given reference sindex
+         * and add the appropriate statistics to the entry.
+		**/
+		virtual void prepareStats(size_t sindex, const string& text, ScoreStats& entry)
+		{}
+
+		virtual void prepareStats(const string& sindex, const string& text, ScoreStats& entry)
+		{
+			
+//			cerr << sindex << endl;
+			this->prepareStats((size_t) atoi(sindex.c_str()), text, entry);
+			//cerr << text << std::endl;
+		}
+				
+        /**
+          * Score using each of the candidate index, then go through the diffs
+          * applying each in turn, and calculating a new score each time.
+          **/
+        virtual void score(const candidates_t& candidates, const diffs_t& diffs,
+                statscores_t& scores) {
+            //dummy impl
+			if (!_scoreData) {
+				throw runtime_error("score data not loaded");
+			}
+            scores.push_back(0);
+            for (size_t i = 0; i < diffs.size(); ++i) {
+                scores.push_back(0);
+            }
+        }
+
+
+		/**
+		  * Calculate the score of the sentences corresponding to the list of candidate
+		  * indices. Each index indicates the 1-best choice from the n-best list.
+		  **/
+		float score(const candidates_t& candidates) {
+            diffs_t diffs;
+            statscores_t scores;
+            score(candidates, diffs, scores);
+            return scores[0];
+		}
+
+		const string& getName() const {return _name;}
+
+        size_t getReferenceSize() {
+            if (_scoreData) {
+                return _scoreData->size();
+            }
+            return 0;
+        }
+		
+
+		/**
+		  * Set the score data, prior to scoring.
+		  **/
+		void setScoreData(ScoreData* data) {
+			_scoreData = data;
+		}
+
+		protected:
+      typedef map<string,int> encodings_t;
+      typedef map<string,int>::iterator encodings_it;
+
+			ScoreData* _scoreData;
+	    encodings_t _encodings;
+
+	    bool _preserveCase;
+
+			
+            /**
+              * Tokenise line and encode.
+              * 	Note: We assume that all tokens are separated by single spaces
+              **/
+            void encode(const string& line, vector<int>& encoded) {
+                //cerr << line << endl;
+                istringstream in (line);
+                string token;
+                while (in >> token) {
+                    if (!_preserveCase) {
+                        for (string::iterator i = token.begin(); i != token.end(); ++i) {
+                            *i = tolower(*i);
+                        }
+                    }
+                    encodings_it encoding = _encodings.find(token);
+                    int encoded_token;
+                    if (encoding == _encodings.end()) {
+                        encoded_token = (int)_encodings.size();
+                        _encodings[token] = encoded_token;
+                        //cerr << encoded_token << "(n) ";
+                    } else {
+                        encoded_token = encoding->second;
+                        //cerr << encoded_token << " ";
+                    }
+                    encoded.push_back(encoded_token);
+                }
+                //cerr << endl;
+            }
+
+
+};
+
+
+/**
+  * Abstract base class for scorers that work by adding statistics across all 
+  * outout sentences, then apply some formula, e.g. bleu, per. **/
+class StatisticsBasedScorer : public Scorer {
+
+    public:
+  StatisticsBasedScorer(const string& name): Scorer(name) {cerr << "I AM A StatisticsBasedScorer" << std::endl;}
+	~StatisticsBasedScorer(){};
+	virtual void score(const candidates_t& candidates, const diffs_t& diffs,
+                statscores_t& scores);
+
+protected:
+        //calculate the actual score
+        virtual statscore_t calculateScore(const vector<int>& totals) = 0;
+
+};
+
+
+#endif //__SCORER_H
