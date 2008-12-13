1c333d18fad127b7cdd626badf8224a046a150ab
phkoehn <phkoehn@1f5c12ca-751b-0410-a591-d2e778427230>
2008-12-13 12:18:45 +0000

    ooops
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@1957 1f5c12ca-751b-0410-a591-d2e778427230

--- trunk/moses/src/SearchNormal.h
+++ trunk/moses/src/SearchNormal.h
@@ -21,6 +21,7 @@ protected:
 		std::vector < HypothesisStack* > m_hypoStackColl; /**< stacks to store hypotheses (partial translations) */ 
 	// no of elements = no of words in source + 1
 	TargetPhrase m_initialTargetPhrase; /**< used to seed 1st hypo */
+	clock_t m_start; /**< starting time, used for logging */
 	size_t interrupted_flag;
 	HypothesisStackNormal* actual_hypoStack; /**actual (full expanded) stack of hypotheses*/ 
 	const TranslationOptionCollection &m_transOptColl; /**< pre-computed list of translation options for the phrases in this sentence */
