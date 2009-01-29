5c18dcb27d73e2731149d35a0b5a8e4ed1cedbe7
redpony <redpony@1f5c12ca-751b-0410-a591-d2e778427230>
2009-01-29 14:38:32 +0000

    max trans decoder
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/josiah@2036 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ moses/src/GibblerMaxTransDecoder.cpp
@@ -0,0 +1,41 @@
+#include "GibblerMaxTransDecoder.h"
+
+#include <sstream>
+#include <map>
+
+using namespace __gnu_cxx;
+using namespace std;
+
+namespace Moses {
+
+GibblerMaxTransDecoder::GibblerMaxTransDecoder() : n(0) {}
+
+void GibblerMaxTransDecoder::collect(Sample& sample) {
+  ++n;
+  const Hypothesis* h = sample.GetSampleHypothesis();
+  vector<const Factor*> trans;
+  h->GetTranslation(&trans, 0);
+  ++samples[trans];
+}
+
+string ToString(const vector<const Factor*>& ws) {
+  ostringstream os;
+  for (vector<const Factor*>::const_iterator i = ws.begin(); i != ws.end(); ++i)
+    os << (*i)->GetString() << " ";
+  return os.str();
+}
+
+vector<const Factor*> GibblerMaxTransDecoder::Max() {
+  hash_map<vector<const Factor*>, int>::const_iterator ci;
+  multimap<float, const vector<const Factor*>*> sorted;
+  const float nf = n;
+  for (ci = samples.begin(); ci != samples.end(); ++ci) {
+    sorted.insert(make_pair<float, const vector<const Factor*>*>(static_cast<float>(ci->second) / nf, &ci->first));
+  }
+  multimap<float, const vector<const Factor*>*>::iterator i;
+  for (i = sorted.begin(); i != sorted.end(); ++i)
+    cout << i->first << "\t" << ToString(*i->second) << endl;
+  return *sorted.begin()->second;
+}
+
+}
