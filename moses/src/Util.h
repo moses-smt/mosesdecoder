0fd815e85e35e32ed270c0d5001c6682a884c25b
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2009-02-06 19:52:05 +0000

    squeeze
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/mt3_chart@2093 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/Util.h
+++ moses/src/Util.h
@@ -149,6 +149,27 @@ inline std::vector<std::string> Tokenize(const std::string& str,
 	return tokens;
 }
 
+// speeded up version of above
+inline void Tokenize(std::vector<std::string> &output
+										, const std::string& str
+										, const std::string& delimiters = " \t")
+{
+	// Skip delimiters at beginning.
+	std::string::size_type lastPos = str.find_first_not_of(delimiters, 0);
+	// Find first "non-delimiter".
+	std::string::size_type pos     = str.find_first_of(delimiters, lastPos);
+
+	while (std::string::npos != pos || std::string::npos != lastPos)
+	{
+		// Found a token, add it to the vector.
+		output.push_back(str.substr(lastPos, pos - lastPos));
+		// Skip delimiters.  Note the "not_of"
+		lastPos = str.find_first_not_of(delimiters, pos);
+		// Find next "non-delimiter"
+		pos = str.find_first_of(delimiters, lastPos);
+	}
+}
+
 //! tokenise input string to vector of type T
 template<typename T>
 inline std::vector<T> Tokenize( const std::string &input
