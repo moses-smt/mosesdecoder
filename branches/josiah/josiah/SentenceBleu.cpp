9c4329e9aaa953d408b342aa0689a97c6c6e1ce6
abarun <abarun@1f5c12ca-751b-0410-a591-d2e778427230>
2009-02-13 20:05:32 +0000

    Implemented MBR decoding
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@2146 1f5c12ca-751b-0410-a591-d2e778427230

--- branches/josiah/josiah/SentenceBleu.cpp
+++ branches/josiah/josiah/SentenceBleu.cpp
@@ -17,12 +17,20 @@ SentenceBLEU::SentenceBLEU(int n, const std::vector<std::string>& refs) :
     vector<const Factor*> fv;
     GainFunction::ConvertStringToFactorArray(*ci, &fv);
     lengths_.push_back(fv.size());
-    CountRef(fv);
+    CountRef(fv, ngrams_);
   }
 }
 
-void SentenceBLEU::CountRef(const vector<const Factor*>& ref) {
+SentenceBLEU::SentenceBLEU(int n, const vector<const Factor*> & ref) :
+  n_(n) {
+  lengths_.push_back(ref.size());
+  CountRef(ref, ngrams_);
+}  
+  
+  
+void SentenceBLEU::CountRef(const vector<const Factor*>& ref, NGramCountMap& ngrams) const{
   NGramCountMap tc;
+
   vector<const Factor*> ngram(n_);
   int s = ref.size();
   for (int j=0; j<s; ++j) {
@@ -37,42 +45,52 @@ void SentenceBLEU::CountRef(const vector<const Factor*>& ref) {
 
   // clipping
   for (NGramCountMap::iterator i = tc.begin(); i != tc.end(); ++i) {
-    pair<int,int>& p = ngrams_[i->first];
+    pair<int,int>& p = ngrams[i->first];
     if (p.first < i->second.first)
       p = i->second;
   }
 }
 
 float SentenceBLEU::ComputeGain(const vector<const Factor*>& sent) const {
-  for (NGramCountMap::iterator i = ngrams_.begin(); i != ngrams_.end(); ++i)
-    i->second.second = 0;
-  vector<const Factor*> ngram(n_);
+  NGramCountMap sentNgrams;
+  CountRef(sent, sentNgrams);
+  
+  return CalcScore(ngrams_, sentNgrams, sent.size());
+}
+  
+float SentenceBLEU::CalcScore(const NGramCountMap & refNgrams, const NGramCountMap & hypNgrams, int hypLen) const {
+  
   valarray<int> hyp(n_);
   valarray<int> correct(n_);
-  int s = sent.size();
-  for (int j=0; j<s; ++j) {
-    int remaining = s-j;
-    int k = (n_ < remaining ? n_ : remaining);
-    ngram.clear();
-    for (int i=1; i<=k; ++i) {
-        ngram.push_back(sent[j + i - 1]);
-        pair<int,int>& p = ngrams_[ngram];
-        if (p.second < p.first) {
-          ++p.second;
-          correct[i-1]++;
-        }
-        // if the 1 gram isn't found, don't try to match don't need to match any 2- 3- .. grams:
-        if (!p.first) {
-          for (; i<=k; ++i)
-            hyp[i-1]++;
-        } else {
-          hyp[i-1]++;
-        }
-      }
+ 
+  float ref_len = GetClosestLength(hypLen);
+  
+  for (NGramCountMap::const_iterator hypIt = hypNgrams.begin();
+       hypIt != hypNgrams.end(); ++hypIt)
+  {
+    NGramCountMap::iterator refIt = ngrams_.find(hypIt->first);
+    
+    if(refIt != refNgrams.end())
+    {
+      correct[hypIt->first.size() - 1] += min(refIt->second.first, hypIt->second.first); 
+    }
+    
+    hyp[hypIt->first.size() - 1] += hypIt->second.first;
   }
-  const float ref_len = GetClosestLength(sent.size());
-  const float hyp_len = sent.size();
+  
+  return CalcBleu(hyp, correct, ref_len, (float) hypLen);
+}
+  
+  
+float SentenceBLEU::ComputeGain(const GainFunction& hyp) const {
+  assert(hyp.GetType() == this->GetType());
+  const NGramCountMap& hyp_ngrams = static_cast<const SentenceBLEU&>(hyp).GetNgrams();
 
+  return CalcScore(ngrams_, hyp_ngrams, static_cast<const SentenceBLEU&>(hyp).GetLength());
+}  
+
+float SentenceBLEU::CalcBleu(const valarray<int> & hyp, const valarray<int> & correct, float ref_len, float hyp_len) const{
+    
   float log_bleu = 0;
   int count = 0;
   for (int i = 0; i < n_; ++i) {
@@ -88,7 +106,7 @@ float SentenceBLEU::ComputeGain(const vector<const Factor*>& sent) const {
     lbp = (hyp_len - ref_len) / hyp_len;
   log_bleu += lbp;
   return exp(log_bleu);
-}
-
+}  
+  
 }
 
--- branches/josiah/josiah/SentenceBleu.h
+++ branches/josiah/josiah/SentenceBleu.h
@@ -2,6 +2,7 @@
 
 #include <map>
 #include <vector>
+#include <valarray>
 #include "GibblerExpectedLossTraining.h"
 #include "GainFunction.h"
 
@@ -10,7 +11,17 @@ namespace Josiah {
 class SentenceBLEU : public GainFunction {
  public:
   SentenceBLEU(int n, const std::vector<std::string>& refs);
+  SentenceBLEU(int n, const vector<const Factor*> & ref);
+    
+  int GetType() const { return 1;}
   float ComputeGain(const std::vector<const Factor*>& hyp) const;
+  float ComputeGain(const GainFunction& hyp) const;
+  
+  int GetLength() const {
+    assert (lengths_.size());
+    return lengths_[0];
+  }
+  
  private:
   struct NGramCompare {
     int operator() (const vector<const Factor*>& a, const vector<const Factor*>& b) {
@@ -27,7 +38,7 @@ class SentenceBLEU : public GainFunction {
   };
   typedef std::map<std::vector<const Factor*>, std::pair<int,int>, NGramCompare> NGramCountMap;
 
-  void CountRef(const vector<const Factor*>& ref);
+  void CountRef(const vector<const Factor*>& ref, NGramCountMap&) const;
 
   inline int GetClosestLength(int hl) const {
     if (lengths_.size() == 1) return lengths_[0];
@@ -43,6 +54,13 @@ class SentenceBLEU : public GainFunction {
     return bl;
   }
 
+  float CalcScore(const NGramCountMap & refNgrams, const NGramCountMap & hypNgrams, int hypLen) const ;
+  float CalcBleu(const valarray<int> & hyp, const valarray<int> & correct, float ref_len, float hyp_len) const;
+  
+  const NGramCountMap& GetNgrams() const {
+    return ngrams_;
+  } 
+  
   std::vector<int> lengths_;
   mutable NGramCountMap ngrams_;
   int n_;
