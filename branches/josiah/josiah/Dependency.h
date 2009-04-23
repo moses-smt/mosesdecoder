0ccd3a71e564349294b2c774fa6c8b0a7d30ae62
bhaddow <bhaddow@1f5c12ca-751b-0410-a591-d2e778427230>
2009-04-23 13:19:45 +0000

    add dependency tree
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@2299 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ branches/josiah/josiah/Dependency.h
@@ -0,0 +1,77 @@
+/***********************************************************************
+Moses - factored phrase-based language decoder
+Copyright (C) 2009 University of Edinburgh
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+***********************************************************************/
+
+#pragma once
+
+#include <set>
+#include <vector>
+
+#include "Factor.h"
+
+#include "Gibbler.h"
+#include "FeatureFunction.h"
+
+namespace Josiah {
+
+class DependencyTree {
+  public:
+    DependencyTree(const std::vector<Word>& words, Moses::FactorType parentFactor);
+    /** Parent of this index, -1 if root*/
+    int getParent(size_t index) const;
+    /** Does the parent word cover the child word? */
+    bool covers(size_t parent, size_t child) const;
+    /** length of sentence */
+    size_t getLength() const {return m_parents.size();}
+    
+  private:
+    std::vector<int> m_parents;
+    std::vector<std::set<size_t> > m_spans;
+};
+ostream& operator<<(ostream& out, const DependencyTree& t);
+
+class CherrySyntacticCohesionFeature : public FeatureFunction {
+  public:
+    CherrySyntacticCohesionFeature(Moses::FactorType parentFactor): FeatureFunction("Cherry"), m_parentFactor(parentFactor) {}
+    /** Initialise with new sample */
+    virtual void init(const Sample& sample) {
+      m_sourceTree.reset(new DependencyTree(sample.GetSourceWords(), m_parentFactor));
+      cerr << "New Tree: " << *(m_sourceTree.get()) << endl;
+    }
+    /** Compute full score of a sample from scratch **/
+    virtual float computeScore();
+    /** Score due to  one segment */
+    virtual float getSingleUpdateScore(const TranslationOption* option, const WordsRange& targetSegment);
+    /** Score due to two segments **/
+    virtual float getPairedUpdateScore(const TranslationOption* leftOption,
+                                       const TranslationOption* rightOption, const WordsRange& targetSegment, const Phrase& targetPhrase);
+    
+    /** Score due to flip */
+    virtual float getFlipUpdateScore(const TranslationOption* leftTgtOption, const TranslationOption* rightTgtOption, 
+                                     const Hypothesis* leftTgtHyp, const Hypothesis* rightTgtHyp, 
+                                     const WordsRange& leftTargetSegment, const WordsRange& rightTargetSegment);
+    
+    virtual ~CherrySyntacticCohesionFeature() {}
+  
+  private:
+    auto_ptr<DependencyTree> m_sourceTree;
+    Moses::FactorType m_parentFactor; //which factor is the parent index?
+    
+};
+
+}
