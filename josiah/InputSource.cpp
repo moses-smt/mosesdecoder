ae1a45871725899e477be5559552ba9430baac12
bhaddow <bhaddow@1f5c12ca-751b-0410-a591-d2e778427230>
2009-05-14 15:29:29 +0000

    Fix mpi decoding so that it batches properly
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/josiah@2331 1f5c12ca-751b-0410-a591-d2e778427230

--- josiah/InputSource.cpp
+++ josiah/InputSource.cpp
@@ -1,7 +1,7 @@
 #include "InputSource.h"
 
-#include <string>
-#include <istream>
+using namespace std;
+
 
 namespace Josiah {
 
@@ -18,6 +18,38 @@ void StreamInputSource::GetSentence(std::string* sentence, int* lineno) {
   (void) lineno;
   std::getline(in, *sentence);
 };
+
+
+BatchedFileInputSource::BatchedFileInputSource(
+            const string& filename, int rank, int size): m_next(0) {
+   ifstream in(filename.c_str());
+   if (!in) {
+       throw runtime_error("Failed to open input file: " + filename);
+   }
+
+   vector<string> lines;
+   string line;
+   while(getline(in,line)) {
+       lines.push_back(line);
+   }
+   float batchSize = (float)lines.size()/size;
+   cerr << "Batch size: " << batchSize << endl;
+   size_t start = (size_t)(rank*batchSize+0.5);
+   size_t end = (size_t)((rank+1)*batchSize+0.5);
+   m_lines.resize(end-start);
+   copy(lines.begin()+start,lines.begin()+end,m_lines.begin());
+   cerr << "batch start: " << start << " batch end: " << end << endl;
+
+}
+
+bool BatchedFileInputSource::HasMore() const {
+    return m_next < m_lines.size();
+}
+
+void BatchedFileInputSource::GetSentence(string* sentence, int* lineno) {
+    *lineno = m_next;
+    *sentence = m_lines[m_next++];
+}
 
 }
 
