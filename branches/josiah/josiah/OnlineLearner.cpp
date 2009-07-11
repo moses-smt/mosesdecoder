3793598bf9781e08d12e02902fdfc3692e2de26e
abarun <abarun@1f5c12ca-751b-0410-a591-d2e778427230>
2009-07-11 13:32:46 +0000

    Sample Rank
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@2368 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ branches/josiah/josiah/OnlineLearner.cpp
@@ -0,0 +1,161 @@
+/*
+ *  OnlineLearner.cpp
+ *  josiah
+ *
+ *  Created by Abhishek Arun on 26/06/2009.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "OnlineLearner.h"
+#include "TranslationDelta.h"
+#include "StaticData.h"
+
+namespace Josiah {
+  
+  ScoreComponentCollection OnlineLearner::GetAveragedWeights() { 
+    ScoreComponentCollection averagedWgts(m_cumulWeights);
+    averagedWgts.DivideEquals(m_iteration);
+    return averagedWgts; 
+  }
+  
+  void PerceptronLearner::doUpdate(TranslationDelta* curr, TranslationDelta* target) {
+    //Do update if target gain is better than curr gain
+    if (target->getGain() > curr->getGain()) {
+      m_currWeights.MinusEquals(curr->getScores());
+      m_currWeights.PlusEquals(target->getScores());  
+    }
+
+    m_cumulWeights.PlusEquals(m_currWeights);
+    m_iteration++;
+    //cerr << "Curr Weights : " << m_currWeights << endl;
+    const_cast<StaticData&>(StaticData::Instance()).SetAllWeights(m_currWeights.data());
+  }
+  
+  void MiraLearner::doUpdate(TranslationDelta* curr, TranslationDelta* target) {
+    
+    vector<float> b;
+    float scoreDiff = target->getScore() - curr->getScore();
+    float gainDiff = target->getGain() - curr->getGain();
+    
+    bool doMira = false;
+    if (scoreDiff < gainDiff) { //MIRA Constraints not satisfied, run MIRA
+      doMira = true;
+      b.push_back( gainDiff - scoreDiff);  
+      vector<ScoreComponentCollection> distance;
+      ScoreComponentCollection dist(target->getScores());
+      dist.MinusEquals(curr->getScores());
+      distance.push_back(dist);    
+      
+      vector<float> alpha = hildreth(distance,b);
+      
+      for (size_t k = 0; k < alpha.size(); k++) {
+        ScoreComponentCollection dist = distance[k];
+        dist.MultiplyEquals(alpha[k]);
+        m_currWeights.PlusEquals(dist);
+      }
+    }
+    
+    m_cumulWeights.PlusEquals(m_currWeights);
+    m_iteration++;
+    const_cast<StaticData&>(StaticData::Instance()).SetAllWeights(m_currWeights.data());
+    
+    //if (doMira) {
+//      //Sanity check
+//      curr->updateWeightedScore();
+//      target->updateWeightedScore();
+//      
+//      cerr << "Target score - curr score " << target->getScore() - curr->getScore() << endl;
+//      cerr << "Target gain - curr gain " << target->getGain() - curr->getGain() << endl;
+//    }
+  }
+  
+  vector<float> MiraLearner::hildreth (const vector<ScoreComponentCollection>& a, const vector<float>& b) {
+    
+    int i;
+    int max_iter = 10000;
+    float eps = 0.00000001;
+    float zero = 0.000000000001;
+    
+    vector<float> alpha ( b.size() );
+    vector<float> F ( b.size() );
+    vector<float> kkt ( b.size() );
+    
+    float max_kkt = -1e100;
+    
+    int K = b.size();
+    
+    float A[K][K];
+    bool is_computed[K];
+    for ( i = 0; i < K; i++ )
+    {
+      A[i][i] = a[i].InnerProduct ( a[i]);
+      is_computed[i] = false;
+    }
+    
+    int max_kkt_i = -1;
+    
+    
+    for ( i = 0; i < b.size(); i++ )
+    {
+      F[i] = b[i];
+      kkt[i] = F[i];
+      if ( kkt[i] > max_kkt )
+      {
+        max_kkt = kkt[i];
+        max_kkt_i = i;
+      }
+    }
+    
+    int iter = 0;
+    float diff_alpha;
+    float try_alpha;
+    float add_alpha;
+    
+    while ( max_kkt >= eps && iter < max_iter )
+    {
+      
+      diff_alpha = A[max_kkt_i][max_kkt_i] <= zero ? 0.0 : F[max_kkt_i]/A[max_kkt_i][max_kkt_i];
+      try_alpha = alpha[max_kkt_i] + diff_alpha;
+      add_alpha = 0.0;
+      
+      if ( try_alpha < 0.0 )
+        add_alpha = -1.0 * alpha[max_kkt_i];
+      else
+        add_alpha = diff_alpha;
+      
+      alpha[max_kkt_i] = alpha[max_kkt_i] + add_alpha;
+      
+      if ( !is_computed[max_kkt_i] )
+      {
+        for ( i = 0; i < K; i++ )
+        {
+          A[i][max_kkt_i] = a[i].InnerProduct (a[max_kkt_i] ); // for version 1
+          //A[i][max_kkt_i] = 0; // for version 1
+          is_computed[max_kkt_i] = true;
+        }
+      }
+      
+      for ( i = 0; i < F.size(); i++ )
+      {
+        F[i] -= add_alpha * A[i][max_kkt_i];
+        kkt[i] = F[i];
+        if ( alpha[i] > zero )
+          kkt[i] = abs ( F[i] );
+      }
+      max_kkt = -1e100;
+      max_kkt_i = -1;
+      for ( i = 0; i < F.size(); i++ )
+        if ( kkt[i] > max_kkt )
+        {
+          max_kkt = kkt[i];
+          max_kkt_i = i;
+        }
+      
+      iter++;
+    }
+    
+    return alpha;
+  }
+  
+}
---
+++ branches/josiah/josiah/OnlineLearnerTrainer.cpp
@@ -0,0 +1,106 @@
+#include "OnlineLearnerTrainer.h"
+
+#ifdef MPI_ENABLED
+#include <mpi.h>
+#endif
+
+#include <cassert>
+
+#include "Optimizer.h"
+#include "Decoder.h"
+
+using namespace std;
+using namespace Moses;
+
+namespace Josiah {
+  
+    
+  OnlineLearnerTrainer::OnlineLearnerTrainer(
+                                             int r, 
+                                             int s, 
+                                             int bsize,
+                                             vector<string>* sents,
+                                             unsigned int rseed,
+                                             bool randomize,
+                                             int maxSents,
+                                             int wt_dump_freq,
+                                             std::string wt_dump_stem, 
+                                             OnlineLearner* learner)
+  : rank(r),
+  size(s),
+  batch_size(bsize),
+  max_sents(maxSents),
+  corpus(),
+  order(batch_size),
+  rng(rseed),
+  keep_going(true),
+  dist(0, sents->size() - 1),
+  draw(rng, dist),
+  randomize_batches(randomize),
+  batch_ctr(0),
+  weight_dump_freq(wt_dump_freq),
+  weight_dump_stem(wt_dump_stem), 
+  m_learner(learner){
+    if (rank >= batch_size) keep_going = false;
+    corpus.swap(*sents);
+    int esize = min(batch_size, size);
+    //cerr << "esize: " << esize << endl;
+    int sents_per_batch = batch_size / esize;
+    cur = cur_start = sents_per_batch * rank;
+    //cerr << "sents_per_batch: " << sents_per_batch << endl;
+    cur_end = min((int)corpus.size(), sents_per_batch * (rank + 1));
+    if (rank == size - 1) cur_end = batch_size;
+    cerr << rank << "/" << size << ": cur_start=" << cur_start << "  cur_end=" << cur_end << endl;
+    assert(cur_end >= cur_start);
+    tlc = 0;
+    ReserveNextBatch();
+  }
+  
+  void OnlineLearnerTrainer::ReserveNextBatch() {
+    cur = cur_start;
+    if (rank == 0) {
+      if (randomize_batches) {
+        for (unsigned int i = 0; i < order.size(); ++i, ++tlc)
+          order[i] = draw();
+      } else {
+        for (unsigned int i = 0; i < order.size(); ++i, ++tlc)
+          order[i] = tlc % corpus.size();
+      }
+      batch_ctr++;
+    } 
+#ifdef MPI_ENABLED
+    if (MPI_SUCCESS != MPI_Bcast(&order[0], order.size(), MPI_INT, 0, MPI_COMM_WORLD))
+      MPI_Abort(MPI_COMM_WORLD,1);
+#endif
+    if (weight_dump_freq > 0 && rank == 0 && batch_ctr > 0 && (batch_ctr % weight_dump_freq) == 0) {
+      stringstream s;
+      s << weight_dump_stem;
+      s << "_";
+      s << batch_ctr;
+      string weight_file = s.str();
+      cerr << "Dumping weights to  " << weight_file << endl;
+      ofstream out(weight_file.c_str());
+      ScoreComponentCollection avgWeights = m_learner->GetAveragedWeights();
+      
+      if (out) {
+        OutputWeights(avgWeights.data(), out);
+        out.close();
+      }  else {
+        cerr << "Failed to dump weights" << endl;
+      }
+    }
+    
+  }
+  
+  bool OnlineLearnerTrainer::HasMore() const {
+    return keep_going && (tlc < max_sents) && (cur < cur_end);
+  }
+  
+  void OnlineLearnerTrainer::GetSentence(string* sentence, int* lineno) {
+    assert(static_cast<unsigned int>(cur) < order.size());
+    if (lineno) *lineno = order[cur];
+    *sentence = corpus[order[cur++]];
+  }
+  
+}
+
