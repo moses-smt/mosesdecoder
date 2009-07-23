6e243df3878571c128c4dfc91f572b68bd634628
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2009-07-23 10:29:30 +0000

    includes suggested by Tom Hoar's mate
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@2399 1f5c12ca-751b-0410-a591-d2e778427230

--- trunk/moses/src/Util.cpp
+++ trunk/moses/src/Util.cpp
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <sys/resource.h>
 #endif
 
+#include <cstring>
 #include <cctype>
 #include <algorithm>
 #include <stdio.h>
