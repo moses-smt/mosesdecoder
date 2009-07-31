8384b4764fd2940a65821696846c3261d42a7371
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2009-07-31 16:24:26 +0000

    berkeley db
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/mt3_chart@2432 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ moses/src/PhraseDictionaryBerkeleyDb.h
@@ -0,0 +1,57 @@
+
+#pragma once
+
+#include <map>
+#include <vector>
+#include <string>
+#include "PhraseDictionary.h"
+#include "ChartRuleCollection.h"
+#include "../../on-disk-phrase-dict/src/Word.h"
+
+namespace Moses
+{
+
+	class PhraseDictionaryBerkeleyDb : public PhraseDictionary
+{
+	typedef PhraseDictionary MyBase;
+	friend std::ostream& operator<<(std::ostream&, const PhraseDictionaryBerkeleyDb&);
+	
+protected:
+	
+public:
+	PhraseDictionaryBerkeleyDb(size_t numScoreComponent)
+	: MyBase(numScoreComponent)
+	{}
+	virtual ~PhraseDictionaryBerkeleyDb();
+
+	PhraseTableImplementation GetPhraseTableImplementation() const
+	{ return BerkeleyDb; }
+
+	bool Load(const std::vector<FactorType> &input
+						, const std::vector<FactorType> &output
+						, const std::string &filePath
+						, const std::vector<float> &weight
+						, size_t tableLimit);
+	
+	// PhraseDictionary impl
+	// for mert
+	void SetWeightTransModel(const std::vector<float> &weightT);
+	//! find list of translations that can translates src. Only for phrase input
+	virtual const TargetPhraseCollection *GetTargetPhraseCollection(const Phrase& src) const;
+	//! Create entry for translation of source to targetPhrase
+	virtual void AddEquivPhrase(const Phrase &source, TargetPhrase *targetPhrase);
+	
+	virtual const ChartRuleCollection *GetChartRuleCollection(
+																														InputType const& src
+																														,WordsRange const& range
+																														,bool adhereTableLimit
+																														,const CellCollection &cellColl) const;
+	
+	void CleanUp();
+	
+};
+
+
+};
+
+
