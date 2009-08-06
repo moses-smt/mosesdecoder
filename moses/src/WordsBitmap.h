2a52af0d86fdfd4ced56ad4445f7c9ae81fb26cf
bhaddow <bhaddow@1f5c12ca-751b-0410-a591-d2e778427230>
2009-08-06 17:11:37 +0000

    merge out from moses trunk
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/moses-server@2472 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/WordsBitmap.h
+++ moses/src/WordsBitmap.h
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <iostream>
 #include <cstring>
 #include <cmath>
+#include <cstdlib>
 #include "TypeDef.h"
 #include "WordsRange.h"
 
