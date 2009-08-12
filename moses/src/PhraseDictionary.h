dbe58e88a309a4511ae6a24f23de1064ec23cc50
bhaddow <bhaddow@1f5c12ca-751b-0410-a591-d2e778427230>
2009-08-12 10:17:22 +0000

    Merged revision r2492 from trunk
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/moses-server@2493 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/PhraseDictionary.h
+++ moses/src/PhraseDictionary.h
@@ -23,6 +23,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include <iostream>
 #include <map>
+#include <memory>
 #include <list>
 #include <vector>
 #include <string>
