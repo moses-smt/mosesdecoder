6a403789d4aed5c43163d84f7e983c638da1d9e4
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2010-01-29 16:19:15 +0000

    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/mt3_chart@2778 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/DynSAInclude/vocab.cpp
+++ moses/src/DynSAInclude/vocab.cpp
@@ -14,8 +14,9 @@ namespace Moses {
     // get id and possibly add to vocab 
     if (words2ids_.find(word) == words2ids_.end())
       if (!closed_) {
-        words2ids_[word] = words2ids_.size(); // size() returns size AFTER insertion of word
-        ids2words_[words2ids_.size()] = word; // so size() is the same here ... 
+        wordID_t id = words2ids_.size() + 1;
+        words2ids_[word] = id; // size() returns size AFTER insertion of word
+        ids2words_[id] = word; // so size() is the same here ... 
       } 
       else {
         return Vocab::kOOVWordID;
