ff0654b7a27bdaa5fee9a0c1297b6a2987f0cc8a
leven101 <leven101@1f5c12ca-751b-0410-a591-d2e778427230>
2010-02-02 12:15:20 +0000

    asserted SA phrases matche offline extraction
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/mt3_chart@2830 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/DynSuffixArray.h
+++ moses/src/DynSuffixArray.h
@@ -19,7 +19,7 @@ public:
   DynSuffixArray();
   DynSuffixArray(vuint_t*);
   ~DynSuffixArray();
-  unsigned countPhrase(const vuint_t*, vuint_t*);
+  bool countPhrase(const vuint_t*, vuint_t*);
   void load(FILE*);
   void save(FILE*);
 private: 
