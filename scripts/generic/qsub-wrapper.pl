a9c20697208fcacc8fe21ac2e14772f1ce696db0
bojar <bojar@1f5c12ca-751b-0410-a591-d2e778427230>
2010-02-03 14:23:53 +0000

    switching to bash, avoiding csh's >& redirection (not accepted by dash) making --robust take the maximum number of restarts
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@2848 1f5c12ca-751b-0410-a591-d2e778427230

--- scripts/generic/qsub-wrapper.pl
+++ scripts/generic/qsub-wrapper.pl
@@ -14,7 +14,7 @@ my $pwdcmd = getPwdCmd();
 
 my $workingdir = `$pwdcmd`; chomp $workingdir;
 my $tmpdir="$workingdir/tmp$$";
-my $jobscript="$workingdir/job$$";
+my $jobscript="$workingdir/job$$.sh";
 my $qsubout="$workingdir/out.job$$";
 my $qsuberr="$workingdir/err.job$$";
 
@@ -87,38 +87,40 @@ sub print_parameters(){
 
 #script creation
 sub preparing_script(){
-  my $scriptheader="\#\!/bin/csh\n\n";
+  my $scriptheader="\#\!/bin/bash\n# the above line is ignored by qsub!\n\n";
   $scriptheader.="uname -a\n\n";
 
   $scriptheader.="cd $workingdir\n\n";
     
-  open (OUT, "> ${jobscript}.csh");
+  open (OUT, "> $jobscript");
   print OUT $scriptheader;
 
-  print OUT "($cmd $parameters > $tmpdir/cmdout$$ ) >& $tmpdir/cmderr$$\n\n";
-  print OUT "echo exit status \$\?\n\n";
+  print OUT "if $cmd $parameters > $tmpdir/cmdout$$ 2> $tmpdir/cmderr$$ ; then
+    echo 'succeeded'
+else
+    echo failed with exit status \$\?
+    die=1
+fi
+";
 
-  if ($cmdout){
-    print OUT "\\mv -f $tmpdir/cmdout$$ $cmdout\n\n";
-    print OUT "echo exit status \$\?\n\n";
+  if (defined $cmdout){
+    print OUT "mv -f $tmpdir/cmdout$$ $cmdout || echo failed to preserve the log: $tmpdir/cmdout$$\n\n";
   }
   else{
-    print OUT "\\rm -f $tmpdir/cmdout$$\n\n";
-    print OUT "echo exit status \$\?\n\n";
+    print OUT "rm -f $tmpdir/cmdout$$\n\n";
   }
 
-  if ($cmderr){
-    print OUT "\\mv -f $tmpdir/cmderr$$ $cmderr\n\n";
-    print OUT "echo exit status \$\?\n\n";
+  if (defined $cmderr){
+    print OUT "mv -f $tmpdir/cmderr$$ $cmderr || echo failed to preserve the log: $tmpdir/cmderr$$\n\n";
   }
   else{
-    print OUT "\\rm -f $tmpdir/cmderr$$\n\n";
-    print OUT "echo exit status \$\?\n\n";
+    print OUT "rm -f $tmpdir/cmderr$$\n\n";
   }
+  print OUT "if [ x\$die == 1 ]; then exit 1; fi\n";
   close(OUT);
 
-  #setting permissions of each script
-  chmod(oct(755),"${jobscript}.csh");
+  #setting permissions of the script
+  chmod(oct(755),$jobscript);
 }
 
 #######################
@@ -135,12 +137,12 @@ preparing_script();
 my $maysync = $old_sge ? "" : "-sync y";
 
 # submit the main job
-my $qsubcmd="qsub $queueparameters $maysync -V -o $qsubout -e $qsuberr -N $qsubname ${jobscript}.csh >& ${jobscript}.log";
+my $qsubcmd="qsub $queueparameters $maysync -V -o $qsubout -e $qsuberr -N $qsubname $jobscript 2> $jobscript.log";
 safesystem($qsubcmd) or die;
 
 #getting id of submitted job
 my $res;
-open (IN,"${jobscript}.log") or die "Can't read main job id: ${jobscript}.log";
+open (IN,"$jobscript.log") or die "Can't read main job id: $jobscript.log";
 chomp($res=<IN>);
 split(/\s+/,$res);
 my $id=$_[2];
@@ -152,10 +154,10 @@ print SDTERR " id:$id\n";
 if ($old_sge) {
   # need to workaround -sync, add another job that will wait for the main one
   # prepare a fake waiting script
-  my $syncscript = "${jobscript}.sync_workaround_script.sh";
+  my $syncscript = "$jobscript.sync_workaround_script.sh";
   safesystem("echo 'date' > $syncscript") or die;
 
-  my $checkpointfile = "${jobscript}.sync_workaround_checkpoint";
+  my $checkpointfile = "$jobscript.sync_workaround_checkpoint";
 
   # ensure checkpoint does not exist
   safesystem("\\rm -f $checkpointfile") or die;
@@ -189,7 +191,7 @@ sub check_exit_status(){
   print STDERR "check_exit_status of submitted job $id\n";
   open(IN,"$qsubout") or die "Can't read $qsubout";
   while (<IN>){
-    $failure=1 if (/exit status 1/);
+    $failure=1 if (/failed with exit status/);
   }
   close(IN);
   return $failure;
