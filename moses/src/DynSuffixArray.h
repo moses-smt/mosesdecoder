118a11497a1c1a2c498fdf2941a267aa1ad2c6dc
leven101 <leven101@1f5c12ca-751b-0410-a591-d2e778427230>
2010-02-12 11:05:43 +0000

    Added suffix array phrase dictionary
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@2890 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ moses/src/DynSuffixArray.h
@@ -0,0 +1,49 @@
+#ifndef MOSES_DYNSFXARY_H
+#define MOSES_DYNSFXARY_H
+
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include "Util.h"
+#include "File.h"
+#include "DynSAInclude/types.h"
+
+namespace Moses {
+using std::vector;
+using std::pair;
+typedef std::vector<unsigned> vuint_t;
+
+class DynSuffixArray {
+public:
+  DynSuffixArray();
+  DynSuffixArray(vuint_t*);
+  ~DynSuffixArray();
+  bool getCorpusIndex(const vuint_t*, vuint_t*);
+  void load(FILE*);
+  void save(FILE*);
+private: 
+  vuint_t* SA_;
+  vuint_t* ISA_;
+  vuint_t* F_;
+  vuint_t* L_;
+  vuint_t* corpus_;
+  void buildAuxArrays();
+  void qsort(int* array, int begin, int end);
+  int compare(int, int, int);
+  void reorder(unsigned, unsigned);
+  void insertFactor(vuint_t*, unsigned);  
+  void deleteFactor(unsigned, unsigned);
+  void substituteFactor(vuint_t*, unsigned);
+  int LF(unsigned);
+  int rank(unsigned, unsigned);
+  int F_firstIdx(unsigned);
+  void printAuxArrays() {
+    std::cerr << "SA\tISA\tF_\tL_\n";
+    for(int i=0; i < SA_->size(); ++i)
+      std::cerr << SA_->at(i) << "\t" << ISA_->at(i) << "\t" << F_->at(i) << "\t" << L_->at(i) << std::endl;
+  }
+};
+
+} //end namespace
+#endif
