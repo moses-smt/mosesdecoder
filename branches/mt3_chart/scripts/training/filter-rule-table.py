e80bc6b42d6888a7c2c8a8a4c3989bd7bcd50112
pjwilliams <pjwilliams@1f5c12ca-751b-0410-a591-d2e778427230>
2010-02-28 20:39:32 +0000

    Faster rule table filtering.
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@2949 1f5c12ca-751b-0410-a591-d2e778427230

--- branches/mt3_chart/scripts/training/filter-rule-table.py
+++ branches/mt3_chart/scripts/training/filter-rule-table.py
@@ -15,8 +15,18 @@ import sys
 # will be allowed if there's an input sequence "the slobbering dog," even
 # if there's no rule to derive a JJ from "slobbering."  (If "slobbering"
 # were an unknown word, the 'unknown-lhs' decoder option would allow it
-# to take a number of NT labels, including JJ, with varying
+# to take a number of NT labels, likely including JJ, with varying
 # probabilities, so removing the rule would be a bad idea.)
+
+class Ngram(tuple):
+    pass
+
+class Gap:
+    def __init__(self, minSpan):
+        self.minSpan = minSpan
+
+    def getMinSpan(self):
+        return self.minSpan
 
 def printUsage():
     sys.stderr.write("Usage: filter-rule-table.py INPUT")
@@ -25,20 +35,23 @@ def main():
     if len(sys.argv) != 2:
         printUsage()
         sys.exit(1)
+    N = 7
     inputSentences = []
     for line in open(sys.argv[1]):
         inputSentences.append(line.split())
-    filterRuleTable(sys.stdin, inputSentences)
+    filterRuleTable(sys.stdin, inputSentences, N)
 
-def filterRuleTable(ruleTable, inputSentences):
-    # Map each input word to a map from sentence indices to lists of
-    # intra-sentence indices.
+def filterRuleTable(ruleTable, inputSentences, N):
+    # Map each input n-gram (n = 1..N) to a map from sentence indices to
+    # lists of intra-sentence indices.
     occurrences = {}
     for i, sentence in enumerate(inputSentences):
-        for j, word in enumerate(sentence):
-            innerMap = occurrences.setdefault(word, {})
-            indices = innerMap.setdefault(i, [])
-            indices.append(j)
+        for n in range(1, N+1):
+            for j in range(0, len(sentence)-n+1):
+                ngram = Ngram(sentence[j:j+n])
+                innerMap = occurrences.setdefault(ngram, {})
+                indices = innerMap.setdefault(i, [])
+                indices.append(j)
     # Compare rules against input.
     prevRHS = None
     prevRuleIncluded = None
@@ -54,25 +67,22 @@ def filterRuleTable(ruleTable, inputSentences):
             print line,
             prevRuleIncluded = True
             continue
-        i = findTerminal(rhs)
-        if i == None:
+        segments = segmentRHS(rhs, N)
+        ngramMaps = [occurrences.get(s, {}) for s in segments if isinstance(s, Ngram)]
+        if len(ngramMaps) == 0:
             print line,
             prevRuleIncluded = True
             continue
-        terminals = getTerminals(rhs)
-        innerMap = occurrences.get(rhs[i], {})
-        # Determine the sentences in which all terminals co-occur.
-        sentences = set(innerMap.keys())
-        for terminal in terminals[1:]:
-            sentences &= set(occurrences.get(terminal, {}).keys())
+        # Determine the sentences in which all n-grams co-occur.
+        sentences = set(ngramMaps[0].keys())
+        for ngramMap in ngramMaps[1:]:
+            sentences &= set(ngramMap.keys())
         # Try to match rule in candidate sentences.
         match = False
         for sentenceIndex in sentences:
-            indices = innerMap[sentenceIndex]
-            for start in indices:
-                if start < i:
-                    continue
-                if matchRule(rhs[i:], inputSentences[sentenceIndex], start):
+            sentenceLength = len(inputSentences[sentenceIndex])
+            for indexSeq in enumerateIndexSeqs(ngramMaps, sentenceIndex):
+                if matchSegments(segments, indexSeq, sentenceLength):
                     print line,
                     match = True
                     break
@@ -88,48 +98,69 @@ def parseRule(line):
 def isNT(symbol):
     return symbol[0] == '[' and symbol[-1] == ']'
 
-def getTerminals(symbols):
-    return [s for s in symbols if not isNT(s)]
-
-def findTerminal(symbols):
-    for i, symbol in enumerate(symbols):
-        if not isNT(symbol):
-            return i
-    return None
-
-def matchRule(rhsSuffix, sentence, start):
-    assert(rhsSuffix[0] == sentence[start])
-    if len(rhsSuffix) == 1:
-        return True
-    i = findTerminal(rhsSuffix[1:])
-    if i == None:
-        remainingNTs = len(rhsSuffix) - 1
-        remainingWords = len(sentence) - start - 1
-        return remainingWords >= remainingNTs
-    i += 1
-    if i == 1:
-        # No intervening NTs: try to match next the word of sentence then
-        # recursively match whatever's left.
-        remainingWords = len(sentence) - start - 1
-        if remainingWords > 0 and rhsSuffix[1] == sentence[start+1]:
-            return matchRule(rhsSuffix[1:], sentence, start+1)
+def segmentRHS(rhs, N):
+    segments = []
+    terminals = []
+    minGapWidth = 0
+    for symbol in rhs:
+        if isNT(symbol):
+            if len(terminals) > 0:
+                assert minGapWidth == 0
+                segments.append(Ngram(terminals))
+                terminals = []
+            minGapWidth += 1
         else:
-            return False
+            if minGapWidth > 0:
+                assert len(terminals) == 0
+                segments.append(Gap(minGapWidth))
+                minGapWidth = 0
+            terminals.append(symbol)
+            if len(terminals) == N:
+                segments.append(Ngram(terminals))
+                terminals = []
+    if minGapWidth > 0:
+        assert len(terminals) == 0
+        segments.append(Gap(minGapWidth))
+    elif len(terminals) > 0:
+        segments.append(Ngram(terminals))
+    return segments
+
+def matchSegments(segments, indexSeq, sentenceLength):
+    assert len(segments) > 0
+    firstSegment = segments[0]
+    i = 0
+    if isinstance(firstSegment, Gap):
+        minPos = firstSegment.getMinSpan()
+        maxPos = sentenceLength-1
     else:
-        # Search for the next terminal in the remainder of sentence (after
-        # skipping one word per intervening NT).  If it's found, recursively
-        # match whatever's left.
-        j = None
-        # (In the following slice, it's fine for start+i to be out of range:
-        # we'll get an empty slice.)
-        for k, word in enumerate(sentence[start+i:]):
-            if word == rhsSuffix[i]:
-                j = k
-                break
-        if j == None:
-            return False
+        minPos = indexSeq[i] + len(firstSegment)
+        i += 1
+        maxPos = minPos
+    for segment in segments[1:]:
+        if isinstance(segment, Gap):
+            if minPos + segment.getMinSpan() > sentenceLength:
+                return False
+            minPos = minPos + segment.getMinSpan()
+            maxPos = sentenceLength-1
         else:
-            return matchRule(rhsSuffix[i:], sentence, start+i+j)
+            pos = indexSeq[i]
+            i += 1
+            if pos < minPos or pos > maxPos:
+                return False
+            minPos = pos + len(segment)
+            maxPos = minPos
+    return True
+
+def enumerateIndexSeqs(ngramMaps, sentenceIndex):
+    assert len(ngramMaps) > 0
+    if len(ngramMaps) == 1:
+        for index in ngramMaps[0][sentenceIndex]:
+            yield [index]
+        return
+    for index in ngramMaps[0][sentenceIndex]:
+        for seq in enumerateIndexSeqs(ngramMaps[1:], sentenceIndex):
+            if seq[0] > index:
+                yield [index] + seq
 
 if __name__ == "__main__":
     main()
