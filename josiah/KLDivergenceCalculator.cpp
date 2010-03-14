25e2b015a076dff3d406cdf94fc6947fde94eab1
abarun <abarun@1f5c12ca-751b-0410-a591-d2e778427230>
2010-03-14 17:55:20 +0000

    Can now compute KLDivergence between sampler estimated derivation or translation distribution  and true distribution. True distribution is obtained running Moses without pruning/discarding.
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/josiah@2969 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ josiah/KLDivergenceCalculator.cpp
@@ -0,0 +1,59 @@
+/***********************************************************************
+Moses - factored phrase-based language decoder
+Copyright (C) 2009 University of Edinburgh
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+***********************************************************************/
+
+#include "KLDivergenceCalculator.h"
+#include "GibblerMaxTransDecoder.h"
+
+
+namespace Josiah {
+
+  KLDivergenceCalculator::KLDivergenceCalculator(const std::string& line) : m_input(line), m_Z(0.0) { 
+    m_klDecoder.reset(new KLDecoder); 
+    m_Z = m_klDecoder->GetZ(m_input); //calc sum of all paths in lattice
+    cerr << "Lattice score for : "<< line << " = " << m_Z << endl;
+  }
+    
+  float KLDivergenceCalculator::calcDDDivergence(const std::vector<std::pair<const Derivation*, float> >& nbest) {
+    float kl = 0;
+    for (size_t i = 0; i < nbest.size(); ++i) {
+      float estimatedProb = log(nbest[i].second);
+      float trueProb = (nbest[i].first)->getScore() - m_Z;
+      IFVERBOSE(3) {
+        cerr << *(nbest[i].first) << " ||| " << nbest[i].second << " ||| " << exp(trueProb) << endl;
+      }
+      kl += nbest[i].second * (estimatedProb - trueProb);
+    }
+    return kl;
+  }
+  
+  
+  float KLDivergenceCalculator::calcTDDivergence(const std::vector<std::pair<const Translation*, float> >& nbest) {
+    float kl = 0;
+    for (size_t i = 0; i < nbest.size(); ++i) {
+      float estimatedProb = log(nbest[i].second);
+      float trueProb = m_klDecoder->GetTranslationScore(*(nbest[i].first)) - m_Z;
+      IFVERBOSE(3) {
+        cerr << *(nbest[i].first) << " ||| " << nbest[i].second << " ||| " << exp(trueProb) << endl;
+      }
+      kl += nbest[i].second * (estimatedProb - trueProb);
+    }
+    return kl;
+  }
+
+}
