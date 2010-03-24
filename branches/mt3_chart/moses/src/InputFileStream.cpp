d1bf8642626a39f55858083a65179d7dd0e86e04
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2010-03-24 18:23:56 +0000

    check file exists when opening
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@2999 1f5c12ca-751b-0410-a591-d2e778427230

--- branches/mt3_chart/moses/src/InputFileStream.cpp
+++ branches/mt3_chart/moses/src/InputFileStream.cpp
@@ -19,26 +19,33 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ***********************************************************************/
 
+#include <iostream>
 #include "InputFileStream.h"
 #include "gzfilebuf.h"
+
+using namespace std;
 
 namespace Moses
 {
 InputFileStream::InputFileStream(const std::string &filePath)
-: std::istream(NULL)
-, m_streambuf(NULL)
-{
-  if (filePath.size() > 3 &&
-      filePath.substr(filePath.size() - 3, 3) == ".gz")
-  {
-    m_streambuf = new gzfilebuf(filePath.c_str());
-  } else {
-    std::filebuf* fb = new std::filebuf();
-    fb->open(filePath.c_str(), std::ios::in);
-    m_streambuf = fb;
-  }
-  this->init(m_streambuf);
-}
+	: std::istream(NULL)
+	, m_streambuf(NULL)
+	{
+		if (filePath.size() > 3 &&
+				filePath.substr(filePath.size() - 3, 3) == ".gz")
+		{
+			m_streambuf = new gzfilebuf(filePath.c_str());
+		} else {
+			std::filebuf* fb = new std::filebuf();
+			fb = fb->open(filePath.c_str(), std::ios::in);
+			if (! fb) {
+				cerr << "Can't read " << filePath.c_str() << endl;
+				exit(1);
+			}
+			m_streambuf = fb;
+		}
+		this->init(m_streambuf);
+	}
 
 InputFileStream::~InputFileStream()
 {
