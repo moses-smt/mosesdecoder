2235615aaa6a2cdab7c6c811677a53db48ec6491
bhaddow <bhaddow@1f5c12ca-751b-0410-a591-d2e778427230>
2010-04-06 15:01:45 +0000

    Scaling of log probabilities
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@3059 1f5c12ca-751b-0410-a591-d2e778427230

--- branches/josiah/moses/src/Util.cpp
+++ branches/josiah/moses/src/Util.cpp
@@ -43,6 +43,15 @@ namespace Moses
 //global variable
 Timer g_timer;
 
+static float logbasefactor = 1;
+void SetLogBaseFactor(float factor) {
+    logbasefactor = factor;
+}
+
+float GetLogBaseFactor() {
+    return logbasefactor;
+}
+
 string GetTempFolder()
 {	
 #ifdef _WIN32
