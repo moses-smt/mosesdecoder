98d6db96ca5350bba0b5198b484dd2775306d624
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2010-04-07 18:20:11 +0000

    chart
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@3072 1f5c12ca-751b-0410-a591-d2e778427230

--- branches/chart_merge/moses/src/Word.cpp
+++ branches/chart_merge/moses/src/Word.cpp
@@ -34,7 +34,12 @@ namespace Moses
 // static
 int Word::Compare(const Word &targetWord, const Word &sourceWord)
 {
-	for (size_t factorType = 0 ; factorType < MAX_NUM_FACTORS ; factorType++)
+    if (targetWord.IsNonTerminal() != sourceWord.IsNonTerminal())
+    {
+	return targetWord.IsNonTerminal() ? -1 : 1;
+    }
+
+    for (size_t factorType = 0 ; factorType < MAX_NUM_FACTORS ; factorType++)
 	{
 		const Factor *targetFactor		= targetWord[factorType]
 								,*sourceFactor	= sourceWord[factorType];
