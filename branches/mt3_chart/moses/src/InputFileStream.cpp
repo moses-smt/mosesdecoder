093071688b35cae3530642355dc1b0212b86d0b7
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2010-04-07 12:07:23 +0000

    gcc versions
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@3068 1f5c12ca-751b-0410-a591-d2e778427230

--- branches/mt3_chart/moses/src/InputFileStream.cpp
+++ branches/mt3_chart/moses/src/InputFileStream.cpp
@@ -20,6 +20,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ***********************************************************************/
 
 #include <iostream>
+#include <stdlib.h>
 #include "InputFileStream.h"
 #include "gzfilebuf.h"
 
