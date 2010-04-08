5f1fd96111be9cdb2922dab44d2d051ac0504c17
bojar <bojar@1f5c12ca-751b-0410-a591-d2e778427230>
2010-04-08 14:52:35 +0000

    Merge branch 'bilingualSA' into moses-svn
    Conflicts:
    
    	moses/src/DynSAInclude/file.h
    
    
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@3074 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/DynSuffixArray.cpp
+++ moses/src/DynSuffixArray.cpp
@@ -1,102 +1,120 @@
 #include "DynSuffixArray.h"
 #include <iostream>
+
 namespace Moses {
+
 DynSuffixArray::DynSuffixArray() {
-  SA_ = new vuint_t();
-  ISA_ = new vuint_t();
-  F_ = new vuint_t();
-  L_ = new vuint_t();
+  m_SA = new vuint_t();
+  m_ISA = new vuint_t();
+  m_F = new vuint_t();
+  m_L = new vuint_t();
   std::cerr << "DYNAMIC SUFFIX ARRAY CLASS INSTANTIATED" << std::endl;
 }
+
 DynSuffixArray::~DynSuffixArray() {
-  delete SA_;
-  delete ISA_;
-  delete F_;
-  delete L_;
+  delete m_SA;
+  delete m_ISA;
+  delete m_F;
+  delete m_L;
 }
+
 DynSuffixArray::DynSuffixArray(vuint_t* crp) {
   // make native int array and pass to SA builder
-  corpus_ = crp; 
-  int size = corpus_->size();
+  m_corpus = crp; 
+  int size = m_corpus->size();
   int* tmpArr = new int[size];
   for(int i=0 ; i < size; ++i) tmpArr[i] = i; 
+  
   qsort(tmpArr, 0, size-1);
-  SA_ = new vuint_t(tmpArr, tmpArr + size);
+  
+  m_SA = new vuint_t(tmpArr, tmpArr + size);
   //std::cerr << "printing SA " << std::endl;
-  //for(int i=0; i < size; ++i) std::cerr << SA_->at(i) << std::endl;
+  //for(int i=0; i < size; ++i) std::cerr << m_SA->at(i) << std::endl;
   delete[] tmpArr;
   std::cerr << "DYNAMIC SUFFIX ARRAY CLASS INSTANTIATED WITH SIZE " << size << std::endl;
   buildAuxArrays();
   //printAuxArrays();
 }
+
 void DynSuffixArray::buildAuxArrays() {
-  int size = SA_->size();
-  ISA_ = new vuint_t(size);
-  F_ = new vuint_t(size);
-  L_ = new vuint_t(size);
+  int size = m_SA->size();
+  m_ISA = new vuint_t(size);
+  m_F = new vuint_t(size);
+  m_L = new vuint_t(size);
+  
   for(int i=0; i < size; ++i) {
-    ISA_->at(SA_->at(i)) = i;
-    //(*ISA_)[(*SA_)[i]] = i;
-    (*F_)[i] = (*corpus_)[SA_->at(i)];
-    (*L_)[i] = (*corpus_)[(SA_->at(i) == 0 ? size-1 : SA_->at(i)-1)]; 
+    m_ISA->at(m_SA->at(i)) = i;
+    //(*m_ISA)[(*m_SA)[i]] = i;
+    (*m_F)[i] = (*m_corpus)[m_SA->at(i)];
+    (*m_L)[i] = (*m_corpus)[(m_SA->at(i) == 0 ? size-1 : m_SA->at(i)-1)]; 
   }
 }
+
 int DynSuffixArray::rank(unsigned word, unsigned idx) {
 /* use Gerlach's code to make rank faster */
   // the number of word in L[0..i]
   int r(0);
   for(unsigned i=0; i < idx; ++i)
-    if(L_->at(i) == word) ++r;
+    if(m_L->at(i) == word) ++r;
   return r;
 }
+
 /* count function should be implemented
  * with binary search over suffix array!! */
 int DynSuffixArray::F_firstIdx(unsigned word) {
-  // return index of first row where word is found in F_
-  int low = std::lower_bound(F_->begin(), F_->end(), word) - F_->begin();
-  if(F_->at(low) == word) return low;
+  // return index of first row where word is found in m_F
+  int low = std::lower_bound(m_F->begin(), m_F->end(), word) - m_F->begin();
+  if(m_F->at(low) == word) return low;
   else return -1;
 }
+
 /* uses rank() and c() to obtain the LF function */
 int DynSuffixArray::LF(unsigned L_idx) {
   int fIdx(-1);
-  unsigned word = L_->at(L_idx);
+  unsigned word = m_L->at(L_idx);
   if((fIdx = F_firstIdx(word)) != -1) 
     return fIdx + rank(word, L_idx);
+  else return -1;
 }
+
 void DynSuffixArray::insertFactor(vuint_t* newSent, unsigned newIndex) {
   // for sentences
   //stages 1, 2, 4 stay same from 1char case
   //(use last word of new text in step 2 and save Ltmp until last insert?)
   //stage 3...all words of new sentence are inserted backwards
   // stage 2: k=ISA[newIndex], tmp= L[k], L[k]  = newChar
-  assert(newIndex <= SA_->size());
+  assert(newIndex <= m_SA->size());
   int k(-1), kprime(-1);
-  k = (newIndex < SA_->size() ? ISA_->at(newIndex) : ISA_->at(0)); // k is now index of the cycle that starts at newindex
+  k = (newIndex < m_SA->size() ? m_ISA->at(newIndex) : m_ISA->at(0)); // k is now index of the cycle that starts at newindex
   int true_pos = LF(k); // track cycle shift (newIndex - 1)
-  int Ltmp = L_->at(k);
-  L_->at(k) = (*newSent)[newSent->size()-1];  // cycle k now ends with correct word
+  int Ltmp = m_L->at(k);
+  m_L->at(k) = (*newSent)[newSent->size()-1];  // cycle k now ends with correct word
+  
   for(int j = newSent->size()-1; j > -1; --j) {
     kprime = LF(k);  // find cycle that starts with (newindex - 1)
-    //kprime += ((L_[k] == Ltmp) && (k > isa[k]) ? 1 : 0); // yada yada
+    //kprime += ((m_L[k] == Ltmp) && (k > isa[k]) ? 1 : 0); // yada yada
     // only terminal char can be 0 so add new vocab at end
-    kprime = (kprime > 0 ? kprime : SA_->size());  
+    kprime = (kprime > 0 ? kprime : m_SA->size());  
     true_pos += (kprime <= true_pos ? 1 : 0); // track changes
     // insert everything
-    F_->insert(F_->begin() + kprime, (*newSent)[j]);
+    m_F->insert(m_F->begin() + kprime, (*newSent)[j]);
     int theLWord = (j == 0 ? Ltmp : (*newSent)[j-1]);
-    L_->insert(L_->begin() + kprime, theLWord);
-    piterate(SA_, itr) 
+  
+    m_L->insert(m_L->begin() + kprime, theLWord);
+    piterate(m_SA, itr) 
       if(*itr >= newIndex) ++(*itr);
-    SA_->insert(SA_->begin() + kprime, newIndex);
-    piterate(ISA_, itr)
-      if(*itr >= kprime) ++(*itr);
-    ISA_->insert(ISA_->begin() + newIndex, kprime);
+  
+    m_SA->insert(m_SA->begin() + kprime, newIndex);
+    piterate(m_ISA, itr)
+      if((int)*itr >= kprime) ++(*itr);
+  
+    m_ISA->insert(m_ISA->begin() + newIndex, kprime);
     k = kprime;
   }
   // Begin stage 4
   reorder(true_pos, LF(kprime)); // actual position vs computed position of cycle (newIndex-1)
 }
+
 void DynSuffixArray::reorder(unsigned j, unsigned jprime) {
   printf("j=%d\tj'=%d\n", j, jprime);
   while(j != jprime) {
@@ -104,72 +122,78 @@ void DynSuffixArray::reorder(unsigned j, unsigned jprime) {
     int tmp, isaIdx(-1);
     int new_j = LF(j);
     // for SA, L, and F, the element at pos j is moved to j'
-    tmp = L_->at(j); // L
-    L_->at(j) = L_->at(jprime);
-    L_->at(jprime) = tmp;
-    tmp = SA_->at(j);  // SA
-    SA_->at(j) = SA_->at(jprime);
-    SA_->at(jprime) = tmp;
+    tmp = m_L->at(j); // L
+    m_L->at(j) = m_L->at(jprime);
+    m_L->at(jprime) = tmp;
+    tmp = m_SA->at(j);  // SA
+    m_SA->at(j) = m_SA->at(jprime);
+    m_SA->at(jprime) = tmp;
+  
     // all ISA values between (j...j'] decremented
-    for(int i = 0; i < ISA_->size(); ++i) {
-      if((ISA_->at(i) == j) && (isaIdx == -1)) 
+    for(size_t i = 0; i < m_ISA->size(); ++i) {
+      if((m_ISA->at(i) == j) && (isaIdx == -1)) 
         isaIdx = i; // store index of ISA[i] = j
-      if((ISA_->at(i) > j) && (ISA_->at(i) <= jprime)) --(*ISA_)[i];
+      if((m_ISA->at(i) > j) && (m_ISA->at(i) <= jprime)) --(*m_ISA)[i];
     }
     // replace j with j' in ISA
     //isa[isaIdx] = jprime;
-    ISA_->at(isaIdx) = jprime;
+    m_ISA->at(isaIdx) = jprime;
     j = new_j;
     jprime = LF(jprime);
   }
 }
+
 void DynSuffixArray::deleteFactor(unsigned index, unsigned num2del) {
-  int ltmp = L_->at(ISA_->at(index));
-  int true_pos = LF(ISA_->at(index)); // track cycle shift (newIndex - 1)
-  for(int q = 0; q < num2del; ++q) {
-    int row = ISA_->at(index); // gives the position of index in SA and F_
+  int ltmp = m_L->at(m_ISA->at(index));
+  int true_pos = LF(m_ISA->at(index)); // track cycle shift (newIndex - 1)
+  for(size_t q = 0; q < num2del; ++q) {
+    int row = m_ISA->at(index); // gives the position of index in SA and m_F
     std::cerr << "row = " << row << std::endl;
-    std::cerr << "SA[r]/index = " << SA_->at(row) << "/" << index << std::endl;
+    std::cerr << "SA[r]/index = " << m_SA->at(row) << "/" << index << std::endl;
     true_pos -= (row <= true_pos ? 1 : 0); // track changes
-    L_->erase(L_->begin() + row);     
-    F_->erase(F_->begin() + row);     
-    ISA_->erase(ISA_->begin() + index);  // order is important     
-    piterate(ISA_, itr)
-      if(*itr > row) --(*itr);
-    SA_->erase(SA_->begin() + row);
-    piterate(SA_, itr)
+    m_L->erase(m_L->begin() + row);     
+    m_F->erase(m_F->begin() + row);     
+  
+    m_ISA->erase(m_ISA->begin() + index);  // order is important     
+    piterate(m_ISA, itr)
+      if((int)*itr > row) --(*itr);
+  
+    m_SA->erase(m_SA->begin() + row);
+    piterate(m_SA, itr)
       if(*itr > index) --(*itr);
   }
-  L_->at(ISA_->at(index))= ltmp;
-  reorder(LF(ISA_->at(index)), true_pos);
+  m_L->at(m_ISA->at(index))= ltmp;
+  reorder(LF(m_ISA->at(index)), true_pos);
   printAuxArrays();
 }
+
 void DynSuffixArray::substituteFactor(vuint_t* newSents, unsigned newIndex) {
-  std::cerr << "NEEDS TO IMPELEMNT SUBSITITUTE FACTOR\n";
+  std::cerr << "NEEDS TO IMPLEMENT SUBSITITUTE FACTOR\n";
   return;
 }
+
 bool DynSuffixArray::getCorpusIndex(const vuint_t* phrase, vuint_t* indices) {
   pair<vuint_t::iterator,vuint_t::iterator> bounds;
   indices->clear();
-  int phrasesize = phrase->size();
+  size_t phrasesize = phrase->size();
   // find lower and upper bounds on phrase[0]
-  bounds = std::equal_range(F_->begin(), F_->end(), phrase->at(0));
-  // bounds holds first and (last + 1) index of phrase[0] in SA_
-  int lwrBnd = int(bounds.first - F_->begin());
-  int uprBnd = int(bounds.second - F_->begin());
+  bounds = std::equal_range(m_F->begin(), m_F->end(), phrase->at(0));
+  // bounds holds first and (last + 1) index of phrase[0] in m_SA
+  size_t lwrBnd = size_t(bounds.first - m_F->begin());
+  size_t uprBnd = size_t(bounds.second - m_F->begin());
   if(uprBnd - lwrBnd == 0) return false;  // not found
   if(phrasesize == 1) {
-    for(int i=lwrBnd; i < uprBnd; ++i) {
-      indices->push_back(SA_->at(i));
+    for(size_t i=lwrBnd; i < uprBnd; ++i) {
+      indices->push_back(m_SA->at(i));
     }
     return (indices->size() > 0);
   }
   //find longer phrases if they exist
-  for(int i = lwrBnd; i < uprBnd; ++i) {
-    int crpIdx = SA_->at(i);
-    if((crpIdx + phrasesize) >= corpus_->size()) continue; // past end of corpus
-    for(int pos = 1; pos < phrasesize; ++pos) { // for all following words
-      if(corpus_->at(crpIdx + pos) != phrase->at(pos)) {  // if word doesn't match
+  for(size_t i = lwrBnd; i < uprBnd; ++i) {
+    size_t crpIdx = m_SA->at(i);
+    if((crpIdx + phrasesize) >= m_corpus->size()) continue; // past end of corpus
+    for(size_t pos = 1; pos < phrasesize; ++pos) { // for all following words
+      if(m_corpus->at(crpIdx + pos) != phrase->at(pos)) {  // if word doesn't match
         if(indices->size() > 0) i = uprBnd;  // past the phrases since SA is ordered
         break; 
       }
@@ -181,24 +205,28 @@ bool DynSuffixArray::getCorpusIndex(const vuint_t* phrase, vuint_t* indices) {
   //cerr << "Total count of phrase = " << indices->size() << endl;
   return (indices->size() > 0);
 }
+
 void DynSuffixArray::save(FILE* fout) {
-  fWriteVector(fout, *SA_);
+  fWriteVector(fout, *m_SA);
 }
+
 void DynSuffixArray::load(FILE* fin) {
-  fReadVector(fin, *SA_);
+  fReadVector(fin, *m_SA);
 }
+
 int DynSuffixArray::compare(int pos1, int pos2, int max) {
-  for (int i=0; i < max; ++i) {
-    if((pos1 + i < corpus_->size()) && (pos2 + i >= corpus_->size()))
+  for (size_t i = 0; i < (unsigned)max; ++i) {
+    if((pos1 + i < m_corpus->size()) && (pos2 + i >= m_corpus->size()))
       return 1;
-    if((pos2 + i < corpus_->size()) && (pos1 + i >= corpus_->size()))
+    if((pos2 + i < m_corpus->size()) && (pos1 + i >= m_corpus->size()))
       return -1;
   
-    int diff = corpus_->at(pos1+i) - corpus_->at(pos2+i);
+    int diff = m_corpus->at(pos1+i) - m_corpus->at(pos2+i);
     if(diff != 0) return diff;
   }
   return 0;
 }
+
 void DynSuffixArray::qsort(int* array, int begin, int end) {
   if(end > begin) 
   {
--- moses/src/PhraseDictionaryDynSuffixArray.h
+++ moses/src/PhraseDictionaryDynSuffixArray.h
@@ -4,124 +4,33 @@
 #include <map>
 
 #include "PhraseDictionary.h"
-#include "DynSuffixArray.h" 
-#include "DynSAInclude/vocab.h"
-#include "DynSAInclude/types.h"
-#include "DynSAInclude/utils.h"
-#include "InputFileStream.h"
+#include "BilingualDynSuffixArray.h"
+
 namespace Moses {
 
-class SAPhrase
-{
-public:
-	vector<wordID_t> words;		
-	
-	SAPhrase(size_t phraseSize)
-	:words(phraseSize)
-	{}
-	
-	void SetId(size_t pos, wordID_t id)
-	{
-    assert(pos < words.size());
-		words[pos] = id;
-	}
-	bool operator<(const SAPhrase& phr2) const
-  { return words < phr2.words; }
-};
-
-class PhrasePair
-{
-public:
-	int m_startTarget, m_endTarget, m_startSource, m_endSource, m_sntIndex;
-	PhrasePair(int startTarget, int endTarget, int startSource, int endSource, int sntIndex)
-	: m_startTarget(startTarget)
-	, m_endTarget(endTarget)
-	, m_startSource(startSource)
-	, m_endSource(endSource)
-  , m_sntIndex(sntIndex)
-	{}
-
-	size_t GetTargetSize() const
-	{ return m_endTarget - m_startTarget + 1; }
-};
-	
-class SentenceAlignment 
-{
-public:
-  SentenceAlignment(int sntIndex, int sourceSize, int targetSize);
-	int m_sntIndex;
-	vector<wordID_t>* trgSnt;
-  vector<wordID_t>* srcSnt;
-  vector<int> numberAligned; 
-  vector< vector<int> > alignedList; 
-	bool Extract(int maxPhraseLength, vector<PhrasePair*> &ret, int startSource, int endSource) const;
-};
-class ScoresComp {
-public: 
-  ScoresComp(const vector<float>& weights): m_weights(weights) {}
-  bool operator()(const Scores& s1, const Scores& s2) const { 
-    float score1(1), score2(1);
-    int idx1(0), idx2(0);
-    iterate(s1, itr) score1 += (*itr * m_weights.at(idx1++)); 
-    iterate(s2, itr) score2 += (*itr * m_weights.at(idx2++));
-    return score1 < score2;
-  }
-private: 
-  const vector<float>& m_weights;
-};
-	
 class PhraseDictionaryDynSuffixArray: public PhraseDictionary {
 public: 
-  PhraseDictionaryDynSuffixArray(size_t numScoreComponent, PhraseDictionaryFeature* feature);
-  ~PhraseDictionaryDynSuffixArray();
+	PhraseDictionaryDynSuffixArray(size_t numScoreComponent, PhraseDictionaryFeature* feature);
+	~PhraseDictionaryDynSuffixArray();
 	bool Load(string source, string target, string alignments
 						, const vector<float> &weight
 						, size_t tableLimit
 						, const LMList &languageModels
 						, float weightWP);
-	void LoadVocabLookup();
-  void save(string);
-  void load(string);
-  // functions below required by base class
-  void SetWeightTransModel(const vector<float, std::allocator<float> >&);
-  const TargetPhraseCollection* GetTargetPhraseCollection(const Phrase& src) const;
-  void InitializeForInput(const InputType& i);
-  void AddEquivPhrase(const Phrase &source, const TargetPhrase &targetPhrase){}
-  void CleanUp();
+	// functions below required by base class
+	void SetWeightTransModel(const vector<float, std::allocator<float> >&);
+	const TargetPhraseCollection* GetTargetPhraseCollection(const Phrase& src) const;
+	void InitializeForInput(const InputType& i);
+	void AddEquivPhrase(const Phrase &, const TargetPhrase &){}
+	void CleanUp();
 private:
-  DynSuffixArray* srcSA_;
-  DynSuffixArray* trgSA_;
-  vector<wordID_t>* srcCrp_;
-  vector<wordID_t>* trgCrp_;
-  vector<unsigned> srcSntBreaks_, trgSntBreaks_;
-  Vocab* vocab_;
-  ScoresComp* scoreCmp_;
-  vector<SentenceAlignment> alignments_;
-  vector<vector<short> > rawAlignments_;
+	BilingualDynSuffixArray *m_biSA;
 	vector<float> m_weight;
 	size_t m_tableLimit;
 	const LMList *m_languageModels;
 	float m_weightWP;
-	std::map<const Factor *, wordID_t> vocabLookup_;
-	std::map<wordID_t, const Factor *> vocabLookupRev_;	
-  mutable std::map<pair<wordID_t, wordID_t>, pair<float, float> > wordPairCache_; 
-  const int maxPhraseLength_, maxSampleSize_;
-  int loadCorpus(InputFileStream&, vector<wordID_t>&, vector<wordID_t>&);
-  int loadAlignments(InputFileStream& aligs);
-  int loadRawAlignments(InputFileStream& aligs);
-  bool extractPhrases(const int&, const int&, const int&, vector<PhrasePair*>&, bool=false) const;
-  SentenceAlignment getSentenceAlignment(const int, bool=false) const; 
-  vector<unsigned> sampleSelection(vector<unsigned>) const;
-  vector<int> getSntIndexes(vector<unsigned>&, const int) const; 	
-  TargetPhrase* getMosesFactorIDs(const SAPhrase&) const;
-  SAPhrase trgPhraseFromSntIdx(const PhrasePair&) const;
-  bool getLocalVocabIDs(const Phrase&, SAPhrase &) const;
-  void cacheWordProbs(wordID_t) const;
-  pair<float, float> getLexicalWeight(const PhrasePair&) const;
-	int GetSourceSentenceSize(size_t sentenceId) const
-	{ return (sentenceId==srcSntBreaks_.size()-1) ? srcCrp_->size() - srcSntBreaks_.at(sentenceId) : srcSntBreaks_.at(sentenceId+1) - srcSntBreaks_.at(sentenceId); }
-	int GetTargetSentenceSize(size_t sentenceId) const
-	{ return (sentenceId==trgSntBreaks_.size()-1) ? trgCrp_->size() - trgSntBreaks_.at(sentenceId) : trgSntBreaks_.at(sentenceId+1) - trgSntBreaks_.at(sentenceId); }
+	
 };
+
 } // end namespace
 #endif
