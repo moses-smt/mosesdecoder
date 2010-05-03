0118733a45ad8f9513187717f19289bbc1c7ba53
bojar <bojar@1f5c12ca-751b-0410-a591-d2e778427230>
2010-05-03 21:41:28 +0000

    fixing traditional distortion penalty with -continue-partial-translation
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@3222 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/InputType.cpp
+++ moses/src/InputType.cpp
@@ -27,7 +27,12 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 namespace Moses
 {
 
-InputType::InputType(long translationId) : m_translationId(translationId) {}
+InputType::InputType(long translationId) : m_translationId(translationId)
+{
+	m_frontSpanCoveredLength = 0;
+	m_sourceCompleted.resize(0);
+}
+
 InputType::~InputType() {}
 
 TO_STRING_BODY(InputType);
