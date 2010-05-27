a7ef6c6dea4ed905613a3c843c355fbcd51a0612
leven101 <leven101@1f5c12ca-751b-0410-a591-d2e778427230>
2010-05-27 18:53:39 +0000

    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder@3281 1f5c12ca-751b-0410-a591-d2e778427230

--- trunk/moses/src/PhraseDictionaryDynSuffixArray.cpp
+++ trunk/moses/src/PhraseDictionaryDynSuffixArray.cpp
@@ -62,7 +62,7 @@ const TargetPhraseCollection *PhraseDictionaryDynSuffixArray::GetTargetPhraseCol
 		//std::transform(scoreVector.begin(),scoreVector.end(),scoreVector.begin(),NegateScore);
 		std::transform(scoreVector.begin(),scoreVector.end(),scoreVector.begin(),FloorScore);
 		targetPhrase->SetScore(m_feature, scoreVector, m_weight, m_weightWP, *m_languageModels);
-		//cout << *targetPhrase << "\t" << std::setprecision(8) << scoreVector[2] << endl;
+		 //cout << *targetPhrase << "\t" << std::setprecision(8) << scoreVector[2] << endl;
 		ret->Add(targetPhrase);
 	}
 	ret->NthElement(m_tableLimit); // sort the phrases for the dcoder
