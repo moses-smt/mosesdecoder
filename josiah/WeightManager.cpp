8047a6eb8e5fa8b76bbde972e54848ff3c80ef11
bhaddow <bhaddow@1f5c12ca-751b-0410-a591-d2e778427230>
2010-06-03 12:41:46 +0000

    missed from check-in
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/josiah@3298 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ josiah/WeightManager.cpp
@@ -0,0 +1,60 @@
+/***********************************************************************
+ Moses - factored phrase-based language decoder
+ Copyright (C) 2009 University of Edinburgh
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ ***********************************************************************/
+ 
+#include <cassert>
+#include <stdexcept>
+
+#include "WeightManager.h"
+
+using namespace std;
+
+namespace Josiah {
+  
+  auto_ptr<WeightManager> WeightManager::s_instance;
+
+  void WeightManager::init(const std::string& weightsFile) {
+    init();
+    s_instance->m_weights.load(weightsFile);
+  }
+  
+  void WeightManager::init() {
+    assert(!s_instance.get());
+    s_instance.reset(new WeightManager());
+  }
+  
+  WeightManager& WeightManager::instance() {
+    assert(s_instance.get());
+    return *s_instance;
+  }
+
+  FVector& WeightManager::get() {
+    return m_weights;
+  }
+  
+  void WeightManager::scale(FValue& scale) {
+    m_weights *= scale;
+  }
+  
+  void WeightManager::dump(const string& filename) {
+    throw runtime_error("Not implemented");
+  }
+  
+
+
+}
