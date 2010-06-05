c9eb463fe67ba908cff327572484209af852ab9f
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2010-06-05 13:10:26 +0000

    vs.net
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@3301 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/Parameter.cpp
+++ moses/src/Parameter.cpp
@@ -19,6 +19,7 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ***********************************************************************/
 
+#include <ctime>
 #include <iostream>
 #include <iterator>
 #include <fstream>
