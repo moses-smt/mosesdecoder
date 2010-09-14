c1acbe69a15368c21bb944c2368763aa3e2d24f0
oliver-wilson <oliver-wilson@1f5c12ca-751b-0410-a591-d2e778427230>
2010-09-14 16:44:31 +0000

    Implemented basic BleuScoreFeature Evaluate function.
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/mira-mtm5@3442 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/BleuScoreFeature.cpp
+++ moses/src/BleuScoreFeature.cpp
@@ -56,16 +56,64 @@ BleuScoreFeature::BleuScoreFeature() {
 
 FFState* BleuScoreFeature::Evaluate(const Hypothesis& cur_hypo, 
                                     const FFState* prev_state, 
-                                    ScoreComponentCollection* accumulator) const {
+                                    ScoreComponentCollection* accumulator) const
+{
+    std::map< Phrase, size_t > reference_ngrams;
+    std::map< Phrase, size_t >::iterator reference_ngrams_iter;
+    const BleuScoreState& ps = dynamic_cast<const BleuScoreState&>(*prev_state);
+
+    BleuScoreState* new_state = new BleuScoreState(ps);
+
+    float old_bleu, new_bleu;
+    size_t num_new_words, ctx_start_idx, ctx_end_idx, ngram_start_idx,
+           ngram_end_idx;
+
+    // Calculate old bleu;
+    old_bleu = CalculateBleu();
+
+    // Get context and append new words.
+    num_new_words = cur_hypo.GetTargetPhrase().GetSize();
+    Phrase new_words = ps.m_words;
+    new_words.Append(cur_hypo.GetTargetPhrase());
+
+    for (size_t i = 0; i < num_new_words; i++) {
+        for (size_t n = 0; n < BleuScoreState::bleu_order; n++) {
+            ngram_end_idx = new_words.GetSize() - i;
+            ngram_start_idx = ngram_end_idx - n;
+            Phrase ngram = new_words.GetSubString(WordsRange(ngram_start_idx,
+                                                             ngram_end_idx));
+
+            // Update count of ngram order.
+            new_state->m_ngram_counts[n]++;
+
+            // Find out if this ngram is in the reference.
+            reference_ngrams_iter = reference_ngrams.find(ngram);
+            if (reference_ngrams_iter != reference_ngrams.end())
+                new_state->m_ngram_matches[n]++;
+        }
+    }
+
+    // Calculate new bleu.
+    new_bleu = CalculateBleu();
+
+    // Update state.
+    ctx_end_idx = new_words.GetSize();
+    ctx_start_idx = ctx_end_idx - (BleuScoreState::bleu_order - 1);
+    new_state->m_words = new_words.GetSubString(WordsRange(ctx_start_idx,
+                                                           ctx_start_idx));
+
     accumulator->PlusEquals( this, -1.0 );
-    return NULL;
+    return new_state;
 }
 
-const FFState* BleuScoreFeature::EmptyHypothesisState(const InputType &input) const
+float BleuScoreFeature::CalculateBleu() const {
+    return 0.0;
+}
+
+const FFState* BleuScoreFeature::EmptyHypothesisState(const InputType& input) const
 {
     return new BleuScoreState();
 }
-
 
 } // namespace.
 
--- moses/src/BleuScoreFeature.h
+++ moses/src/BleuScoreFeature.h
@@ -50,7 +50,9 @@ public:
     FFState* Evaluate( const Hypothesis& cur_hypo, 
                        const FFState* prev_state, 
                        ScoreComponentCollection* accumulator) const;
-    const FFState* EmptyHypothesisState(const InputType &input) const;
+    float CalculateBleu() const;
+    const FFState* EmptyHypothesisState(const InputType&) const;
+
 };
 
 } // Namespace.
