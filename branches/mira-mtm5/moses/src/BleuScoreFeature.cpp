50bd4fe9debb1fc6fbcf4c309bfec7345073655c
oliver-wilson <oliver-wilson@cc96ff50-19ce-11e0-b349-13d7f0bd23df>
2010-09-14 15:00:22 +0000

    Added BleuScoreFeature for MIRA training.
    git-svn-id: http://svn.statmt.org/repository/mira@3438 cc96ff50-19ce-11e0-b349-13d7f0bd23df

---
+++ branches/mira-mtm5/moses/src/BleuScoreFeature.cpp
@@ -0,0 +1,71 @@
+#include "BleuScoreFeature.h"
+
+#include "StaticData.h"
+
+namespace Moses {
+
+size_t BleuScoreState::bleu_order = 4;
+
+BleuScoreState::BleuScoreState(): m_words(Output),
+                                  m_ngram_counts(bleu_order),
+                                  m_ngram_matches(bleu_order)
+{
+}
+
+int BleuScoreState::Compare(const FFState& o) const
+{
+    if (&o == this)
+        return 0;
+
+    const BleuScoreState& other = dynamic_cast<const BleuScoreState&>(o);
+
+    if (m_target_length < other.m_target_length)
+        return -1;
+    if (m_target_length > other.m_target_length)
+        return 1;
+
+    if (m_reference_length < other.m_reference_length)
+        return -1;
+    if (m_reference_length > other.m_reference_length)
+        return 1;
+
+    int c = m_words.Compare(other.m_words);
+
+    if (c != 0)
+        return c;
+
+    for(size_t i = 0; i < m_ngram_counts.size(); i++) {
+        if (m_ngram_counts[i] < other.m_ngram_counts[i])
+            return -1;
+        if (m_ngram_counts[i] > other.m_ngram_counts[i])
+            return 1;
+        if (m_ngram_matches[i] < other.m_ngram_matches[i])
+            return -1;
+        if (m_ngram_matches[i] > other.m_ngram_matches[i])
+            return 1;
+    }
+
+    return 0;
+}
+
+BleuScoreFeature::BleuScoreFeature() {
+    const_cast<ScoreIndexManager&>
+        (StaticData::Instance().GetScoreIndexManager()).AddScoreProducer(this);
+
+}
+
+FFState* BleuScoreFeature::Evaluate(const Hypothesis& cur_hypo, 
+                                    const FFState* prev_state, 
+                                    ScoreComponentCollection* accumulator) const {
+    accumulator->PlusEquals( this, -1.0 );
+    return NULL;
+}
+
+const FFState* BleuScoreFeature::EmptyHypothesisState() const
+{
+    return NULL;
+}
+
+
+} // namespace.
+
