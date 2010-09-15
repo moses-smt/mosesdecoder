4a7c87b802eb581844432eef58ece38469205816
ales-t <ales-t@cc96ff50-19ce-11e0-b349-13d7f0bd23df>
2010-09-15 12:23:11 +0000

    Added feature name lookup by index.
    git-svn-id: http://svn.statmt.org/repository/mira@3450 cc96ff50-19ce-11e0-b349-13d7f0bd23df

--- branches/mira-mtm5/moses/src/ScoreComponentCollection.h
+++ branches/mira-mtm5/moses/src/ScoreComponentCollection.h
@@ -71,7 +71,7 @@ public:
 	{}
 
 	inline size_t size() const { return m_scores.size(); }
-	const float& operator[](size_t x) const { return m_scores[x]; }
+	const float& operator[](size_t x) const { return m_scores[m_sim.getFeatureName(x)]; }
 
   //! Set all values to 0.0
 	void ZeroAll()
