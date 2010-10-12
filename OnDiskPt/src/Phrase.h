677f0f47a8d6ae558482d27e071c82deca8e6540
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2010-10-12 16:17:41 +0000

    copyright notice & svn properties
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@3620 1f5c12ca-751b-0410-a591-d2e778427230

--- OnDiskPt/src/Phrase.h
+++ OnDiskPt/src/Phrase.h
@@ -1,12 +1,23 @@
 #pragma once
-/*
- *  Phrase.h
- *  CreateOnDisk
- *
- *  Created by Hieu Hoang on 31/12/2009.
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
- *
- */
+// $Id$
+/***********************************************************************
+ Moses - factored phrase-based, hierarchical and syntactic language decoder
+ Copyright (C) 2009 Hieu Hoang
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ ***********************************************************************/
 #include <vector>
 #include <iostream>
 #include "Word.h"
