ec902882e05df55fc5853a4d117fbeda249306c3
bhaddow <bhaddow@1f5c12ca-751b-0410-a591-d2e778427230>
2010-10-12 13:00:36 +0000

    Unit testing
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/mira-mtm5@3615 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ unittest/ScoreComponentCollectionTest.cpp
@@ -0,0 +1,92 @@
+/***********************************************************************
+Moses - factored phrase-based language decoder
+Copyright (C) 2010 University of Edinburgh
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+***********************************************************************/
+
+
+#include <boost/test/unit_test.hpp>
+
+#include <DummyScoreProducers.h>
+#include <FeatureFunction.h>
+#include <ScoreComponentCollection.h>
+
+using namespace Moses;
+
+BOOST_AUTO_TEST_SUITE(scc)
+
+class MockSingleFeature : public StatelessFeatureFunction {
+  public:
+    MockSingleFeature(): StatelessFeatureFunction("MockSingle") {}
+    std::string GetScoreProducerWeightShortName() const {return "sf";}
+    size_t GetNumScoreComponents() const {return 1;}
+};
+
+class MockMultiFeature : public StatelessFeatureFunction {
+  public:
+    MockMultiFeature(): StatelessFeatureFunction("MockMulti") {}
+    std::string GetScoreProducerWeightShortName() const {return "mf";}
+    size_t GetNumScoreComponents() const {return 5;}
+};
+
+struct MockProducers {
+  MockProducers(): single(new MockSingleFeature()), 
+                    multi(new MockMultiFeature()) {}
+  ~MockProducers() {delete single; delete multi;}
+
+  MockSingleFeature* single;
+  MockMultiFeature* multi;
+};
+
+BOOST_FIXTURE_TEST_CASE(ctor, MockProducers) 
+{
+  ScoreComponentCollection scc;
+  BOOST_CHECK_EQUAL(scc.GetScoreForProducer(single),0);
+  float expected[] =  {0,0,0,0,0};
+  std::vector<float> actual= scc.GetScoresForProducer(multi);
+  BOOST_CHECK_EQUAL_COLLECTIONS(expected, expected+5, actual.begin(), actual.begin()+5);
+}
+
+BOOST_FIXTURE_TEST_CASE(plusequals, MockProducers)
+{
+  float arr1[] = {1,2,3,4,5};
+  float arr2[] = {2,4,6,8,10};
+  std::vector<float> vec1(arr1,arr1+5);
+  std::vector<float> vec2(arr2,arr2+5);
+
+  ScoreComponentCollection scc;
+  scc.PlusEquals(single, 3.4f);
+  BOOST_CHECK_EQUAL(scc.GetScoreForProducer(single), 3.4f);
+  scc.PlusEquals(multi,vec1);
+  std::vector<float> actual = scc.GetScoresForProducer(multi);
+  BOOST_CHECK_EQUAL_COLLECTIONS(vec1.begin(),vec1.end()
+        ,actual.begin(), actual.end());
+  scc.PlusEquals(multi,vec1);
+  actual = scc.GetScoresForProducer(multi);
+  BOOST_CHECK_EQUAL_COLLECTIONS(vec2.begin(),vec2.end(),
+         actual.begin(), actual.end());
+
+  BOOST_CHECK_EQUAL(scc.GetScoreForProducer(single), 3.4f);
+}
+
+BOOST_AUTO_TEST_CASE(test)
+{
+  BOOST_CHECK(true);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
