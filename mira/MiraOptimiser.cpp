85a71793a6807d666723474de9488e88674af992
evahasler <evahasler@1f5c12ca-751b-0410-a591-d2e778427230>
2010-10-25 12:22:35 +0000

    Update mira optimatization code and merge Main.cpp
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/mira-mtm5@3652 1f5c12ca-751b-0410-a591-d2e778427230

--- mira/MiraOptimiser.cpp
+++ mira/MiraOptimiser.cpp
@@ -4,75 +4,187 @@ using namespace Moses;
 using namespace std;
 
 namespace Mira {
-	void MiraOptimiser::updateWeights(Moses::ScoreComponentCollection& weights,
-                         	const std::vector< std::vector<Moses::ScoreComponentCollection> >& scores,
-                         	const std::vector< std::vector<float> >& losses,
-                         	const Moses::ScoreComponentCollection& oracleScores) {
 
-	for(unsigned batch = 0; batch < scores.size(); batch++) {
+void MiraOptimiser::updateWeights(ScoreComponentCollection& currWeights,
+		const vector< vector<ScoreComponentCollection> >& featureValues,
+		const vector< vector<float> >& losses,
+		const ScoreComponentCollection& oracleFeatureValues) {
 
-	  Moses::ScoreComponentCollection oldWeights(weights);
-	  float maxTranslation = -1000.0; //what wrong with FLT_MIN ?!
+	// TODO: do we need the oracle feature values?
 
-	  for(unsigned analyseSentence = 0; analyseSentence < scores[batch].size(); analyseSentence++) {
+	size_t numberOfUpdates = 0;
 
-            /* do this:
-            for(unsigned score = 0; score < scores[batch][analyseSentence].size(); score++) {
-              float currentScoreChange = oracleScores[score] - scores[batch][analyseSentence][score];
-              scoreChange += currentScoreChange * weights[score];
-              norm += currentScoreChange * currentScoreChange;
-            }
-            */ 
-            Moses::ScoreComponentCollection currentScoreColl = oracleScores;
-            currentScoreColl.MinusEquals(scores[batch][analyseSentence]);
-	    currentScoreColl.MultiplyEquals(weights);
-	    float scoreChange = currentScoreColl.InnerProduct(weights);
-	    float norm = currentScoreColl.InnerProduct(currentScoreColl);	     
+	cout << "Selected clipping scheme: " << m_clippingScheme << endl;
+	cout << "lower bound: " << m_lowerBound << endl;
+	cout << "upper bound: " << m_upperBound << endl;
 
-            float delta;
-            if(norm == 0.0) //just in case... :-)
-              delta = 0.0;
-            else {
-              
-              delta = (losses[batch][analyseSentence] - scoreChange) / norm;
+	vector< float> alphas(3*m_n);
+	for(size_t batch = 0; batch < featureValues.size(); batch++) {
+		if (m_clippingScheme == 2) {
+			// initialise alphas for each source (alpha for oracle translation = C, all other alphas = 0)
+			for (size_t j = 0; j < 3*m_n; ++j) {
+				if (j == m_n) {
+					// oracle
+					alphas[j] = m_upperBound;
+					std::cout << "alpha " << j << ": " << alphas[j] << endl;
+				}
+				else {
+					alphas[j] = 0;
+					std::cout << "alpha " << j << ": " << alphas[j] << endl;
+				}
+			}
+		}
 
-	      //now get in shape
-              if(delta > upperBound_)
-                delta = upperBound_;
-              else if(delta < lowerBound_)
-                delta = lowerBound_;
-	
-              cout << "scoreChange: " << scoreChange
-                   << "\ndelta: " << delta
-                   << "\nloss: " << losses[batch][analyseSentence] << endl;
-            }
-           
- 	    // do this:	weights += delta * (oracleScores - scores[batch][analyseSentence])
-            Moses::ScoreComponentCollection tempColl = oracleScores;
-            tempColl.MinusEquals(scores[batch][analyseSentence]);
-	    tempColl.MultiplyEquals(delta);
-	    weights.MinusEquals(tempColl);
+		// iterate over nbest lists of translations, feature list contains n*model, n*hope, n*fear)
+		// Combinations for j and j': hope/fear, hope/model, model/fear?
+		// Currently we compare each hope against each fear (10x10),
+		// each hope against each model (10x10), each model against each fear translation (10x10)
+		for (size_t j = 0; j < m_n; ++j) {
+			size_t indexModel_j = j;
+			size_t indexHope_j = j + m_n;		// e_ij'
+			size_t indexFear_j = j + 2*m_n;	// e_ij
 
-	    float tmp = losses[batch][analyseSentence] - oracleScores.InnerProduct(weights);
-	    if(tmp > maxTranslation)
-		maxTranslation = tmp;	
-			
-            //calculate max. for criterion
-            /*
- 	    float sumWeightedFeatures = 0.0;
-            for(unsigned score = 0; score < scores[analyseSentence]->size(); score++) {
-              sumWeightedFeatures += oracleScores[score]*newWeights[score];
-            }
+			for (size_t k = 0; k < m_n; ++k) {
+				size_t indexModel_k = k;
+				size_t indexHope_k = k + m_n;		// e_ij'
+				size_t indexFear_k = k + 2*m_n;	// e_ij
 
-	    if((losses[analyseSentence] - sumWeightedFeatures) > maxTranslation_) {
-              maxTranslation_ = losses[analyseSentence] - sumWeightedFeatures;
-	    } 
-	    */
-          }
-	  oldWeights.MinusEquals(weights);
-	  float criterion = 0.5*oldWeights.InnerProduct(oldWeights) + 0.01*maxTranslation;
-	  cout << "criterion: " << criterion << endl;
+				// Hypothesis pair hope/fear
+				// Compute delta:
+				cout << "\nComparing hope/fear (" << indexHope_j << "," << indexFear_k << ")" << endl;
+				ScoreComponentCollection featureValueDiffs;
+				float delta = computeDelta(currWeights, featureValues[batch], indexHope_j, indexFear_k, losses[batch], alphas, featureValueDiffs);
+
+				// update weight vector:
+				if (delta != 0) {
+					update(currWeights, featureValueDiffs, delta);
+					++numberOfUpdates;
+				}
+
+				// Hypothesis pair hope/model
+				// Compute delta:
+				cout << "\nComparing hope/model (" << indexHope_j << "," << indexModel_k << ")" << endl;
+				featureValueDiffs.ZeroAll();
+				delta = computeDelta(currWeights, featureValues[batch], indexHope_j, indexModel_k, losses[batch], alphas, featureValueDiffs);
+
+				// update weight vector:
+				if (delta != 0) {
+					update(currWeights, featureValueDiffs, delta);
+					++numberOfUpdates;
+				}
+
+				// Hypothesis pair model/fear
+				// Compute delta:
+				cout << "\nComparing model/fear (" << indexModel_j << "," << indexFear_k << ")" << endl;
+				featureValueDiffs.ZeroAll();
+				delta = computeDelta(currWeights, featureValues[batch], indexModel_j, indexFear_k, losses[batch], alphas, featureValueDiffs);
+
+				// update weight vector:
+				if (delta != 0) {
+					update(currWeights, featureValueDiffs, delta);
+					++numberOfUpdates;
+				}
+			}
+
+			cout << endl;
+		}
 	}
-  }
+
+	cout << "Number of updates: " << numberOfUpdates << endl;
+}
+
+/*
+ * Compute delta for weight update.
+ * As part of this compute feature value differences
+ * Dh_ij - Dh_ij' ---> h(e_ij') - h(e_ij)) --> h(hope) - h(fear)
+ * which are used in the delta term and in the weight update term.
+ */
+float MiraOptimiser::computeDelta(ScoreComponentCollection& currWeights,
+		const vector< ScoreComponentCollection>& featureValues,
+		const size_t indexHope,
+		const size_t indexFear,
+		const vector< float>& losses,
+		vector< float>& alphas,
+		ScoreComponentCollection& featureValueDiffs) {
+
+	const ScoreComponentCollection featureValuesHope = featureValues[indexHope];		// hypothesis j'
+	const ScoreComponentCollection featureValuesFear = featureValues[indexFear];		// hypothesis j
+
+	// compute delta
+	float delta = 0.0;
+	float diffOfModelScores = 0.0;		// (Dh_ij - Dh_ij') * w' --->  (h(e_ij') - h(e_ij))) * w' (inner product)
+	float squaredNorm = 0.0;			// ||Dh_ij - Dh_ij'||^2  --->  sum over squares of elements of h(e_ij') - h(e_ij)
+
+	featureValueDiffs = featureValuesHope;
+	featureValueDiffs.MinusEquals(featureValuesFear);
+	cout << "feature value diffs: " << featureValueDiffs << endl;
+	squaredNorm = featureValueDiffs.InnerProduct(featureValueDiffs);
+	diffOfModelScores = featureValueDiffs.InnerProduct(currWeights);
+
+	if (squaredNorm == 0.0) {
+		delta = 0.0;
+	}
+	else {
+		// loss difference used to compute delta: (l_ij - l_ij')  --->  B(e_ij') - B(e_ij)
+		// TODO: simplify and use BLEU scores of hypotheses directly?
+		float lossDiff = losses[indexFear] - losses[indexHope];
+		delta = (lossDiff - diffOfModelScores) / squaredNorm;
+		cout << "delta: " << delta << endl;
+		cout << "loss diff - model diff: " << lossDiff << " - " << diffOfModelScores << endl;
+
+		// clipping
+		switch (m_clippingScheme) {
+		case 1:
+			if (delta > m_upperBound) {
+				cout << "clipping " << delta << " to " << m_upperBound << endl;
+				delta = m_upperBound;
+			}
+			else if (delta < m_lowerBound) {
+				cout << "clipping " << delta << " to " << m_lowerBound << endl;
+				delta = m_lowerBound;
+			}
+
+			// TODO: update
+			//m_lowerBound += delta;
+			//m_upperBound -= delta;
+			//cout << "m_lowerBound = " << m_lowerBound << endl;
+			//cout << "m_upperBound = " << m_upperBound << endl;
+
+			break;
+		case 2:
+			// fear translation: e_ij  --> alpha_ij  = alpha_ij  + delta
+			// hope translation: e_ij' --> alpha_ij' = alpha_ij' - delta
+			// clipping interval: [-alpha_ij, alpha_ij']
+			// clip delta
+			cout << "Interval [" << (-1 * alphas[indexFear]) << "," << alphas[indexHope] << "]" << endl;
+			if (delta > alphas[indexHope]) {
+				cout << "clipping " << delta << " to " << alphas[indexHope] << endl;
+				delta = alphas[indexHope];
+			}
+			else if (delta < (-1 * alphas[indexFear])) {
+				cout << "clipping " << delta << " to " << (-1 * alphas[indexFear]) << endl;
+				delta = (-1 * alphas[indexFear]);
+			}
+
+			// update alphas
+			alphas[indexHope] -= delta;
+			alphas[indexFear] += delta;
+			cout << "alpha[" << indexHope << "] = " << alphas[indexHope] << endl;
+			cout << "alpha[" << indexFear << "] = " << alphas[indexFear] << endl;
+			break;
+		}
+	}
+
+	return delta;
+}
+
+/*
+ * Update the weight vector according to delta and the feature value difference
+ * w' = w' + delta * (Dh_ij - Dh_ij') ---> w' = w' + delta * (h(e_ij') - h(e_ij)))
+ */
+void MiraOptimiser::update(ScoreComponentCollection& currWeights, ScoreComponentCollection& featureValueDiffs, const float delta) {
+	featureValueDiffs.MultiplyEquals(delta);
+	currWeights.PlusEquals(featureValueDiffs);
+}
 }
 
