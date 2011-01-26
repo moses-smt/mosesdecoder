fa04e673bfed1737d7ec161ce20d284c00a08722
heafield <heafield@1f5c12ca-751b-0410-a591-d2e778427230>
2011-01-26 01:19:11 +0000

    Minor fixes: unused parameter, factor optional components into a central header.
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@3849 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ kenlm/util/have.hh
@@ -0,0 +1,11 @@
+/* This ties kenlm's config into Moses's build system.  If you are using kenlm
+ * outside Moses, see http://kheafield.com/code/kenlm/developers/ . 
+ */
+#ifndef UTIL_HAVE__
+#define UTIL_HAVE__
+
+#include "../config.h"
+
+#define HAVE_ZLIB
+
+#endif // UTIL_HAVE__
