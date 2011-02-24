a59ad11b58acf5dd64ec6f5990bb4ef5b74ebf28
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2011-02-24 12:40:21 +0000

    run beautify.perl. Consistent formatting for .h & .cpp files
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@3898 1f5c12ca-751b-0410-a591-d2e778427230

--- moses-chart-cmd/src/Main.cpp
+++ moses-chart-cmd/src/Main.cpp
@@ -5,28 +5,28 @@ Moses - factored phrase-based language decoder
 Copyright (c) 2006 University of Edinburgh
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
+Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-    * Redistributions of source code must retain the above copyright notice, 
+    * Redistributions of source code must retain the above copyright notice,
 			this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, 
-			this list of conditions and the following disclaimer in the documentation 
+    * Redistributions in binary form must reproduce the above copyright notice,
+			this list of conditions and the following disclaimer in the documentation
 			and/or other materials provided with the distribution.
-    * Neither the name of the University of Edinburgh nor the names of its contributors 
-			may be used to endorse or promote products derived from this software 
+    * Neither the name of the University of Edinburgh nor the names of its contributors
+			may be used to endorse or promote products derived from this software
 			without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
-PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS 
-BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
-IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 ***********************************************************************/
 
@@ -74,16 +74,17 @@ using namespace MosesChart;
  **/
 class TranslationTask : public Task
 {
- public:
+public:
   TranslationTask(InputType *source, IOWrapper &ioWrapper)
     : m_source(source)
     , m_ioWrapper(ioWrapper)
   {}
 
-  ~TranslationTask() { delete m_source; }
+  ~TranslationTask() {
+    delete m_source;
+  }
 
-  void Run()
-  {
+  void Run() {
     const StaticData &staticData = StaticData::Instance();
     const TranslationSystem &system = staticData.GetTranslationSystem(TranslationSystem::DEFAULT);
     const size_t lineNumber = m_source->GetTranslationId();
@@ -100,26 +101,27 @@ class TranslationTask : public Task
     m_ioWrapper.OutputBestHypo(bestHypo, lineNumber,
                                staticData.GetReportSegmentation(),
                                staticData.GetReportAllFactors());
-    IFVERBOSE(2) { PrintUserTime("Best Hypothesis Generation Time:"); }
+    IFVERBOSE(2) {
+      PrintUserTime("Best Hypothesis Generation Time:");
+    }
 
-    if (staticData.IsDetailedTranslationReportingEnabled())
-    {
+    if (staticData.IsDetailedTranslationReportingEnabled()) {
       m_ioWrapper.OutputDetailedTranslationReport(bestHypo, lineNumber);
     }
-  
+
     // n-best
     size_t nBestSize = staticData.GetNBestSize();
-    if (nBestSize > 0)
-    {
+    if (nBestSize > 0) {
       VERBOSE(2,"WRITING " << nBestSize << " TRANSLATION ALTERNATIVES TO " << staticData.GetNBestFilePath() << endl);
       MosesChart::TrellisPathList nBestList;
       manager.CalcNBest(nBestSize, nBestList,staticData.GetDistinctNBest());
       m_ioWrapper.OutputNBestList(nBestList, bestHypo, &system, lineNumber);
-      IFVERBOSE(2) { PrintUserTime("N-Best Hypotheses Generation Time:"); }
+      IFVERBOSE(2) {
+        PrintUserTime("N-Best Hypotheses Generation Time:");
+      }
     }
-  
-    if (staticData.GetOutputSearchGraph())
-    {
+
+    if (staticData.GetOutputSearchGraph()) {
       std::ostringstream out;
       manager.GetSearchGraph(lineNumber, out);
       OutputCollector *oc = m_ioWrapper.GetSearchGraphOutputCollector();
@@ -127,11 +129,13 @@ class TranslationTask : public Task
       oc->Write(lineNumber, out.str());
     }
 
-    IFVERBOSE(2) { PrintUserTime("Sentence Decoding Time:"); }
+    IFVERBOSE(2) {
+      PrintUserTime("Sentence Decoding Time:");
+    }
     manager.CalcDecoderStatistics();
   }
 
- private:
+private:
   // Non-copyable: copy constructor and assignment operator not implemented.
   TranslationTask(const TranslationTask &);
   TranslationTask &operator=(const TranslationTask &);
@@ -140,32 +144,42 @@ class TranslationTask : public Task
   IOWrapper &m_ioWrapper;
 };
 
-bool ReadInput(IOWrapper &ioWrapper, InputTypeEnum inputType, InputType*& source) 
+bool ReadInput(IOWrapper &ioWrapper, InputTypeEnum inputType, InputType*& source)
 {
-	delete source;
-	switch(inputType)
-	{
-		case SentenceInput:         source = ioWrapper.GetInput(new Sentence(Input)); break;
-		case ConfusionNetworkInput: source = ioWrapper.GetInput(new ConfusionNet);    break;
-		case WordLatticeInput:      source = ioWrapper.GetInput(new WordLattice);     break;
-		case TreeInputType:					source = ioWrapper.GetInput(new TreeInput(Input));break;
-		default: TRACE_ERR("Unknown input type: " << inputType << "\n");
-	}
-	return (source ? true : false);
+  delete source;
+  switch(inputType) {
+  case SentenceInput:
+    source = ioWrapper.GetInput(new Sentence(Input));
+    break;
+  case ConfusionNetworkInput:
+    source = ioWrapper.GetInput(new ConfusionNet);
+    break;
+  case WordLatticeInput:
+    source = ioWrapper.GetInput(new WordLattice);
+    break;
+  case TreeInputType:
+    source = ioWrapper.GetInput(new TreeInput(Input));
+    break;
+  default:
+    TRACE_ERR("Unknown input type: " << inputType << "\n");
+  }
+  return (source ? true : false);
 }
 
-static void PrintFeatureWeight(const FeatureFunction* ff) {
-  
+static void PrintFeatureWeight(const FeatureFunction* ff)
+{
+
   size_t weightStart  = StaticData::Instance().GetScoreIndexManager().GetBeginIndex(ff->GetScoreBookkeepingID());
   size_t weightEnd  = StaticData::Instance().GetScoreIndexManager().GetEndIndex(ff->GetScoreBookkeepingID());
   for (size_t i = weightStart; i < weightEnd; ++i) {
-    cout << ff->GetScoreProducerDescription() <<  " " << ff->GetScoreProducerWeightShortName() << " " 
-        << StaticData::Instance().GetAllWeights()[i] << endl;
+    cout << ff->GetScoreProducerDescription() <<  " " << ff->GetScoreProducerWeightShortName() << " "
+         << StaticData::Instance().GetAllWeights()[i] << endl;
   }
 }
 
 
-static void ShowWeights() {
+static void ShowWeights()
+{
   cout.precision(6);
   const StaticData& staticData = StaticData::Instance();
   const TranslationSystem& system = staticData.GetTranslationSystem(TranslationSystem::DEFAULT);
@@ -190,80 +204,77 @@ static void ShowWeights() {
 
 int main(int argc, char* argv[])
 {
-	IFVERBOSE(1)
-	{
-		TRACE_ERR("command: ");
-		for(int i=0;i<argc;++i) TRACE_ERR(argv[i]<<" ");
-		TRACE_ERR(endl);
-	}
+  IFVERBOSE(1) {
+    TRACE_ERR("command: ");
+    for(int i=0; i<argc; ++i) TRACE_ERR(argv[i]<<" ");
+    TRACE_ERR(endl);
+  }
 
   IOWrapper::FixPrecision(cout);
   IOWrapper::FixPrecision(cerr);
 
-	// load data structures
-	Parameter parameter;
-	if (!parameter.LoadParam(argc, argv))
-	{
-		return EXIT_FAILURE;		
-	}
+  // load data structures
+  Parameter parameter;
+  if (!parameter.LoadParam(argc, argv)) {
+    return EXIT_FAILURE;
+  }
 
   // create threadpool, if necessary
   int threadcount = (parameter.GetParam("threads").size() > 0) ?
                     Scan<size_t>(parameter.GetParam("threads")[0]) : 1;
 
 #ifdef WITH_THREADS
-  if (threadcount < 1)
-  {
+  if (threadcount < 1) {
     cerr << "Error: Need to specify a positive number of threads" << endl;
     exit(1);
   }
   ThreadPool pool(threadcount);
 #else
-  if (threadcount > 1)
-  {
+  if (threadcount > 1) {
     cerr << "Error: Thread count of " << threadcount
          << " but moses not built with thread support" << endl;
     exit(1);
   }
 #endif
 
-	const StaticData &staticData = StaticData::Instance();
-	if (!StaticData::LoadDataStatic(&parameter))
-		return EXIT_FAILURE;
-    
-    if (parameter.isParamSpecified("show-weights")) {
-      ShowWeights();
-      exit(0);
+  const StaticData &staticData = StaticData::Instance();
+  if (!StaticData::LoadDataStatic(&parameter))
+    return EXIT_FAILURE;
+
+  if (parameter.isParamSpecified("show-weights")) {
+    ShowWeights();
+    exit(0);
+  }
+
+  assert(staticData.GetSearchAlgorithm() == ChartDecoding);
+
+  // set up read/writing class
+  IOWrapper *ioWrapper = GetIODevice(staticData);
+
+  // check on weights
+  vector<float> weights = staticData.GetAllWeights();
+  IFVERBOSE(2) {
+    TRACE_ERR("The score component vector looks like this:\n" << staticData.GetScoreIndexManager());
+    TRACE_ERR("The global weight vector looks like this:");
+    for (size_t j=0; j<weights.size(); j++) {
+      TRACE_ERR(" " << weights[j]);
     }
+    TRACE_ERR("\n");
+  }
+  // every score must have a weight!  check that here:
+  if(weights.size() != staticData.GetScoreIndexManager().GetTotalNumberOfScores()) {
+    TRACE_ERR("ERROR: " << staticData.GetScoreIndexManager().GetTotalNumberOfScores() << " score components, but " << weights.size() << " weights defined" << std::endl);
+    return EXIT_FAILURE;
+  }
 
-	assert(staticData.GetSearchAlgorithm() == ChartDecoding);
+  if (ioWrapper == NULL)
+    return EXIT_FAILURE;
 
-	// set up read/writing class
-	IOWrapper *ioWrapper = GetIODevice(staticData);
-
-	// check on weights
-	vector<float> weights = staticData.GetAllWeights();
-	IFVERBOSE(2) {
-	  TRACE_ERR("The score component vector looks like this:\n" << staticData.GetScoreIndexManager());
-	  TRACE_ERR("The global weight vector looks like this:");
-	  for (size_t j=0; j<weights.size(); j++) { TRACE_ERR(" " << weights[j]); }
-	  TRACE_ERR("\n");
-	}
-	// every score must have a weight!  check that here:
-	if(weights.size() != staticData.GetScoreIndexManager().GetTotalNumberOfScores()) {
-	  TRACE_ERR("ERROR: " << staticData.GetScoreIndexManager().GetTotalNumberOfScores() << " score components, but " << weights.size() << " weights defined" << std::endl);
-	  return EXIT_FAILURE;
-	}
-
-	if (ioWrapper == NULL)
-		return EXIT_FAILURE;
-
-	// read each sentence & decode
-	InputType *source=0;
-	while(ReadInput(*ioWrapper,staticData.GetInputType(),source))
-	{
-		IFVERBOSE(1)
-			ResetUserTime();
+  // read each sentence & decode
+  InputType *source=0;
+  while(ReadInput(*ioWrapper,staticData.GetInputType(),source)) {
+    IFVERBOSE(1)
+    ResetUserTime();
     TranslationTask *task = new TranslationTask(source, *ioWrapper);
     source = NULL;  // task will delete source
 #ifdef WITH_THREADS
@@ -278,48 +289,45 @@ int main(int argc, char* argv[])
   pool.Stop(true);  // flush remaining jobs
 #endif
 
-	delete ioWrapper;
+  delete ioWrapper;
 
-	IFVERBOSE(1)
-		PrintUserTime("End.");
+  IFVERBOSE(1)
+  PrintUserTime("End.");
 
-	#ifdef HACK_EXIT
-	//This avoids that detructors are called (it can take a long time)
-		exit(EXIT_SUCCESS);
-	#else
-		return EXIT_SUCCESS;
-	#endif
+#ifdef HACK_EXIT
+  //This avoids that detructors are called (it can take a long time)
+  exit(EXIT_SUCCESS);
+#else
+  return EXIT_SUCCESS;
+#endif
 }
 
-IOWrapper *GetIODevice(const StaticData &staticData) 
+IOWrapper *GetIODevice(const StaticData &staticData)
 {
-	IOWrapper *ioWrapper;
-	const std::vector<FactorType> &inputFactorOrder = staticData.GetInputFactorOrder()
-																,&outputFactorOrder = staticData.GetOutputFactorOrder();
-	FactorMask inputFactorUsed(inputFactorOrder);
+  IOWrapper *ioWrapper;
+  const std::vector<FactorType> &inputFactorOrder = staticData.GetInputFactorOrder()
+      ,&outputFactorOrder = staticData.GetOutputFactorOrder();
+  FactorMask inputFactorUsed(inputFactorOrder);
 
-	// io
-	if (staticData.GetParam("input-file").size() == 1)
-	{
-	  VERBOSE(2,"IO from File" << endl);
-		string filePath = staticData.GetParam("input-file")[0];
+  // io
+  if (staticData.GetParam("input-file").size() == 1) {
+    VERBOSE(2,"IO from File" << endl);
+    string filePath = staticData.GetParam("input-file")[0];
 
-		ioWrapper = new IOWrapper(inputFactorOrder, outputFactorOrder, inputFactorUsed
-																	, staticData.GetNBestSize()
-																	, staticData.GetNBestFilePath()
-																	, filePath);
-	}
-	else
-	{
-	  VERBOSE(1,"IO from STDOUT/STDIN" << endl);
-		ioWrapper = new IOWrapper(inputFactorOrder, outputFactorOrder, inputFactorUsed
-																	, staticData.GetNBestSize()
-																	, staticData.GetNBestFilePath());
-	}
-	ioWrapper->ResetTranslationId();
+    ioWrapper = new IOWrapper(inputFactorOrder, outputFactorOrder, inputFactorUsed
+                              , staticData.GetNBestSize()
+                              , staticData.GetNBestFilePath()
+                              , filePath);
+  } else {
+    VERBOSE(1,"IO from STDOUT/STDIN" << endl);
+    ioWrapper = new IOWrapper(inputFactorOrder, outputFactorOrder, inputFactorUsed
+                              , staticData.GetNBestSize()
+                              , staticData.GetNBestFilePath());
+  }
+  ioWrapper->ResetTranslationId();
 
-	IFVERBOSE(1)
-		PrintUserTime("Created input-output object");
+  IFVERBOSE(1)
+  PrintUserTime("Created input-output object");
 
-	return ioWrapper;
+  return ioWrapper;
 }
