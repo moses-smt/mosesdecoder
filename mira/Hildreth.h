53cbedfd28275a082ad39a12c8fabd892e30c4a5
ehasler <ehasler@cc96ff50-19ce-11e0-b349-13d7f0bd23df>
2011-04-22 19:28:58 +0000

    update hildreth code for linear classifier to work
    git-svn-id: http://svn.statmt.org/repository/mira@3879 cc96ff50-19ce-11e0-b349-13d7f0bd23df

--- mira/Hildreth.h
+++ mira/Hildreth.h
@@ -5,6 +5,8 @@ namespace Mira {
 
   class Hildreth {
     public :
+			static std::vector<Moses::FValue> optimise (const std::vector<Moses::FVector>& a, const std::vector<Moses::FValue>& b );
+			static std::vector<Moses::FValue> optimise (const std::vector<Moses::FVector>& a, const std::vector<Moses::FValue>& b, Moses::FValue C);
       static std::vector<Moses::FValue> optimise (const std::vector<Moses::ScoreComponentCollection>& a, const std::vector<Moses::FValue>& b );
       static std::vector<Moses::FValue> optimise (const std::vector<Moses::ScoreComponentCollection>& a, const std::vector<Moses::FValue>& b, Moses::FValue C);
   };
