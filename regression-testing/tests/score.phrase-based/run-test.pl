49e56f35bbfc9510c964803b65b50b645de3a7a9
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2011-08-02 10:28:36 +0000

    regression test for score
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@4102 1f5c12ca-751b-0410-a591-d2e778427230

--- regression-testing/tests/score.phrase-based/run-test.pl
+++ regression-testing/tests/score.phrase-based/run-test.pl
@@ -5,6 +5,24 @@ use FindBin qw($Bin);
 
 my $scoreExe	= $ARGV[0];
 
-my $cmd = "$scoreExe $Bin/extract.sorted $Bin/lex.f2e $Bin/phrase-table.4.half.f2e  --GoodTuring \n";
+my $outPath = "$Bin/phrase-table.4.half.f2e";
+my $cmdMain = "$scoreExe $Bin/extract.sorted $Bin/lex.f2e $outPath  --GoodTuring \n";
 
-`$cmd`;
+`$cmdMain`;
+
+my $truthPath = "$Bin/truth/results.txt";
+my $cmd = "diff $outPath $truthPath | wc -l";
+
+my $numDiff = 554;
+$numDiff = `$cmd`;
+
+if ($numDiff == 0)
+{
+  print STDERR "SUCCESS\n";
+  exit 0;
+}
+else
+{
+  print STDERR "FAILURE. Ran $cmdMain\n";
+  exit 1;
+}
