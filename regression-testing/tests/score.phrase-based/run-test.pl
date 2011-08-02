d45a29d9c74bcfdd05679d23193963cd7c2b85a3
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2011-08-02 10:10:09 +0000

    data for score regression test
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@4101 1f5c12ca-751b-0410-a591-d2e778427230

--- regression-testing/tests/score.phrase-based/run-test.pl
+++ regression-testing/tests/score.phrase-based/run-test.pl
@@ -1,3 +1,10 @@
 #!/usr/bin/perl -w
 
+use strict;
+use FindBin qw($Bin);
 
+my $scoreExe	= $ARGV[0];
+
+my $cmd = "$scoreExe $Bin/extract.sorted $Bin/lex.f2e $Bin/phrase-table.4.half.f2e  --GoodTuring \n";
+
+`$cmd`;
