eef0f213e988033dbc0f81c128bcbd3dbc700b0a
servan <servan@1f5c12ca-751b-0410-a591-d2e778427230>
2011-10-06 11:00:48 +0000

    A    mert/MergeScorer.h A    mert/MergeScorer.cpp
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@4303 1f5c12ca-751b-0410-a591-d2e778427230

---
+++ mert/MergeScorer.h
@@ -0,0 +1,73 @@
+#ifndef __MERGESCORER_H__
+#define __MERGESCORER_H__
+
+// #include <stdio.h>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <limits.h>
+#include "Types.h"
+#include "ScoreData.h"
+#include "Scorer.h"
+#include "TerScorer.h"
+#include "BleuScorer.h"
+#include "PerScorer.h"
+#include "CderScorer.h"
+//#include "TERsrc/tercalc.h"
+//#include "TERsrc/terAlignment.h"
+
+using namespace std;
+using namespace TERCpp;
+
+// enum MergeReferenceLengthStrategy { MERGE_AVERAGE, MERGE_SHORTEST, MERGE_CLOSEST };
+
+
+/**
+  * Bleu scoring
+ **/
+class MergeScorer: public StatisticsBasedScorer {
+
+	public:
+		MergeScorer(const string& config = "") : StatisticsBasedScorer("MERGE",config){}
+		virtual void setReferenceFiles(const vector<string>& referenceFiles);
+		virtual void prepareStats(size_t sid, const string& text, ScoreStats& entry);
+		static const int LENGTH;	
+		virtual void whoami() 
+		{
+			cerr << "I AM MergeScorer" << std::endl;
+		}
+// 		size_t NumberOfScores(){ cerr << "MergeScorer: " << (2 * LENGTH + 1) << endl; return (2 * LENGTH + 1); };
+		
+		
+       protected:
+/*        friend float PerScorer::calculateScore(const vector<int>& comps);
+        friend float BleuScorer::calculateScore(const vector<int>& comps);
+        friend float TerScorer::calculateScore(const vector<int>& comps);
+        friend float CderScorer::calculateScore(const vector<int>& comps);
+*/	friend class PerScorer;
+        float calculateScore(const vector<int>& comps);
+//        float calculateScore(const vector<float>& comps);
+		
+	private:
+		string javaEnv;
+		string tercomEnv;
+		//no copy
+		MergeScorer(const MergeScorer&);
+		~MergeScorer(){};
+		MergeScorer& operator=(const MergeScorer&);
+		// data extracted from reference files
+		vector<size_t> _reflengths;
+		vector<multiset<int> > _reftokens;
+		vector<vector<int> > m_references;
+		string m_pid;
+  
+};
+
+
+#endif //__TERSCORER_H
