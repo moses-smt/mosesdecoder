5a0d84da9a524277d045c85aecb601754cabee70
heafield <heafield@1f5c12ca-751b-0410-a591-d2e778427230>
2011-10-10 11:15:13 +0000

    Move LanguageModelChartState into LanguageModelImplementation in preparation for moving responsibility for boundary word tracking from ChartHypothesis to LanguageModelChartState.
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/trunk@4316 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/LanguageModel.cpp
+++ moses/src/LanguageModel.cpp
@@ -28,7 +28,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include "FFState.h"
 #include "LanguageModel.h"
 #include "LanguageModelImplementation.h"
-#include "LanguageModelChartState.h"
 #include "TypeDef.h"
 #include "Util.h"
 #include "Manager.h"
--- moses/src/LanguageModelImplementation.cpp
+++ moses/src/LanguageModelImplementation.cpp
@@ -33,7 +33,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include "FactorCollection.h"
 #include "Phrase.h"
 #include "StaticData.h"
-#include "LanguageModelChartState.h"
+#include "ChartManager.h"
 #include "ChartHypothesis.h"
 
 using namespace std;
@@ -67,6 +67,59 @@ void LanguageModelImplementation::GetState(
 {
   GetValueForgotState(contextFactor, state);
 }
+
+namespace {
+
+// This is the FFState used by LanguageModelImplementation::EvaluateChart.  
+class LanguageModelChartState : public FFState
+{
+private:
+  float m_prefixScore;
+  FFState* m_lmRightContext;
+  const ChartHypothesis *m_hypo;
+
+public:
+  LanguageModelChartState(float prefixScore,
+                          FFState *lmRightContext,
+                          const ChartHypothesis &hypo)
+      :m_prefixScore(prefixScore)
+      ,m_lmRightContext(lmRightContext)
+      ,m_hypo(&hypo)
+  {}
+
+  ~LanguageModelChartState() {
+    delete m_lmRightContext;
+  }
+
+  float GetPrefixScore() const { return m_prefixScore; }
+  FFState* GetRightContext() const { return m_lmRightContext; }
+  const ChartHypothesis* GetHypothesis() const { return m_hypo; }
+
+  int Compare(const FFState& o) const {
+    const LanguageModelChartState &other =
+      dynamic_cast<const LanguageModelChartState &>( o );
+
+    // prefix
+    if (m_hypo->GetCurrSourceRange().GetStartPos() > 0) // not for "<s> ..."
+    {
+      int ret = m_hypo->GetPrefix().Compare(other.GetHypothesis()->GetPrefix());
+      if (ret != 0)
+        return ret;
+    }
+
+    // suffix
+    size_t inputSize = m_hypo->GetManager().GetSource().GetSize();
+    if (m_hypo->GetCurrSourceRange().GetEndPos() < inputSize - 1)// not for "... </s>"
+    {
+      int ret = other.GetRightContext()->Compare( *m_lmRightContext );
+      if (ret != 0)
+        return ret;
+    }
+    return 0;
+  }
+};
+
+} // namespace
 
 FFState* LanguageModelImplementation::EvaluateChart(const ChartHypothesis& hypo, int featureID, ScoreComponentCollection* out, const LanguageModel *scorer) const {
   // data structure for factored context phrase (history and predicted word)
