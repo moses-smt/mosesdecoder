b91e0969acaebcb676097948f70b67892dedda73
Eva Hasler <evahasler@gmail.com>
2011-12-20 13:42:23 +0000

    lazy evaluation in CalculateScore()

--- moses/src/ChartHypothesis.cpp
+++ moses/src/ChartHypothesis.cpp
@@ -61,6 +61,8 @@ ChartHypothesis::ChartHypothesis(const ChartTranslationOption &transOpt,
   {
     m_prevHypos.push_back(iter->GetHypothesis());
   }
+
+  m_lmList = m_manager.GetTranslationSystem()->GetLanguageModels();
 }
 
 ChartHypothesis::~ChartHypothesis()
@@ -141,36 +143,29 @@ int ChartHypothesis::RecombineCompare(const ChartHypothesis &compare) const
 
 void ChartHypothesis::CalcScore()
 {
-  // total scores from prev hypos
-  std::vector<const ChartHypothesis*>::iterator iter;
-  for (iter = m_prevHypos.begin(); iter != m_prevHypos.end(); ++iter) {
-    const ChartHypothesis &prevHypo = **iter;
-    const ScoreComponentCollection &scoreBreakdown = prevHypo.GetScoreBreakdown();
-
-    m_scoreBreakdown.PlusEquals(scoreBreakdown);
-  }
-
   // translation models & word penalty
   const ScoreComponentCollection &scoreBreakdown = GetCurrTargetPhrase().GetScoreBreakdown();
-  m_scoreBreakdown.PlusEquals(scoreBreakdown);
+  m_currScoreBreakdown.PlusEquals(scoreBreakdown);
 
-	// compute values of stateless feature functions that were not
-  // cached in the translation option-- there is no principled distinction
-
-  //const vector<const StatelessFeatureFunction*>& sfs =
-  //  m_manager.GetTranslationSystem()->GetStatelessFeatureFunctions();
-	// TODO!
-  //for (unsigned i = 0; i < sfs.size(); ++i) {
-  //  sfs[i]->ChartEvaluate(m_targetPhrase, &m_scoreBreakdown);
-  //}
-
+  // lm, sparse features, etc
   const std::vector<const StatefulFeatureFunction*>& ffs =
     m_manager.GetTranslationSystem()->GetStatefulFeatureFunctions();
   for (unsigned i = 0; i < ffs.size(); ++i) {
-		m_ffStates[i] = ffs[i]->EvaluateChart(*this,i,&m_scoreBreakdown);
+	  if (ffs[i]->GetScoreProducerWeightShortName() == "lm")
+	  	m_ffStates[i] = ffs[i]->EvaluateChart(*this,i,&m_lmCurrScoreBreakdown);
+	  else
+	  	m_ffStates[i] = ffs[i]->EvaluateChart(*this,i,&m_currScoreBreakdown);
   }
 
-  m_totalScore	= m_scoreBreakdown.GetWeightedScore();
+  m_lmScore = m_lmCurrScoreBreakdown.GetWeightedScore();
+  m_currScoreBreakdown.PlusEquals(m_lmCurrScoreBreakdown);
+  m_totalScore	= m_currScoreBreakdown.GetWeightedScore();
+
+  std::vector<const ChartHypothesis*>::iterator iter;
+  for (iter = m_prevHypos.begin(); iter != m_prevHypos.end(); ++iter) {
+	  const ChartHypothesis &prevHypo = **iter;
+	  m_totalScore += prevHypo.GetTotalScore() - prevHypo.GetLMScore();
+  }
 }
 
 void ChartHypothesis::AddArc(ChartHypothesis *loserHypo)
--- moses/src/ChartHypothesis.h
+++ moses/src/ChartHypothesis.h
@@ -56,7 +56,9 @@ protected:
 
   WordsRange					m_currSourceWordsRange;
 	std::vector<const FFState*> m_ffStates; /*! stateful feature function states */
-  ScoreComponentCollection m_scoreBreakdown /*! detailed score break-down by components (for instance language model, word penalty, etc) */
+  //ScoreComponentCollection m_scoreBreakdown /*! detailed score break-down by components (for instance language model, word penalty, etc) */
+  mutable std::auto_ptr<ScoreComponentCollection> m_scoreBreakdown; /*! detailed score break-down by components (for instance language model, word penalty, etc) */
+  ScoreComponentCollection m_currScoreBreakdown /*! scores for this hypothesis */
   ,m_lmNGram
   ,m_lmPrefix;
   float m_totalScore;
@@ -69,6 +71,13 @@ protected:
   ChartManager& m_manager;
 
   unsigned m_id; /* pkoehn wants to log the order in which hypotheses were generated */
+
+  mutable std::auto_ptr<ScoreComponentCollection> m_lmScoreBreakdown;
+  ScoreComponentCollection m_lmCurrScoreBreakdown;
+
+  LMList m_lmList;
+
+  float m_lmScore;
 
   ChartHypothesis(); // not implemented
   ChartHypothesis(const ChartHypothesis &copy); // not implemented
@@ -125,10 +134,35 @@ public:
   void SetWinningHypo(const ChartHypothesis *hypo);
 
   const ScoreComponentCollection &GetScoreBreakdown() const {
-    return m_scoreBreakdown;
+  	if (!m_scoreBreakdown.get()) {
+      m_scoreBreakdown.reset(new ScoreComponentCollection(m_currScoreBreakdown));
+
+      std::vector<const ChartHypothesis*>::const_iterator iter;
+      for (iter = m_prevHypos.begin(); iter != m_prevHypos.end(); ++iter) {
+        const ChartHypothesis &prevHypo = **iter;
+
+        ScoreComponentCollection prevScoreBreakdown = prevHypo.GetScoreBreakdown();
+        // remove LM scores before adding previous score breakdown
+        LMList::const_iterator lm_iter;
+        for (lm_iter = m_lmList.begin(); lm_iter != m_lmList.end(); ++lm_iter) {
+        	LanguageModel *lm = *lm_iter;
+        	prevScoreBreakdown.Assign(lm, 0);
+        }
+
+        m_scoreBreakdown->PlusEquals(prevScoreBreakdown);
+//        m_scoreBreakdown->PlusEquals(prevHypo.GetScoreBreakdown());
+      }
+    }
+
+    return *m_scoreBreakdown;
   }
+
   float GetTotalScore() const {
     return m_totalScore;
+  }
+
+  float GetLMScore() const {
+    return m_lmScore;
   }
 
   const std::vector<const ChartHypothesis*> &GetPrevHypos() const {
