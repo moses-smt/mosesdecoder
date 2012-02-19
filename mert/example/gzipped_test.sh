a70925317e24ccd932411e170fd305c8d0080c83
Tetsuo Kiso <tetsuo-s@is.naist.jp>
2012-02-20 08:02:23 +0900

    Put global variables in mert/util.cpp in anonymous space.
    We do not allow clients to access the following variables.
    Instead, use the APIs which we provide for that.
    
    Also, remove the unused function, and fix smoke tests.

--- mert/example/gzipped_test.sh
+++ mert/example/gzipped_test.sh
@@ -16,6 +16,8 @@ $extractor --nbest NBEST.gz --reference REF.0,REF.1,REF.2 \
     --ffile FEATSTAT_gz --scfile SCORESTAT_gz \
     --sctype BLEU 2> extractor_gz1.log
 
+gzip -d NBEST.gz
+
 $extractor --nbest NBEST --reference REF.0,REF.1,REF.2 \
     --prev-ffile FEATSTAT --prev-scfile SCORESTAT \
     --ffile FEATSTAT2 --scfile SCORESTAT2 \
@@ -30,10 +32,10 @@ for f in FEATSTAT_gz SCORESTAT_gz; do
     echo "done."
 done
 
-$extractor --nbest NBEST.gz --reference REF.0,REF.1,REF.2 \
+$extractor --nbest NBEST --reference REF.0,REF.1,REF.2 \
     --prev-ffile FEATSTAT_gz.gz --prev-scfile SCORESTAT_gz.gz \
     --ffile FEATSTAT2_gz --scfile SCORESTAT2_gz \
     --sctype BLEU 2> extractor_gz3.log
 
-gzip -d NBEST.gz FEATSTAT_gz.gz SCORESTAT_gz.gz
+gzip -d FEATSTAT_gz.gz SCORESTAT_gz.gz
 echo "Done."
