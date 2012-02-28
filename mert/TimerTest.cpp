b99ebb7a19d50b75e07ce5a2622608e9cee52ef5
Tetsuo Kiso <tetsuo-s@is.naist.jp>
2012-02-28 12:34:40 +0900

    Fix failure of the Timer unit test.

--- mert/TimerTest.cpp
+++ mert/TimerTest.cpp
@@ -5,23 +5,28 @@
 
 #include <string>
 #include <iostream>
+#include <unistd.h>
 
 BOOST_AUTO_TEST_CASE(timer_basic_test) {
   Timer timer;
+  const int sleep_time_microsec = 40; // ad-hoc microseconds to pass unit tests.
+
   timer.start();
   BOOST_REQUIRE(timer.is_running());
-  BOOST_REQUIRE(timer.get_elapsed_cpu_time() > 0.0);
-  BOOST_REQUIRE(timer.get_elapsed_cpu_time_microseconds() > 0);
-  BOOST_REQUIRE(timer.get_elapsed_wall_time() > 0.0);
-  BOOST_REQUIRE(timer.get_elapsed_wall_time_microseconds() > 0);
+  BOOST_REQUIRE(usleep(sleep_time_microsec) == 0);
+  BOOST_CHECK(timer.get_elapsed_cpu_time() > 0.0);
+  BOOST_CHECK(timer.get_elapsed_cpu_time_microseconds() > 0);
+  BOOST_CHECK(timer.get_elapsed_wall_time() > 0.0);
+  BOOST_CHECK(timer.get_elapsed_wall_time_microseconds() > 0);
 
   timer.restart();
   BOOST_REQUIRE(timer.is_running());
-  BOOST_REQUIRE(timer.get_elapsed_cpu_time() > 0.0);
-  BOOST_REQUIRE(timer.get_elapsed_cpu_time_microseconds() > 0);
-  BOOST_REQUIRE(timer.get_elapsed_wall_time() > 0.0);
-  BOOST_REQUIRE(timer.get_elapsed_wall_time_microseconds() > 0);
+  BOOST_REQUIRE(usleep(sleep_time_microsec) == 0);
+  BOOST_CHECK(timer.get_elapsed_cpu_time() > 0.0);
+  BOOST_CHECK(timer.get_elapsed_cpu_time_microseconds() > 0);
+  BOOST_CHECK(timer.get_elapsed_wall_time() > 0.0);
+  BOOST_CHECK(timer.get_elapsed_wall_time_microseconds() > 0);
 
   const std::string s = timer.ToString();
-  BOOST_REQUIRE(!s.empty());
+  BOOST_CHECK(!s.empty());
 }
