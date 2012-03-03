cbf6b7b1425c92d069abad636c7ab3a162899f62
Eva Hasler <evahasler@gmail.com>
2012-03-03 22:20:04 +0000

    modify phrase pair feature

--- moses/src/PhrasePairFeature.cpp
+++ moses/src/PhrasePairFeature.cpp
@@ -24,12 +24,10 @@ size_t PhrasePairFeature::GetNumInputScores() const
   return 0;
 }
 
-void PhrasePairFeature::Evaluate(
-  const TargetPhrase& cur_hypo,
-  ScoreComponentCollection* accumulator) const {
-   const Phrase& source = cur_hypo.GetSourcePhrase();
-   const AlignmentInfo& align = cur_hypo.GetAlignmentInfo();
-  for (AlignmentInfo::const_iterator i = align.begin(); i != align.end(); ++i) {
+void PhrasePairFeature::Evaluate(const TargetPhrase& target, ScoreComponentCollection* accumulator) const {
+   const Phrase& source = target.GetSourcePhrase();
+/*   const AlignmentInfo& align = cur_hypo.GetAlignmentInfo();
+   for (AlignmentInfo::const_iterator i = align.begin(); i != align.end(); ++i) {
     const Factor* sourceFactor = source.GetWord(i->first).GetFactor(m_sourceFactorId);
     const Factor* targetFactor = cur_hypo.GetWord(i->second).GetFactor(m_targetFactorId);
     ostringstream namestr;
@@ -37,7 +35,22 @@ void PhrasePairFeature::Evaluate(
     namestr << ":";
     namestr << targetFactor->GetString();
     accumulator->PlusEquals(this,namestr.str(),1);
-  }
+  }*/
+   ostringstream namestr;
+   namestr << source.GetWord(0).GetFactor(m_sourceFactorId)->GetString();
+   for (size_t i = 1; i < source.GetSize(); ++i) {
+  	 const Factor* sourceFactor = source.GetWord(i).GetFactor(m_sourceFactorId);
+  	 namestr << ",";
+  	 namestr << sourceFactor->GetString();
+   }
+   namestr << "~";
+   namestr << target.GetWord(0).GetFactor(m_targetFactorId)->GetString();
+   for (size_t i = 1; i < target.GetSize(); ++i) {
+  	 const Factor* targetFactor = target.GetWord(i).GetFactor(m_targetFactorId);
+  	 namestr << ",";
+  	 namestr << targetFactor->GetString();
+   }
+   accumulator->PlusEquals(this,namestr.str(),1);
 }
 
 bool PhrasePairFeature::ComputeValueInTranslationOption() const {
