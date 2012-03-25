d2ff30791c40e6493e057b72605f6067f296e4f1
Tetsuo Kiso <tetsuo-s@is.naist.jp>
2012-03-25 21:31:44 +0900

    Use assertions in the test case.

--- mert/OptimizerFactoryTest.cpp
+++ mert/OptimizerFactoryTest.cpp
@@ -7,14 +7,14 @@
 
 namespace {
 
-inline void CheckBuildOptimizer(unsigned dim,
+inline bool CheckBuildOptimizer(unsigned dim,
                                 const vector<unsigned>& to_optimize,
                                 const vector<parameter_t>& start,
                                 const string& type,
                                 unsigned int num_random) {
   boost::scoped_ptr<Optimizer> optimizer(
       OptimizerFactory::BuildOptimizer(dim, to_optimize, start, type, num_random));
-  BOOST_CHECK(optimizer.get() != NULL);
+  return optimizer.get() != NULL;
 }
 
 } // namespace
@@ -40,7 +40,7 @@ BOOST_AUTO_TEST_CASE(optimizer_build) {
   start.push_back(0.2);
   const unsigned int num_random = 1;
 
-  CheckBuildOptimizer(dim, to_optimize, start, "powell", num_random);
-  CheckBuildOptimizer(dim, to_optimize, start, "random", num_random);
-  CheckBuildOptimizer(dim, to_optimize, start, "random-direction", num_random);
+  BOOST_CHECK(CheckBuildOptimizer(dim, to_optimize, start, "powell", num_random));
+  BOOST_CHECK(CheckBuildOptimizer(dim, to_optimize, start, "random", num_random));
+  BOOST_CHECK(CheckBuildOptimizer(dim, to_optimize, start, "random-direction", num_random));
 }
