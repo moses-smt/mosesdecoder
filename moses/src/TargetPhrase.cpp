f51ec46009422a5ae39028da5ec5e686b49cb8e0
Eva Hasler <ehasler@saxnot.inf.ed.ac.uk>
2012-04-12 12:21:23 +0100

    fix SetRuleCount

--- moses/src/TargetPhrase.cpp
+++ moses/src/TargetPhrase.cpp
@@ -335,31 +335,32 @@ std::ostream& operator<<(std::ostream& os, const TargetPhrase& tp)
 }
 
 void TargetPhrase::SetRuleCount(const StringPiece &ruleCountString, std::vector<float> &scoreVector) {
-	set<pair<size_t,size_t> > ruleCountInfo;
-	float p_f_given_e = 0, p_e_given_f = 0;
-	p_f_given_e = scoreVector[0];
-	if (scoreVector.size() >= 4) {
-		p_f_given_e = scoreVector[0];
-		p_e_given_f = scoreVector[2];
-	}
-	else {
-		if (scoreVector.size() >= 1 ) p_f_given_e = scoreVector[0];
-		std::cerr << "Warning: possibly wrong format of phrase translation scores" << endl;
-	}
-
   std::vector<std::string> tokens;
   boost::split(tokens, ruleCountString, boost::is_any_of("\t "));
-
-  float targetCount = 0, sourceCount = 0;
+  
   if (tokens.size() == 2) {
-   targetCount = Scan<float>(tokens[0]);
-   sourceCount = Scan<float>(tokens[1]);
-   float ruleCount = p_f_given_e * targetCount;
-   //float ruleCount2 = p_e_given_f * sourceCount; // could use this to double-check the counts
-   m_ruleCount = floor(ruleCount + 0.5);
+    // TODO: if no third column is provided, do we have to take smoothing into account (consolidate.cpp)? 
+    // infer rule counts from target counts
+    float targetCount = 0, sourceCount = 0;
+    float p_f_given_e = 0, p_e_given_f = 0;
+    p_f_given_e = scoreVector[0];
+    if (scoreVector.size() >= 5) {
+      p_f_given_e = scoreVector[0];
+      p_e_given_f = scoreVector[2];
+    }
+    else {
+      if (scoreVector.size() >= 1 ) p_f_given_e = scoreVector[0];
+      std::cerr << "Warning: possibly wrong format of phrase translation scores, number of scores: " << scoreVector.size() << endl;
+    }
+    
+    targetCount = Scan<float>(tokens[0]);
+    sourceCount = Scan<float>(tokens[1]);
+    float ruleCount = p_f_given_e * targetCount;
+    //float ruleCount2 = p_e_given_f * sourceCount; // could use this to double-check the counts
+    m_ruleCount = floor(ruleCount + 0.5);
   }
   else if (tokens.size() == 3) {
-  	m_ruleCount = Scan<float>(tokens[2]);
+    m_ruleCount = Scan<float>(tokens[2]);
   }
 }
 
