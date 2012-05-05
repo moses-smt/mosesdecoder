99c354e5734971af8ae63cd24a74fd49e0060aff
Tetsuo Kiso <tetsuo-s@is.naist.jp>
2012-05-06 06:11:25 +0900

    Base class destructors should be virtual.
    See e.g., http://www.gotw.ca/publications/mill18.htm for details.

--- scripts/training/phrase-extract/SentenceAlignment.cpp
+++ scripts/training/phrase-extract/SentenceAlignment.cpp
@@ -25,6 +25,8 @@
 
 #include "tables-core.h"
 
+SentenceAlignment::~SentenceAlignment() {}
+
 bool SentenceAlignment::processTargetSentence(const char * targetString, int)
 {
   target = tokenize(targetString);
