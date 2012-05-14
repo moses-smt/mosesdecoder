e993d4275f5b170604f4a114c2af456e80b74138
Hieu Hoang <fishandfrolick@gmail.com>
2012-05-14 19:44:53 +0100

    Begin integrating Adam's suffix arrays into EMS

--- scripts/training/wrappers/suffix-array-create.sh
+++ scripts/training/wrappers/suffix-array-create.sh
@@ -1,18 +1,22 @@
-SA_EXEC_DIR=/Users/hieuhoang/workspace/github/cdec/sa-extract
-WORKING_DIR=/Users/hieuhoang/workspace/data/europarl/exp/fr-en/
-EXP_NUM=2
-SOURCE_LANG=fr
-TARGET_LANG=en
+#!/bin/bash
 
-SA_DIR=$WORKING_DIR/model/suffix-array.$EXP_NUM
-mkdir $SA_DIR
+# execute: ~/workspace/bin/moses-smt/scripts/training/wrappers/suffix-array-create.sh $SA_EXEC_DIR $SOURCE_CORPUS $TARGET_CORPUS $ALIGNMENT $SA_OUTPUT
 
-rm -rf $SA_DIR/bitext
+
+SA_EXEC_DIR=$1
+SOURCE_CORPUS=$2
+TARGET_CORPUS=$3
+ALIGNMENT=$4
+SA_OUTPUT=$5
+
+mkdir $SA_OUTPUT
+
+rm -rf $SA_OUTPUT/bitext
 
 pushd .
 cd $SA_EXEC_DIR
 
-./sa-compile.pl -output $SA_DIR -b bitext_name=$WORKING_DIR/training/corpus.$EXP_NUM.$SOURCE_LANG,$WORKING_DIR/training/corpus.$EXP_NUM.$TARGET_LANG -a alignment_name=$WORKING_DIR/model/aligned.$EXP_NUM.grow-diag-final-and > $SA_DIR/extract.ini
+./sa-compile.pl -output $SA_OUTPUT -b bitext_name=$SOURCE_CORPUS,$TARGET_CORPUS -a alignment_name=$ALIGNMENT > $SA_OUTPUT/extract.ini
 
 popd
 
