82ca9fba6943bd01c55287f5765ba7fa3b746df8
Hieu Hoang <fishandfrolick@gmail.com>
2012-06-07 19:40:10 +0100

    delete example suffix array files

--- scripts/training/wrappers/adam-suffix-array/run.sa.create.sh
+++
@@ -1,8 +0,0 @@
-SA_EXEC_DIR=/Users/hieuhoang/workspace/github/cdec/sa-extract
-SOURCE_CORPUS=/Users/hieuhoang/workspace/data/europarl/exp/fr-en/training/corpus.2.fr
-TARGET_CORPUS=/Users/hieuhoang/workspace/data/europarl/exp/fr-en/training/corpus.2.en
-ALIGNMENT=/Users/hieuhoang/workspace/data/europarl/exp/fr-en/model/aligned.3.grow-diag-final-and
-SA_OUTPUT=/Users/hieuhoang/workspace/data/europarl/exp/fr-en/model/suffix-array.3
-
-~/workspace/bin/moses-smt/scripts/training/wrappers/adam-suffix-array/suffix-array-create.sh $SA_EXEC_DIR $SOURCE_CORPUS $TARGET_CORPUS $ALIGNMENT $SA_OUTPUT
-
--- scripts/training/wrappers/adam-suffix-array/suffix-array-extract.sh
+++ scripts/training/wrappers/adam-suffix-array/suffix-array-extract.sh
@@ -2,6 +2,12 @@
 
 # execute: ~/workspace/bin/moses-smt/scripts/training/wrappers/adam-suffix-array/suffix-array-extract.sh $SA_EXEC_DIR $MODEL_DIR $INPUT_FILE $OUTPUT_DIR
 
+# eg.
+#SA_EXEC_DIR=/Users/hieuhoang/workspace/github/cdec/sa-extract
+#MODEL_DIR=/Users/hieuhoang/workspace/data/europarl/exp/fr-en/model/suffix-array.3
+#INPUT_FILE=/Users/hieuhoang/workspace/data/europarl/exp/fr-en/tuning/input.lc.2
+#OUTPUT_DIR=/Users/hieuhoang/workspace/data/europarl/exp/fr-en/tuning/filtered.sa.3
+
 SA_EXEC_DIR=$1
 MODEL_DIR=$2
 INPUT_FILE=$3
