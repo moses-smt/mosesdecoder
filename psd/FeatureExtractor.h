5f6ee7059d1aa16f8772ea6a94aca079fc70cf9f
Ales Tamchyna <a.tamchyna@gmail.com>
2012-07-10 12:56:47 -0400

    draft of FeatureExtractor implementation

--- psd/FeatureExtractor.h
+++ psd/FeatureExtractor.h
@@ -1,46 +1,60 @@
 #ifndef moses_FeatureExtractor_h
 #define moses_FeatureExtractor_h
 
+#include "FeatureConsumer.h"
 
+#include <vector>
+#include <string>
+#include <map>
+#include <boost/bimap/bimap.hpp>
+
+// vector of words, each word is a vector of factors
+typedef std::vector<std::vector<std::string> > ContextType; 
+
+// index of possible target spans
+typedef boost::bimaps::bimap<std::string, size_t> TargetIndexType;
+
+// configuration of feature extractor
 struct FeatureTypes
 {
-  bool m_sourceExternal;
-  bool m_sourceInternal;
-  bool m_targetInternal;
-  bool m_paired;
+  bool m_sourceExternal; // generate context features
+  bool m_sourceInternal; // generate source-side phrase-internal features
+  bool m_targetInternal; // generate target-side phrase-internal features
+  bool m_paired;         // generate paired features
+  bool m_bagOfWords;     // generate bag-of-words features
 
-  size_t m_contextWindow;
+  size_t m_contextWindow; // window size for context features
+
+  // list of factors that should be extracted from context (e.g. 0,1,2)
+  vector<size_t> m_factors;     
 }
 
-// vector of words, each word is a vector of factors (form|lemma|POS for now)
-typedef std::vector<std::vector<std::string> > ContextType; 
-typedef std::map<std::string, size_t> TargetIndexType;
-
+// extract features
 class FeatureExtractor
 {
 public:
-	FeatureExtractor(FeatureTypes ft, FeatureConsumer &fc, const TargetIndexType &targetIndex, bool train);
+  FeatureExtractor(FeatureTypes ft,                  
+    FeatureConsumer *fc,
+    const TargetIndexType &targetIndex,
+    bool train);
 
-	void GenerateFeatures(const ContextType &context,
-                  size_t spanStart,
-                  size_t spanEnd,
-                  const vector<size_t> &translations
-                  vector<float> &losses);
-
-  {
-    for (translation in translations) {
-      if (train) {
-        m_fc->Train(name, losses[i]);
-      } else {
-        losses[i] = m_fc->Predict(name);
-      }
-    }
-  }
+  void GenerateFeatures(const ContextType &context,
+    size_t spanStart,
+    size_t spanEnd,
+    const vector<size_t> &translations
+    vector<float> &losses);
 
 private:
   bool m_train;
   FeatureTypes m_ft;
+  FeatureConsumer *m_fc;
   const TargetIndexType &m_targetIndex;
+
+  void GenerateContextFeatures(const ContextType &context,
+    size_t spanStart,
+    size_t spanEnd);
+
+  void GenerateInternalFeatures(const vector<string> &span);
 };
 
 #endif // moses_FeatureExtractor_h
