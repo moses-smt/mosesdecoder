52e32e69da790bdac419c384d85bdca1cbcb1f1c
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2012-08-06 20:39:01 +0200

    Added save wrapper for std::fwrite

--- moses/src/CompactPT/ThrowingFwrite.cpp
+++ moses/src/CompactPT/ThrowingFwrite.cpp
@@ -24,6 +24,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 size_t ThrowingFwrite(const void *ptr, size_t size, size_t count, FILE* stream) {
   assert(size);
   size_t returnValue = std::fwrite(ptr, size, count, stream);
-  UTIL_THROW_IF(1 != returnValue, util::ErrnoException, "Short fwrite; requested size " << size);
+  UTIL_THROW_IF(count != returnValue, util::ErrnoException, "Short fwrite; requested size " << size);
   return returnValue;
 }
