d2e5d155ab5b719c71030609312eff194222a509
braunefe <braune.fabienne@gmail.com>
2012-09-04 18:06:17 +0100

    finally

--- moses/src/SpanLengthEstimator.cpp
+++ moses/src/SpanLengthEstimator.cpp
@@ -6,12 +6,6 @@ using namespace std;
 namespace Moses
 {
 
-SpanLengthEstimator::SpanLengthEstimator(size_t sourceScores, size_t targetScores)
-{
-    m_sourceScores.resize(sourceScores);
-    m_targetScores.resize(targetScores);
-}
-
 //Assumes that the scores are passed into order
 void SpanLengthEstimator::AddSourceSpanProbas(map<size_t,float> sourceProbas)
 {
@@ -24,16 +18,16 @@ void SpanLengthEstimator::AddTargetSpanProbas(map<size_t,float> targetProbas)
     m_targetScores.push_back(targetProbas);
 }
 
-float SpanLengthEstimator::GetSourceLengthProbas(size_t nonTerminal, size_t spanLength)
+float SpanLengthEstimator::GetSourceLengthProbas(size_t nonTerminal, size_t spanLength) const
 {
-  map<size_t,float>::iterator it;
+  map<size_t,float>::const_iterator it;
   it=m_sourceScores[nonTerminal].find(spanLength);
   return (*it).second;
 }
 
-float SpanLengthEstimator::GetTargetLengthProbas(size_t nonTerminal, size_t spanLength)
+float SpanLengthEstimator::GetTargetLengthProbas(size_t nonTerminal, size_t spanLength) const
 {
-     map<size_t,float>::iterator it;
+     map<size_t,float>::const_iterator it;
     it=m_targetScores[nonTerminal].find(spanLength);
     return (*it).second;
 }
