b9fc0614defa9242bdc250c17f17bd79e4551336
braunefe <braune.fabienne@gmail.com>
2012-09-04 20:34:57 +0100

    Fix in Span Length Estimator

--- moses/src/SpanLengthEstimator.cpp
+++ moses/src/SpanLengthEstimator.cpp
@@ -1,6 +1,7 @@
 #include "SpanLengthEstimator.h"
 #include <vector>
 #include <map>
+#include "util/check.hh"
 
 using namespace std;
 namespace Moses
@@ -21,15 +22,35 @@ void SpanLengthEstimator::AddTargetSpanProbas(map<unsigned,float> targetProbas)
 float SpanLengthEstimator::GetSourceLengthProbas(unsigned nonTerminal, unsigned spanLength) const
 {
   map<unsigned,float>::const_iterator it;
+  if(m_sourceScores.empty())
+  {
+      return 0.0;
+  }
+  CHECK(! (nonTerminal > m_sourceScores.size()) );
   it=m_sourceScores[nonTerminal].find(spanLength);
-  return (*it).second;
+  if(it!=m_sourceScores[nonTerminal].end())
+    {return (*it).second;}
+    else
+    {
+      return -1000.0;
+    }
 }
 
 float SpanLengthEstimator::GetTargetLengthProbas(unsigned nonTerminal, unsigned spanLength) const
 {
-     map<unsigned,float>::const_iterator it;
-    it=m_targetScores[nonTerminal].find(spanLength);
-    return (*it).second;
+     CHECK(m_targetScores.size() == 0);
+     /*map<unsigned,float>::const_iterator it;
+     if(m_targetScores.empty())
+    {
+        return 0.0;
+    }
+    if(it!=m_targetScores[nonTerminal].end())
+    {return (*it).second;}
+    else
+    {
+      return -1000.0;
+    }*/
+    return 0.0;
 }
 
 }
