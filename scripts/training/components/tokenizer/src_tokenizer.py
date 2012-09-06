6fe58d4a2506987a1a0a0668c22d1a962d978634
jiejiang <jie.jiang@appliedlanguage.com>
2012-09-06 16:55:28 +0100

    src and trg tokenizer

---
+++ scripts/training/components/tokenizer/src_tokenizer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from tokenizer import Tokenizer
+
+from pypeline.helpers.helpers import cons_function_component
+
+def configure(args):
+    result = {}
+    result['src_lang'] = args['src_lang']
+    result['src_tokenisation_dir'] = args['src_tokenisation_dir']
+    return result
+
+def initialise(config):
+
+    def process(a, s):
+        infilename = a['src_filename']
+        outfilename = Tokenizer.batch_tokenise(config['src_lang'], infilename, config['src_tokenisation_dir'])
+        return {'tokenised_src_filename':outfilename}
+
+    return cons_function_component(process)
+
+if __name__ == '__main__':
+
+    def __test():
+        configuration = {'src_lang':'de',
+                         'src_tokenisation_dir':'tmptok'}
+        values = {'src_filename':'tmp.de'}
+        from pypeline.helpers.helpers import run_pipeline
+        box_config = configure(configuration)
+        box = initialise(configuration)
+        print run_pipeline(box, values, None)
+
+    #do some test
+    __test()
+
---
+++ scripts/training/components/tokenizer/trg_tokenizer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from tokenizer import Tokenizer
+
+from pypeline.helpers.helpers import cons_function_component
+
+def configure(args):
+    result = {}
+    result['trg_lang'] = args['trg_lang']
+    result['trg_tokenisation_dir'] = args['trg_tokenisation_dir']
+    return result
+
+def initialise(config):
+
+    def process(a, s):
+        infilename = a['trg_filename']
+        outfilename = Tokenizer.batch_tokenise(config['trg_lang'], infilename, config['trg_tokenisation_dir'])
+        return {'tokenised_trg_filename':outfilename}
+
+    return cons_function_component(process)
+
+if __name__ == '__main__':
+
+    def __test():
+        configuration = {'trg_lang':'de',
+                         'trg_tokenisation_dir':'tmptoktrg'}
+        values = {'trg_filename':'tmp.de'}
+        from pypeline.helpers.helpers import run_pipeline
+        box_config = configure(configuration)
+        box = initialise(configuration)
+        print run_pipeline(box, values, None)
+
+    #do some test
+    __test()
+
