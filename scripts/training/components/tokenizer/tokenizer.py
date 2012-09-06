51088dcabda5c86a3da4905c5172c20a3707afb5
jiejiang <jie.jiang@appliedlanguage.com>
2012-09-06 17:51:54 +0100

    use original tokenizer in the repo

--- scripts/training/components/tokenizer/tokenizer.py
+++ scripts/training/components/tokenizer/tokenizer.py
@@ -5,20 +5,21 @@ import sys, os, subprocess
 class Tokenizer:
     
     @staticmethod
-    def batch_tokenise(lang, infilename, workdir):
+    def batch_tokenise(lang, mosesdir, infilename, workdir):
         if not os.path.exists(workdir):
             os.makedirs(workdir)
-        tok = Tokenizer(lang)
+        tok = Tokenizer(lang, mosesdir)
         basefilename = os.path.basename(infilename)
         outfilename = workdir + os.sep + basefilename + '.tok'
         tok.file_tokenise(infilename, outfilename)
         return outfilename
         
-    def __init__(self, lang):
+    def __init__(self, lang, mosesdir):
         self.arrows = None
         self.lang = lang
         #check the perl tokenizer is here
-        path = os.path.dirname(os.path.abspath(__file__))
+        #path = os.path.dirname(os.path.abspath(__file__))
+        path = mosesdir + os.sep + 'scripts' + os.sep + 'tokenizer'
         self.perltok = path + os.sep + 'tokenizer.perl'
         if not os.path.exists(path):
             raise Exception, "Perl tokenizer does not exists"
