9b28db8dab0eaee073656c121fd39cd5ac4f8ea8
Ian Johnson <ianj@wgrids.com>
2012-09-07 14:21:14 +0100

    Re-factored the cleanup module

--- scripts/training/components/cleanup/cleanup.py
+++ scripts/training/components/cleanup/cleanup.py
@@ -35,7 +35,7 @@ def initialise(config):
       print "Cleanup: Cleaning [%s] and [%s]..." % (input_src_filename, input_trg_filename)
 
       ifh1 = open(input_src_filename, "r")
-      ifh2 = open(input_trg_filename, "r")  
+      ifh2 = open(input_trg_filename, "r")
 
       cleaned_src_filename = _make_cleaned_filename(input_src_filename)
       cleaned_trg_filename = _make_cleaned_filename(input_trg_filename)
@@ -60,25 +60,31 @@ def initialise(config):
 
 if __name__ == '__main__':
   import os
+  import tempfile
   import training.components.shared.test as thelp
 
+
   def _test_main():
-    configuration = {
-      'segment_length_limit': 20,
+    configuration = {'segment_length_limit': 20}
+
+    src_filename = tempfile.mkstemp(suffix = "src", dir = "/tmp")
+    trg_filename = tempfile.mkstemp(suffix = "trg", dir = "/tmp")
+
+    box_eval = {
+      'src_filename': src_filename[1],
+      'trg_filename': trg_filename[1],
+      'cleaned_src_filename': src_filename[1] + ".clean",
+      'cleaned_trg_filename': trg_filename[1] + ".clean",
+      'cleaned_src_file_expected': src_filename[1] + ".expected",
+      'cleaned_trg_file_expected': trg_filename[1] + ".expected"
     }
 
-    box_eval = { 
-      'tokenised_src_file': '/tmp/_cleanup_test_src_input',
-      'tokenised_trg_file': '/tmp/_cleanup_test_trg_input',
-      'cleaned_src_file': '/tmp/_cleanup_test_src_cleaned',
-      'cleaned_trg_file': '/tmp/_cleanup_test_trg_cleaned',
-      'cleaned_src_file_expected': '/tmp/_cleanup_test_src_expected',
-      'cleaned_trg_file_expected': '/tmp/_cleanup_test_trg_expected',
-    }
+    try:
+      _prep_files(box_eval)
+      _run_test(configuration, box_eval)
+    finally:
+      _cleanup_files(box_eval)
 
-    _prep_files(box_eval)
-    _run_test(configuration, box_eval)
-    _cleanup_files(box_eval)
 
   def _run_test(configuration, box_eval):
     from pypeline.helpers.helpers import run_pipeline
@@ -86,8 +92,8 @@ if __name__ == '__main__':
     box = initialise(box_config)
     
     run_pipeline(box, box_eval, box_config)
-    thelp.diff(box_eval['cleaned_src_file_expected'], box_eval['cleaned_src_file'])
-    thelp.diff(box_eval['cleaned_trg_file_expected'], box_eval['cleaned_trg_file'])
+    thelp.diff(box_eval['cleaned_src_file_expected'], box_eval['cleaned_src_filename'])
+    thelp.diff(box_eval['cleaned_trg_file_expected'], box_eval['cleaned_trg_filename'])
 
 
   def _line(line_lengths):
@@ -95,12 +101,14 @@ if __name__ == '__main__':
       return " ".join(map(lambda n: "tok" + str(n), range(tokens)))
     return map(_gen_line, line_lengths)
 
+
   def _prep_files(box_eval):
-    thelp.cat(box_eval['tokenised_src_file'], _line([10, 20, 30, 40, 17, 21]))
-    thelp.cat(box_eval['tokenised_trg_file'], _line([40, 30, 20, 10, 20, 21]))
+    thelp.cat(box_eval['src_filename'], _line([10, 20, 30, 40, 17, 21]))
+    thelp.cat(box_eval['trg_filename'], _line([40, 30, 20, 10, 20, 21]))
     #expected output:
     thelp.cat(box_eval['cleaned_src_file_expected'], _line([17]))
     thelp.cat(box_eval['cleaned_trg_file_expected'], _line([20]))
+
 
   def _cleanup_files(box_eval):
     try:
@@ -108,6 +116,7 @@ if __name__ == '__main__':
         os.unlink(filename)
     except:
       pass
+
 
   _test_main()
 
