266a81c9cf00627c396b7158866fc5f4757775dd
Hieu Hoang <hieu@hoang.co.uk>
2013-05-28 17:25:59 +0100

    add class InputFeature. To replace 7 year old hack of using phrase table weights for input scores for confusion networks and lattice inputs

---
+++ moses/FF/InputFeature.cpp
@@ -0,0 +1,40 @@
+#include <stdexcept>
+
+#include "InputFeature.h"
+
+using namespace std;
+
+namespace Moses
+{
+InputFeature::InputFeature(const std::string &line)
+:StatelessFeatureFunction("InputFeature", line)
+{
+
+}
+
+const InputFeature &InputFeature::GetInputFeature()
+{
+  static const InputFeature *staticObj = NULL;
+
+  if (staticObj) {
+	return *staticObj;
+  }
+
+  // 1st time looking up the feature
+  const std::vector<const StatelessFeatureFunction*> &statefulFFs = StatelessFeatureFunction::GetStatelessFeatureFunctions();
+  for (size_t i = 0; i < statefulFFs.size(); ++i) {
+	const StatelessFeatureFunction *ff = statefulFFs[i];
+	const InputFeature *lm = dynamic_cast<const InputFeature*>(ff);
+
+	if (lm) {
+		staticObj = lm;
+	  return *staticObj;
+	}
+  }
+
+  throw std::logic_error("No input feature.");
+
+}
+
+}
+
