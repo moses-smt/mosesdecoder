b9f54b195aa780a482bcd9d3667140428f9aebc7
Hieu Hoang <hieu@hoang.co.uk>
2013-06-05 13:42:56 +0100

    implement GenerationDictionary.Load()

--- moses/FF/InputFeature.cpp
+++ moses/FF/InputFeature.cpp
@@ -11,16 +11,16 @@ InputFeature::InputFeature(const std::string &line)
   :StatelessFeatureFunction("InputFeature", line)
 {
   for (size_t i = 0; i < m_args.size(); ++i) {
-	const vector<string> &args = m_args[i];
-	CHECK(args.size() == 2);
+    const vector<string> &args = m_args[i];
+    CHECK(args.size() == 2);
 
-	if (args[0] == "num-input-features") {
-	  m_numInputScores = Scan<size_t>(args[1]);
-	} else if (args[0] == "real-word-count") {
-	  m_numRealWordCount = Scan<size_t>(args[1]);
-	} else {
-	  throw "Unknown argument " + args[0];
-	}
+    if (args[0] == "num-input-features") {
+      m_numInputScores = Scan<size_t>(args[1]);
+    } else if (args[0] == "real-word-count") {
+      m_numRealWordCount = Scan<size_t>(args[1]);
+    } else {
+      throw "Unknown argument " + args[0];
+    }
   }
 
 }
