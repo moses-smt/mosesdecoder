241432b47aac4829743737bdb5d589eebb5c57f5
Kenneth Heafield <github@kheafield.com>
2013-06-05 09:51:00 +0100

    Missing header

--- moses/TranslationOptionCollection.cpp
+++ moses/TranslationOptionCollection.cpp
@@ -21,6 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ***********************************************************************/
 
 #include <algorithm>
+#include <typeinfo>
 #include "TranslationOptionCollection.h"
 #include "Sentence.h"
 #include "DecodeStep.h"
