2c19238c2401a851f1054b512a34f5239f3cd085
Wilker Aziz <wilker.aziz@gmail.com>
2013-06-24 15:38:10 +0100

    Patching up the suffix array wrappers

--- scripts/training/wrappers/adam-suffix-array/suffix-array-extract.sh
+++ scripts/training/wrappers/adam-suffix-array/suffix-array-extract.sh
@@ -12,14 +12,14 @@ SA_EXEC_DIR=$1
 MODEL_DIR=$2
 INPUT_FILE=$3
 OUTPUT_DIR=$4
+JOBS=$5
 
 mkdir $OUTPUT_DIR
 
 pushd .
 cd $OUTPUT_DIR
 
-cat $INPUT_FILE | $SA_EXEC_DIR/escape-testset.pl | $SA_EXEC_DIR/extractor.py -c  $MODEL_DIR/extract.ini
-gzip $OUTPUT_DIR/grammar.out.*
+cat $INPUT_FILE | python $SA_EXEC_DIR/cdec/sa/extract.py -c $MODEL_DIR/extract.ini -g $OUTPUT_DIR -j $JOBS -z > $OUTPUT_DIR/input.sgm
 
 popd
 
