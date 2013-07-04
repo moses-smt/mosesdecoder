d2bc6a2584f18c7037bea65df8566e21ee53c190
Nadir Durrani <nadir@hel.inf.ed.ac.uk>
2013-07-04 19:58:19 +0100

    In EMS

--- scripts/OSM/OSM-Train.sh
+++ scripts/OSM/OSM-Train.sh
@@ -1,34 +1,33 @@
 #!/bin/sh
 
-PATH=$PATH:/fs/hel1/nadir/SRILM/bin/i686-m64/
-
 echo 'Training OSM - Start'
 date
+
+mkdir $5
+ln -s $1 $5/e
+ln -s $2 $5/f
+
+$6/scripts/OSM/flipAlignment $3 > $5/align
+
+echo 'Extracting Singletons'
+
+$6/scripts/OSM/extract-singletons.perl $5/e $5/f $5/align > $5/Singletons
+
+echo 'Converting Bilingual Sentence Pair into Operation Corpus'
+
+$6/scripts/OSM/generateSequences $5/e $5/f $5/align $5/Singletons > $5/opCorpus			# Generates Operation Corpus
+
+echo 'Learning Operation Sequence Translation Model'
+
+ngram-count -kndiscount -order $4 -unk -text $5/opCorpus -lm $5/operationLM
+
+echo 'Binarizing'
+
+$6/bin/build_binary $5/operationLM$4 $5/operationLM.bin
 
 \rm $5/e
 \rm $5/f
 \rm $5/align
-
-ln -s $1 $5/e
-ln -s $2 $5/f
-
-./flipAlignment $3 > $5/align
-
-echo 'Extracting Singletons'
-
-./extract-singletons.perl $5/e $5/f $5/align > $5/Singletons
-
-echo 'Converting Bilingual Sentence Pair into Operation Corpus'
-
-./generateSequences $5/e $5/f $5/align $5/Singletons > $5/opCorpus			# Generates Operation Corpus
-
-echo 'Learning Operation Sequence Translation Model'
-
-ngram-count -kndiscount -order $4 -unk -text $5/opCorpus -lm $5/operationLM$4
-
-echo 'Binarizing'
-
-../../bin/build_binary $5/operationLM$4 $5/operationLM$4.bin
 
 echo 'Training OSM - End'
 date
