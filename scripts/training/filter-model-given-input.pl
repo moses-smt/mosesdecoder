efd9789577e901b802178d7a46b2feadab003d1f
Hieu Hoang <hieuhoang@gmail.com>
2013-07-26 15:06:07 +0100

    update filtering script to correct binarize pb models with CreateOnDiskPt

--- scripts/training/filter-model-given-input.pl
+++ scripts/training/filter-model-given-input.pl
@@ -373,6 +373,10 @@ for(my $i=0;$i<=$#TABLE;$i++) {
           my $cmd = "$catcmd $mid_file | LC_ALL=C sort -T $dir > $mid_file.sorted; $binarizer -in $mid_file.sorted -out $new_file -nscores $TABLE_WEIGHTS[$i]; rm $mid_file.sorted";
           print STDERR $cmd."\n";
           print STDERR `$cmd`;
+        elsif ($binarizer =~ /CreateOnDiskPt/) {
+	  my $cmd = "$binarizer $mid_file $new_file.bin";
+          print STDERR $cmd."\n";
+          print STDERR `$cmd`;
         } else { 
           my $cmd = "$catcmd $mid_file | LC_ALL=C sort -T $dir | $binarizer -ttable 0 0 - -nscores $TABLE_WEIGHTS[$i] -out $new_file";
           print STDERR $cmd."\n";
