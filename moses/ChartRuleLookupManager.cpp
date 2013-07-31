f13bcf74cfc130c2cb54f8f0734166abdf62e368
Hieu Hoang <hieu@hoang.co.uk>
2013-07-31 16:27:08 +0100

    begin cleanup of SCFG parsing. Get rid of Sentence variable in ChartRuleLookupManager, replace with ChartParser variable

---
+++ moses/ChartRuleLookupManager.cpp
@@ -0,0 +1,12 @@
+#include "ChartRuleLookupManager.h"
+#include "ChartParser.h"
+
+namespace Moses
+{
+const Sentence &ChartRuleLookupManager::GetSentence() const
+{
+  return m_parser.GetSentence();
+}
+
+}  // namespace Moses
+
