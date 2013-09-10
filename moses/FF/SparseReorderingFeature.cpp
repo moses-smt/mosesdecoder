f7c53fef552fb1c14047dd0577ad9ade8da5acd0
Barry Haddow <barry.haddow@gmail.com>
2013-09-10 11:20:14 +0100

    Set dense feature count to 0

--- moses/FF/SparseReorderingFeature.cpp
+++ moses/FF/SparseReorderingFeature.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "moses/ChartHypothesis.h"
+
 #include "SparseReorderingFeature.h"
 
 using namespace std;
@@ -8,16 +10,20 @@ namespace Moses
 {
 
 SparseReorderingFeature::SparseReorderingFeature(const std::string &line)
-  :StatefulFeatureFunction("StatefulFeatureFunction", line)
+  :StatefulFeatureFunction("StatefulFeatureFunction",0, line)
 {
   cerr << "Constructing a Sparse Reordering feature" << endl;
 }
 
 FFState* SparseReorderingFeature::EvaluateChart(
-  const ChartHypothesis& /* cur_hypo */,
-  int /* featureID - used to index the state in the previous hypotheses */,
+  const ChartHypothesis&  cur_hypo ,
+  int  featureID /*- used to index the state in the previous hypotheses */,
   ScoreComponentCollection* accumulator) const
 {
+  // get index map for underlying hypotheses
+  const AlignmentInfo::NonTermIndexMap &nonTermIndexMap =
+    cur_hypo.GetCurrTargetPhrase().GetAlignNonTerm().GetNonTermIndexMap();
+
   return new SparseReorderingState();
 }
 
