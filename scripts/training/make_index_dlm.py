ba3590598f9dc2e4b9d56f998abf0bb7fa962014
Ales Tamchyna <tamchyna@ufal.mff.cuni.cz>
2013-09-13 20:57:34 +0200

    sergey's fix

--- scripts/training/make_index_dlm.py
+++ scripts/training/make_index_dlm.py
@@ -45,21 +45,7 @@ import tempfile
 
 # ---------------------------------------------------------------------- Keys --
 
-def cept_to_lookup_keys(cept):
-    return [cept]
-
-def target_to_lemma_key(target_word):
-    return target_word
-
 def prune_group(group, num_top_lemmas=None):
-    """
-    Leave only significant lemmas in group. 
-    
-    'group' is a list of [(freq, source_key, target_lemma)], where freq is 
-    number of times source word was aligned to target lemma.
-
-    Currently, uses stupid top-k pruning.
-    """
     if num_top_lemmas is None:
         return group
 
@@ -78,53 +64,14 @@ def parse_cept_and_target(input_line):
     return cept, target
 
 def make_indices(): 
-    """
-    Read stdin and return a pair of (SOURCE_INDEX, INFLECTIONS_INDEX).
-
-    SOURCE_INDEX will be a file-like object (in text mode), that contains 
-    following entries (one per line):
-
-        [source_key]\t[target_lemma]
-
-    SOURCE_INDEX will be stored in on-disk temporary file.
-
-    INFLECTIONS_INDEX will be in-memory dict, that can be used to restore 
-    inflections from target word lemmas:
-    
-        >>> # Contains set of target words (with frequencies), corresponding to 
-        >>> # given lemma
-        >>> INFLECTIONS_INDEX[target_lemma]
-        {
-            "target_1": 10,
-            "target_2": 1,
-            "target_3": 23,
-        }
-
-    """
-    def make_frequency_counter():
-        return collections.defaultdict(lambda: 0)
-
     SOURCE_INDEX = tempfile.NamedTemporaryFile(mode="w+t")
-    INFLECTIONS_INDEX = collections.defaultdict(make_frequency_counter)
 
     for line in fileinput.input("-"):
         cept, target = parse_cept_and_target(line)
-
-        lookup_keys = cept_to_lookup_keys(cept)
-        lemma_key = target_to_lemma_key(target)
-
-        for key in lookup_keys:
-            SOURCE_INDEX.write("%s\t%s\n" % (key, lemma_key))
-            INFLECTIONS_INDEX[lemma_key][target] += 1
+        SOURCE_INDEX.write("%s\t%s\n" % (cept, target))
 
     SOURCE_INDEX.seek(0)
-    return (SOURCE_INDEX, INFLECTIONS_INDEX)
-
-def merge_inflections(to, from_):
-    RESULT = copy.copy(to)
-    for (inflection, freq) in from_.items():
-        RESULT[inflection] += freq
-    return RESULT
+    return SOURCE_INDEX
 
 # ------------------------------------------------- Index sorting and pruning --
 
@@ -147,32 +94,27 @@ def parse_index(file):
     for line in file:
         line = line.decode("utf-8")
 
-        freq_source, target_lemma = line.strip().split("\t")
+        freq_source, target = line.strip().split("\t")
         freq, source = freq_source.split(" ", 1)
         freq = int(freq)
 
-        yield (freq, source, target_lemma)
+        yield (freq, source, target)
 
-def prune_and_expand(source_index, inflections_index, num_top_lemmas=None):
+def prune(source_index, num_top_lemmas=None):
     get_source = lambda entry: entry[1]
 
     for source, group in itertools.groupby(source_index, key=get_source):
         group = prune_group(list(group), num_top_lemmas)
+        yield source, [(target, freq) for (freq, _, target) in group]
 
-        lemmas = list(entry[2] for entry in group)
-        inflection_sets = list(inflections_index[l] for l in lemmas)
-        target_inflections = functools.reduce(merge_inflections, inflection_sets)
-
-        yield source, target_inflections
-
-def inflections_expander(inflections_index, num_top_lemmas):
+def pruner(num_top_lemmas):
     def RESULT(source_index):
-        return prune_and_expand(source_index, inflections_index, num_top_lemmas)
+        return prune(source_index, num_top_lemmas)
     return RESULT
 
 def print_index(source_index):
-    for source, target_inflections in source_index:
-        for (text, freq) in target_inflections.items():
+    for source, targets in source_index:
+        for (text, freq) in targets:
             print("%s ||| %s ||| %i" % (source, text, freq))
 
 # ---------------------------------------------------------------------- Main --
@@ -182,13 +124,13 @@ def main():
     parser.add_option("--leave-top-lemmas", type=int)
     args, _ = parser.parse_args()
     
-    source_index, inflections_index = make_indices()
+    source_index = make_indices()
     
     fns = [
         sort_file, 
         append_frequencies, 
         parse_index,
-        inflections_expander(inflections_index, args.leave_top_lemmas),
+        pruner(args.leave_top_lemmas),
         print_index
     ]
 
