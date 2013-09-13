a8358ce60e7f0152f8f2b6d35b057a64354b4c7d
Ales Tamchyna <tamchyna@ufal.mff.cuni.cz>
2013-09-13 19:49:38 +0200

    different escaping for VW training data

--- scripts/training/make_index_dlm.py
+++ scripts/training/make_index_dlm.py
@@ -49,7 +49,7 @@ def cept_to_lookup_keys(cept):
     return [cept]
 
 def target_to_lemma_key(target_word):
-    return target_word.split("|")[1]
+    return target_word
 
 def prune_group(group, num_top_lemmas=None):
     """
