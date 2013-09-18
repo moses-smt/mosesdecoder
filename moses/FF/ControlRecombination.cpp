89fe7d3db29bff7ec750daa783a6100c20ea8976
Hieu Hoang <hieuhoang@gmail.com>
2013-09-18 16:27:10 +0200

    feature functions to only allow recombination for the same output

--- moses/FF/ControlRecombination.cpp
+++ moses/FF/ControlRecombination.cpp
@@ -1,70 +1,63 @@
 #include "ControlRecombination.h"
 #include "moses/Hypothesis.h"
+#include "moses/Manager.h"
+#include "moses/ChartHypothesis.h"
+#include "moses/ChartManager.h"
+#include "moses/StaticData.h"
+#include "moses/InputFileStream.h"
+#include "moses/Util.h"
 #include "util/exception.hh"
 
 using namespace std;
 
 namespace Moses
 {
+ControlRecombinationState::ControlRecombinationState(const Hypothesis &hypo)
+{
+	hypo.GetOutputPhrase(m_outputPhrase);
+}
 
-ControlRecombination::ControlRecombination(const std::string &line)
-  :StatefulFeatureFunction("ControlRecombination", 0, line)
-  ,m_type(Output)
+ControlRecombinationState::ControlRecombinationState(const ChartHypothesis &hypo)
+{
+	hypo.GetOutputPhrase(m_outputPhrase);
+}
+
+int ControlRecombinationState::Compare(const FFState& other) const
+{
+	const ControlRecombinationState &otherFF = static_cast<const ControlRecombinationState&>(other);
+	bool ret = 	m_outputPhrase.Compare(otherFF.m_outputPhrase);
+	return ret;
+}
+
+//////////////////////////////////////////////////////////////////
+void ControlRecombination::Load()
+{
+}
+
+std::vector<float> ControlRecombination::DefaultWeights() const
+{
+	CHECK(m_numScoreComponents == 1);
+	vector<float> ret(1, 1);
+	return ret;
+}
+
+FFState* ControlRecombination::Evaluate(
+  const Hypothesis& hypo,
+  const FFState* prev_state,
+  ScoreComponentCollection* accumulator) const
+{
+}
+
+FFState* ControlRecombination::EvaluateChart(
+  const ChartHypothesis &hypo,
+  int /* featureID - used to index the state in the previous hypotheses */,
+  ScoreComponentCollection* accumulator) const
 {
 }
 
 void ControlRecombination::SetParameter(const std::string& key, const std::string& value)
 {
-  if (key == "type") {
-    m_type = (Type) Scan<size_t>(value);
-  } else {
-    StatefulFeatureFunction::SetParameter(key, value);
-  }
-}
-
-FFState* ControlRecombination::Evaluate(
-  const Hypothesis& cur_hypo,
-  const FFState* prev_state,
-  ScoreComponentCollection* accumulator) const
-{
-  ControlRecombinationState *state = new ControlRecombinationState(&cur_hypo);
-  return state;
-}
-
-FFState* ControlRecombination::EvaluateChart(
-  const ChartHypothesis& /* cur_hypo */,
-  int /* featureID - used to index the state in the previous hypotheses */,
-  ScoreComponentCollection* accumulator) const
-{
-  UTIL_THROW(util::Exception, "Not implemented yet");
-}
-
-const FFState* ControlRecombination::EmptyHypothesisState(const InputType &input) const
-{
-  ControlRecombinationState *state = new ControlRecombinationState();
-}
-
-ControlRecombinationState::ControlRecombinationState()
-  :m_hypo(NULL)
-{
-}
-
-ControlRecombinationState::ControlRecombinationState(const Hypothesis *hypo)
-  :m_hypo(hypo)
-{
-}
-
-int ControlRecombinationState::Compare(const FFState& other) const
-{
-  const ControlRecombinationState &other2 = static_cast<const ControlRecombinationState&>(other);
-  const Hypothesis *otherHypo = other2.m_hypo;
-
-  Phrase thisOutputPhrase, otherOutputPhrase;
-  m_hypo->GetOutputPhrase(thisOutputPhrase);
-  otherHypo->GetOutputPhrase(otherOutputPhrase);
-
-  int ret = thisOutputPhrase.Compare(otherOutputPhrase);
-  return ret;
 }
 
 }
+
