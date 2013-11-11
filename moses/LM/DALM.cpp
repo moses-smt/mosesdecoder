d5cde1d874cf9655bdcdc3961d7693aef4b56084
Hieu Hoang <hieuhoang@gmail.com>
2013-11-11 14:39:53 +0000

    DALM load

--- moses/LM/DALM.cpp
+++ moses/LM/DALM.cpp
@@ -1,11 +1,45 @@
 
 #include "DALM.h"
 #include "moses/FactorCollection.h"
+#include<logger.h>
+#include<vocabulary.h>
+#include<lm.h>
 
 using namespace std;
 
+
+
 namespace Moses
 {
+/////////////////////////
+void push(DALM::VocabId *ngram, size_t n, DALM::VocabId wid){
+	for(size_t i = n-1; i+1 >= 1 ; i--){
+		ngram[i] = ngram[i-1];
+	}
+	ngram[0] = wid;
+}
+
+void read_ini(const char *inifile, string &model, string &words){
+	ifstream ifs(inifile);
+	string line;
+
+	getline(ifs, line);
+	while(ifs){
+		unsigned int pos = line.find("=");
+		string key = line.substr(0, pos);
+		string value = line.substr(pos+1, line.size()-pos);
+		if(key=="MODEL"){
+			model = value;
+		}else if(key=="WORDS"){
+			words = value;
+		}
+		getline(ifs, line);
+	}
+}
+
+/////////////////////////
+
+
 LanguageModelDALM::LanguageModelDALM(const std::string &line)
   :LanguageModelSingleFactor(line)
 {
@@ -27,6 +61,25 @@ LanguageModelDALM::LanguageModelDALM(const std::string &line)
 
 LanguageModelDALM::~LanguageModelDALM()
 {
+}
+
+void LanguageModelDALM::Load()
+{
+	/////////////////////
+	// READING INIFILE //
+	/////////////////////
+	string model; // Path to the double-array file.
+	string words; // Path to the vocabulary file.
+	read_ini(m_filePath.c_str(), model, words);
+
+	////////////////
+	// LOADING LM //
+	////////////////
+
+	// Preparing a logger object.
+	DALM::Logger logger(stderr);
+	logger.setLevel(DALM::LOGGER_INFO);
+
 }
 
 LMResult LanguageModelDALM::GetValue(const vector<const Word*> &contextFactor, State* finalState) const
--- moses/LM/DALM.h
+++ moses/LM/DALM.h
@@ -10,10 +10,12 @@ namespace Moses
 class LanguageModelDALM : public LanguageModelSingleFactor
 {
 protected:
+	std::string m_inifile;
 
 public:
 	LanguageModelDALM(const std::string &line);
   ~LanguageModelDALM();
+  void Load();
 
   virtual LMResult GetValue(const std::vector<const Word*> &contextFactor, State* finalState = 0) const;
 };
