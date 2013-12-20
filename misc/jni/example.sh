1bc7d7db24c36c132b071fbbb5b9a6a2dec753ef
Marcin Junczys-Dowmunt <junczys@wipo.int>
2013-12-20 16:17:24 +0100

    Added swig files

--- misc/jni/example.sh
+++ misc/jni/example.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SD=`dirname $0`
 export LD_LIBRARY_PATH=$SD/../../lib
-java -Djava.library.path=$LD_LIBRARY_PATH -cp $SD example $1
+java -Djava.library.path=$LD_LIBRARY_PATH -cp $SD/classes example $1
