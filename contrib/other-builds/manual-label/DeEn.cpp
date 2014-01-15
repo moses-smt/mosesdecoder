9128c4d3b49429b1e9397e9c19b62c8f2f2c4581
Hieu Hoang <s0565741@syn.inf.ed.ac.uk>
2014-01-15 10:30:31 +0000

    Manual source labelling for de-en

---
+++ contrib/other-builds/manual-label/DeEn.cpp
@@ -0,0 +1,62 @@
+#include <list>
+#include "DeEn.h"
+#include "moses/Util.h"
+
+using namespace std;
+
+extern bool g_debug;
+
+bool IsA(const Phrase &source, int pos, int offset, int factor, const string &str)
+{
+  pos += offset;
+  if (pos >= source.size() || pos < 0) {
+    return false;
+  }
+
+  const string &word = source[pos][factor];
+  vector<string> soughts = Moses::Tokenize(str, " ");
+  for (int i = 0; i < soughts.size(); ++i) {
+    string &sought = soughts[i];
+    bool found = (word == sought);
+    if (found) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool Contains(const Phrase &source, int start, int end, int factor, const string &str)
+{
+  for (int pos = start; pos <= end; ++pos) {
+    bool found = IsA(source, pos, 0, factor, str);
+    if (found) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void LabelDeEn(const Phrase &source, ostream &out)
+{
+  list<pair<int,int> > ranges;
+
+  for (int start = 0; start < source.size(); ++start) {
+    for (int end = start; end < source.size(); ++end) {
+     if (IsA(source, start, -1, 1, "VAFIN")
+          && IsA(source, end, +1, 1, "VVINF VVPP")
+          && !Contains(source, start, end, 1, "VAFIN VVINF VVPP VVFIN")) {
+       cerr << start << " " << end << endl;
+       //ranges.push_back(pair<int,int>(start, end);
+      }
+      else if ((start == 0 || IsA(source, start, -1, 1, "$,"))
+          && IsA(source, end, +1, 0, "zu")
+          && IsA(source, end, +2, 1, "VVINF")
+          && !Contains(source, start, end, 1, "$,")) {
+        cerr << start << " " << end << endl;
+        //ranges.push_back(pair<int,int>(start, end);
+      }
+    }
+  }
+
+
+}
