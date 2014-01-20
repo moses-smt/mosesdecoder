8fe07d1c9c1af7a836f492badb5b37834982e2e4
Hieu Hoang <hieu@hoang.co.uk>
2014-01-20 17:01:53 +0000

    oops. Minor compile error

--- moses/FF/HyperParameterAsWeight.h
+++ moses/FF/HyperParameterAsWeight.h
@@ -14,10 +14,20 @@ class HyperParameterAsWeight : public StatelessFeatureFunction
 public:
   HyperParameterAsWeight(const std::string &line);
 
+  virtual bool IsUseable(const FactorMask &mask) const
+  { return true; }
+
   virtual void Evaluate(const Phrase &source
                         , const TargetPhrase &targetPhrase
                         , ScoreComponentCollection &scoreBreakdown
                         , ScoreComponentCollection &estimatedFutureScore) const
+  {}
+
+  virtual void Evaluate(const InputType &input
+                        , const InputPath &inputPath
+                        , const TargetPhrase &targetPhrase
+                        , ScoreComponentCollection &scoreBreakdown
+                        , ScoreComponentCollection *estimatedFutureScore = NULL) const
   {}
 
   virtual void Evaluate(const Hypothesis& hypo,
