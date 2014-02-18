643709ace1d8c9e21c5e1b5ddd14de67ee54502b
Hieu Hoang <hieuhoang@gmail.com>
2014-02-18 17:17:07 +0000

    create lattice

---
+++ contrib/other-builds/extract-mixed-syntax/LatticeArc.h
@@ -0,0 +1,20 @@
+/*
+ * LatticeArc.h
+ *
+ *  Created on: 18 Feb 2014
+ *      Author: s0565741
+ */
+#pragma once
+
+#include <string>
+
+class LatticeArc {
+public:
+	LatticeArc();
+	virtual ~LatticeArc();
+
+	virtual bool IsNonTerm() const = 0;
+	virtual const std::string &GetString() const = 0;
+
+};
+
