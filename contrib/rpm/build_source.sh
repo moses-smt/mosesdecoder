58bc0fd854593807047cbe25e872f3041702af54
Ian Johnson <ian.johnson@appliedlanguage.com>
2014-03-03 16:50:36 +0000

    RPM installers now support updating and release numbers on SPEC and SOURCE creation.

--- contrib/rpm/build_source.sh
+++ contrib/rpm/build_source.sh
@@ -3,11 +3,13 @@
 BRANCH="master"
 BOOST="/usr"
 declare -i NO_RPM_BUILD=0
+declare -i RELEASE=1
 declare -r RPM_VERSION_TAG="___RPM_VERSION__"
+declare -r RPM_RELEASE_TAG="___RPM_RELEASE__"
 declare -r BOOST_TAG="___BOOST_LOCATION__"
 
 function usage() {
-  echo "`basename $0` -r [Moses Git repo] -b [Moses Git branch: default ${BRANCH}] -v [RPM version] -t [Boost install: default ${BOOST}]"
+  echo "`basename $0` -r [Moses Git repo] -b [Moses Git branch: default ${BRANCH}] -v [RPM version] -l [RPM release: default ${RELEASE}] -t [Boost install: default ${BOOST}]"
   exit 1
 }
 
@@ -15,13 +17,14 @@ if [ $# -lt 4 ]; then
   usage
 fi
 
-while getopts r:b:t:v:nh OPTION
+while getopts r:b:t:v:l:nh OPTION
 do
   case "$OPTION" in
       r) REPO="${OPTARG}";;
       b) BRANCH="${OPTARG}";;
       t) BOOST="${OPTARG}";;
       v) VERSION="${OPTARG}";;
+      l) RELEASE="${OPTARG}";;
       n) NO_RPM_BUILD=1;;
       [h\?]) usage;;
   esac
@@ -57,7 +60,7 @@ if [ ${NO_RPM_BUILD} -eq 0 ]; then
     mkdir -p ${HOME}/rpmbuild/SPECS
   fi
   ESC_BOOST=`echo ${BOOST} | gawk '{gsub(/\//, "\\\\/"); print}'`
-  eval sed -e \"s/${RPM_VERSION_TAG}/${VERSION}/\" -e \"s/${BOOST_TAG}/${ESC_BOOST}/\" ./rpmbuild/SPECS/moses.spec > ${HOME}/rpmbuild/SPECS/moses.spec
+  eval sed -e \"s/${RPM_VERSION_TAG}/${VERSION}/\" -e \"s/${RPM_RELEASE_TAG}/${RELEASE}/\" -e \"s/${BOOST_TAG}/${ESC_BOOST}/\" ./rpmbuild/SPECS/moses.spec > ${HOME}/rpmbuild/SPECS/moses.spec
   if [ ! -d ${HOME}/rpmbuild/SOURCES ]; then
     mkdir -p ${HOME}/rpmbuild/SOURCES
   fi
