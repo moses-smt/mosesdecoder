7380a83f752e2a3c0165f6175b7ac097ac7bd041
Matthias Huck <huck@i6.informatik.rwth-aachen.de>
2014-06-11 20:26:24 +0100

    CountsPhraseProperty: add comment to header

--- moses/PP/CountsPhraseProperty.h
+++ moses/PP/CountsPhraseProperty.h
@@ -8,6 +8,22 @@
 namespace Moses
 {
 
+//    A simple phrase property class to access the three phrase count values.
+//
+//    The counts are usually not needed during decoding and are not loaded
+//    from the phrase table. This is just a workaround that can make them
+//    available to features which have a use for them.
+//
+//    If you need access to the counts, copy the two marginal counts and the
+//    joint count into an additional information property with key "Counts",
+//    e.g. using awk:
+//
+//    $ zcat phrase-table.gz | awk -F' \|\|\| '  '{printf("%s {{Counts %s}}\n",$0,$5);}' | gzip -c > phrase-table.withCountsPP.gz
+//
+//    CountsPhraseProperty reads them from the phrase table and provides
+//    methods GetSourceMarginal(), GetTargetMarginal(), GetJointCount().
+
+
 class CountsPhraseProperty : public PhraseProperty
 {
 public:
