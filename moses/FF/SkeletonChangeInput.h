fbbc756117a318b13033cc9f12a4a84f516cb5bb
Hieu Hoang <hieuhoang@gmail.com>
2014-08-01 22:41:24 +0100

    add  virtual void ChangeSource(InputType *&input) const to FeatureFunction

---
+++ moses/FF/SkeletonChangeInput.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include "StatelessFeatureFunction.h"
+
+namespace Moses
+{
+
+class SkeletonChangeInput : public StatelessFeatureFunction
+{
+public:
+  SkeletonChangeInput(const std::string &line);
+
+  bool IsUseable(const FactorMask &mask) const {
+    return true;
+  }
+
+  void EvaluateInIsolation(const Phrase &source
+                , const TargetPhrase &targetPhrase
+                , ScoreComponentCollection &scoreBreakdown
+                , ScoreComponentCollection &estimatedFutureScore) const;
+
+  void ChangeSource(InputType *&input) const;
+
+  void EvaluateWithSourceContext(const InputType &input
+                , const InputPath &inputPath
+                , const TargetPhrase &targetPhrase
+                , const StackVec *stackVec
+                , ScoreComponentCollection &scoreBreakdown
+                , ScoreComponentCollection *estimatedFutureScore = NULL) const;
+  void EvaluateWhenApplied(const Hypothesis& hypo,
+                ScoreComponentCollection* accumulator) const;
+  void EvaluateWhenApplied(const ChartHypothesis &hypo,
+                     ScoreComponentCollection* accumulator) const;
+
+  void SetParameter(const std::string& key, const std::string& value);
+
+};
+
+}
+
