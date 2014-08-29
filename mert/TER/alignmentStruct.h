be9b3cb1c6875c26f0ef8d1240f4eb0b747fa86b
Christophe SERVAN <cservan@ontario.grenoble.xrce.xerox.com>
2014-08-29 14:46:56 +0200

    Bug fix about the TER calculation

--- mert/TER/alignmentStruct.h
+++ mert/TER/alignmentStruct.h
@@ -1,5 +1,26 @@
-#ifndef MERT_TER_ALIGNMENT_STRUCT_H_
-#define MERT_TER_ALIGNMENT_STRUCT_H_
+/*********************************
+tercpp: an open-source Translation Edit Rate (TER) scorer tool for Machine Translation.
+
+Copyright 2010-2013, Christophe Servan, LIUM, University of Le Mans, France
+Contact: christophe.servan@lium.univ-lemans.fr
+
+The tercpp tool and library are free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published by 
+the Free Software Foundation, either version 3 of the licence, or
+(at your option) any later version.
+
+This program and library are distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this library; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+**********************************/
+#ifndef MERT_TER_ALIGNMENTSTRUCT_H_
+#define MERT_TER_ALIGNMENTSTRUCT_H_
+
 
 #include <vector>
 #include <stdio.h>
@@ -7,15 +28,16 @@
 #include <sstream>
 #include "tools.h"
 
+
 using namespace std;
 using namespace Tools;
 
 namespace TERCpp
 {
-class alignmentStruct
-{
-private:
-public:
+    class alignmentStruct
+    {
+        private:
+        public:
 
 // 	alignmentStruct();
 // 	alignmentStruct (int _start, int _end, int _moveto, int _newloc);
@@ -31,15 +53,14 @@ public:
 //   int end;
 //   int moveto;
 //   int newloc;
-  vector<string> nwords; // The words we shifted
-  vector<char> alignment ; // for pra_more output
-  vector<vecInt> aftershift; // for pra_more output
-  // This is used to store the cost of a shift, so we don't have to
-  // calculate it multiple times.
-  double cost;
-  string toString();
-};
+            vector<string> nwords; // The words we shifted
+            vector<char> alignment ; // for pra_more output
+            vector<vecInt> aftershift; // for pra_more output
+            // This is used to store the cost of a shift, so we don't have to
+            // calculate it multiple times.
+            double cost;
+	    string toString();
+    };
 
 }
-
-#endif  // MERT_TER_ALIGNMENT_STRUCT_H_
+#endif
--- mert/TER/stringHasher.h
+++ mert/TER/stringHasher.h
@@ -1,28 +1,50 @@
-#ifndef MERT_TER_STRING_HASHER_H_
-#define MERT_TER_STRING_HASHER_H_
+/*********************************
+tercpp: an open-source Translation Edit Rate (TER) scorer tool for Machine Translation.
 
+Copyright 2010-2013, Christophe Servan, LIUM, University of Le Mans, France
+Contact: christophe.servan@lium.univ-lemans.fr
+
+The tercpp tool and library are free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published by 
+the Free Software Foundation, either version 3 of the licence, or
+(at your option) any later version.
+
+This program and library are distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this library; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+**********************************/
+#ifndef __STRINGHASHER_H_
+#define __STRINGHASHER_H_
 #include <string>
+//#include <ext/hash_map>
 #include <iostream>
 
 using namespace std;
 namespace HashMapSpace
 {
 
-class stringHasher
-{
-private:
-  long m_hashKey;
-  string m_key;
-  string m_value;
+    class stringHasher
+    {
+        private:
+            long m_hashKey;
+            string m_key;
+            string m_value;
 
-public:
-  stringHasher ( long cle, string cleTxt, string valueTxt );
-  long getHashKey();
-  string getKey();
-  string getValue();
-  void setValue ( string value );
-};
+        public:
+            stringHasher ( long cle, string cleTxt, string valueTxt );
+            long getHashKey();
+            string getKey();
+            string getValue();
+            void setValue ( string value );
+
+
+    };
+
 
 }
-
-#endif  // MERT_TER_STRING_HASHER_H_
+#endif
