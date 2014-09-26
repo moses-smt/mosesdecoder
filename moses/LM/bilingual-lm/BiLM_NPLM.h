3babd6dbb9e508103aa361b3ae2706fa78dfb966
Paul Baltescu <pauldb89@gmail.com>
2014-09-26 16:25:48 +0100

    Set BilingualLM parameters nicely.

--- moses/LM/bilingual-lm/BiLM_NPLM.h
+++ moses/LM/bilingual-lm/BiLM_NPLM.h
@@ -6,23 +6,26 @@ namespace nplm {
 
 namespace Moses {
 
-  class BilingualLM_NPLM : public BilingualLM 
-  {
-  public:
-    BilingualLM_NPLM(const std::string &line);
-    
-  private:
-    nplm::neuralLM *m_neuralLM_shared;
-    mutable boost::thread_specific_ptr<nplm::neuralLM> m_neuralLM;
-    
-    bool premultiply;
-    int neuralLM_cache;
-    
-    float Score(std::vector<int>& source_words, std::vector<int>& target_words) const;
-    int LookUpNeuralLMWord(const std::string& str) const;
-    void initSharedPointer() const;
-    void loadModel();
-    bool parseAdditionalSettings(const std::string& key, const std::string& value) ; //Gives pure virtual error...
+class BilingualLM_NPLM : public BilingualLM {
+ public:
+  BilingualLM_NPLM(const std::string &line);
 
-  };
-}
+ private:
+  float Score(std::vector<int>& source_words, std::vector<int>& target_words) const;
+
+  int LookUpNeuralLMWord(const std::string& str) const;
+
+  void initSharedPointer() const;
+
+  void loadModel();
+
+  void SetParameter(const std::string& key, const std::string& value);
+
+  nplm::neuralLM *m_neuralLM_shared;
+  mutable boost::thread_specific_ptr<nplm::neuralLM> m_neuralLM;
+
+  bool premultiply;
+  int neuralLM_cache;
+};
+
+} // namespace Moses
