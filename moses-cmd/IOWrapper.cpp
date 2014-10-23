ba1883b18bbf42d30c821c95cb61b557b3045ed4
mjdenkowski <michael.j.denkowski@gmail.com>
2014-10-22 20:26:54 -0400

    Fixes issue #80. Thanks emjotde

--- moses-cmd/IOWrapper.cpp
+++ moses-cmd/IOWrapper.cpp
@@ -349,7 +349,7 @@ void OutputAlignment(OutputCollector* collector, size_t lineNo , const vector<co
 {
   ostringstream out;
   OutputAlignment(out, edges);
-
+  out << std::endl;
   collector->Write(lineNo,out.str());
 }
 
