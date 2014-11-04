ae8b9cbfefb0bac52567d9bc4d1f27022c9ef021
Rico Sennrich <rico.sennrich@gmx.ch>
2014-11-04 14:05:13 +0000

    glue grammar: alignment for <s> and </s>

--- phrase-extract/extract-rules-main.cpp
+++ phrase-extract/extract-rules-main.cpp
@@ -1119,8 +1119,8 @@ void writeGlueGrammar( const string & fileName, RuleExtractionOptions &options, 
   ofstream grammarFile;
   grammarFile.open(fileName.c_str());
   if (!options.targetSyntax) {
-    grammarFile << "<s> [X] ||| <s> [S] ||| 1 ||| ||| 0" << endl
-                << "[X][S] </s> [X] ||| [X][S] </s> [S] ||| 1 ||| 0-0 ||| 0" << endl
+    grammarFile << "<s> [X] ||| <s> [S] ||| 1 ||| 0-0 ||| 0" << endl
+                << "[X][S] </s> [X] ||| [X][S] </s> [S] ||| 1 ||| 0-0 1-1 ||| 0" << endl
                 << "[X][S] [X][X] [X] ||| [X][S] [X][X] [S] ||| 2.718 ||| 0-0 1-1 ||| 0" << endl;
   } else {
     // chose a top label that is not already a label
@@ -1132,13 +1132,13 @@ void writeGlueGrammar( const string & fileName, RuleExtractionOptions &options, 
       }
     }
     // basic rules
-    grammarFile << "<s> [X] ||| <s> [" << topLabel << "] ||| 1  ||| " << endl
-                << "[X][" << topLabel << "] </s> [X] ||| [X][" << topLabel << "] </s> [" << topLabel << "] ||| 1 ||| 0-0 " << endl;
+    grammarFile << "<s> [X] ||| <s> [" << topLabel << "] ||| 1  ||| 0-0" << endl
+                << "[X][" << topLabel << "] </s> [X] ||| [X][" << topLabel << "] </s> [" << topLabel << "] ||| 1 ||| 0-0 1-1" << endl;
 
     // top rules
     for( map<string,int>::const_iterator i =  targetTopLabelCollection.begin();
          i !=  targetTopLabelCollection.end(); i++ ) {
-      grammarFile << "<s> [X][" << i->first << "] </s> [X] ||| <s> [X][" << i->first << "] </s> [" << topLabel << "] ||| 1 ||| 1-1" << endl;
+      grammarFile << "<s> [X][" << i->first << "] </s> [X] ||| <s> [X][" << i->first << "] </s> [" << topLabel << "] ||| 1 ||| 0-0 1-1 2-2" << endl;
     }
 
     // glue rules
