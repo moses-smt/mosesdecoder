a2b9f87002f0f42f35705d5ac4cbd07c9b083a9d
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2015-01-06 17:52:07 +0100

    Added exception if now features are available

--- moses/FF/VW/VWFeatureBase.h
+++ moses/FF/VW/VWFeatureBase.h
@@ -62,6 +62,7 @@ class VWFeatureBase : public StatelessFeatureFunction
                   , const TargetPhrase &targetPhrase) const = 0;
     
     static const std::vector<VWFeatureBase*>& GetFeatures(std::string name = "VW0") {
+      UTIL_THROW_IF2(s_features.count(name) == 0, "No features registered for parent classifier: " + name);
       return s_features[name];
     }
   
