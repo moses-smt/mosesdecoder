635b4c8181cd9991716bba0712e80d3bd320d6fc
Ales Tamchyna <tamchyna@ufal.mff.cuni.cz>
2015-01-07 17:35:09 +0100

    turn off debugging for now

--- vw/Classifier.h
+++ vw/Classifier.h
@@ -139,7 +139,7 @@ protected:
 
 private:
   VWPredictor(vw * instance, int index); // instantiation by VWPredictorFactory
-  const static bool DEBUG = true;
+  const static bool DEBUG = false;
 };
   
 /**
