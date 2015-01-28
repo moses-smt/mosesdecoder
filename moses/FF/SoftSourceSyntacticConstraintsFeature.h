91dfd36c8b028cc07cd707e254f46f77ef4261cd
Matthias Huck <huck@i6.informatik.rwth-aachen.de>
2015-01-28 20:49:04 +0000

    use FEATUREVERBOSE

--- moses/FF/SoftSourceSyntacticConstraintsFeature.h
+++ moses/FF/SoftSourceSyntacticConstraintsFeature.h
@@ -13,7 +13,9 @@ namespace Moses
 
 class SoftSourceSyntacticConstraintsFeature : public StatelessFeatureFunction
 {
+
 public:
+
   SoftSourceSyntacticConstraintsFeature(const std::string &line);
 
   ~SoftSourceSyntacticConstraintsFeature() {
@@ -28,6 +30,8 @@ public:
   }
 
   void SetParameter(const std::string& key, const std::string& value);
+  
+  void Load();
 
   void EvaluateInIsolation(const Phrase &source
                            , const TargetPhrase &targetPhrase
@@ -57,7 +61,9 @@ public:
     ScoreComponentCollection* accumulator) const
   {};
 
-private:
+
+protected:
+
   std::string m_sourceLabelSetFile;
   std::string m_coreSourceLabelSetFile;
   std::string m_targetSourceLHSJointCountFile;
@@ -84,7 +90,6 @@ private:
   float m_smoothingWeight;
   float m_unseenLHSSmoothingFactorForUnknowns;
 
-  void Load();
   void LoadSourceLabelSet();
   void LoadCoreSourceLabelSet();
   void LoadTargetSourceLeftHandSideJointCountFile();
