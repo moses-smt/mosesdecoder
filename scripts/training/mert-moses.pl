moses-smt/mosesdecoder

commit d39cbca0b9532c8e51d1a42eef2cb2ef2a8d9bf7

(optionally) use n-best file for evaluator/return-best-dev
this adds support for metrics that rely on alignment / trees

scripts/training/mert-moses.pl
@@ -1120,7 +1120,7 @@ if($___RETURN_BEST_DEV) {
   my $bestbleu=0;
   my $evalout = "eval.out";
   for (my $i = 1; $i < $run; $i++) {
-    my $cmd = "$mert_eval_cmd --reference " . join(",", @references) . " $mert_extract_args --candidate run$i.out";
+    my $cmd = "$mert_eval_cmd --reference " . join(",", @references) . " $mert_extract_args --nbest run$i.best$___N_BEST_LIST_SIZE.out.gz";
     $cmd .= " -l $__REMOVE_SEGMENTATION" if defined( $__PROMIX_TRAINING);
     safesystem("$cmd 2> /dev/null 1> $evalout");
     open my $fh, '<', $evalout or die "Can't read $evalout : $!";

mert/Data.cpp
@@ -135,7 +135,7 @@ void Data::load(const std::string &featfile, const std::string &scorefile)
   m_score_data->load(scorefile);
 }
 
-void Data::loadNBest(const string &file)
+void Data::loadNBest(const string &file, bool oneBest)
 {
   TRACE_ERR("loading nbest from " << file << endl);
   util::FilePiece in(file.c_str());
@@ -154,6 +154,7 @@ void Data::loadNBest(const string &file)
       util::TokenIter<util::MultiCharacter> it(line, util::MultiCharacter("|||"));
 
       sentence_index = ParseInt(*it);
+      if (oneBest && m_score_data->exists(sentence_index)) continue;
       ++it;
       sentence = it->as_string();
       ++it;

mert/Data.h
@@ -67,7 +67,7 @@ public:
     m_feature_data->Features(f);
   }
 
-  void loadNBest(const std::string &file);
+  void loadNBest(const std::string &file, bool oneBest=false);
 
   void load(const std::string &featfile, const std::string &scorefile);
 

mert/evaluator.cpp
@@ -14,6 +14,7 @@
 #include "ScorerFactory.h"
 #include "Timer.h"
 #include "Util.h"
+#include "Data.h"
 
 using namespace std;
 using namespace MosesTuning;
@@ -30,17 +31,20 @@ const float g_alpha = 0.05;
 class EvaluatorUtil
 {
 public:
-  static void evaluate(const string& candFile, int bootstrap);
+  static void evaluate(const string& candFile, int bootstrap, bool nbest_mode);
   static float average(const vector<float>& list);
   static string int2string(int n);
+  static vector<ScoreStats> loadNBest(const string& nBestFile);
+  static vector<ScoreStats> loadCand(const string& candFile);
 
 private:
   EvaluatorUtil() {}
   ~EvaluatorUtil() {}
 };
 
-void EvaluatorUtil::evaluate(const string& candFile, int bootstrap)
-{
+// load hypothesis from candidate output
+vector<ScoreStats> EvaluatorUtil::loadCand(const string& candFile) {
+
   ifstream cand(candFile.c_str());
   if (!cand.good()) throw runtime_error("Error opening candidate file");
 
@@ -52,6 +56,34 @@ void EvaluatorUtil::evaluate(const string& candFile, int bootstrap)
   while (getline(cand, line)) {
     g_scorer->prepareStats(entries.size(), line, scoreentry);
     entries.push_back(scoreentry);
+  }
+  return entries;
+}
+
+// load 1-best hypothesis from n-best file (useful if relying on alignment/tree information)
+vector<ScoreStats> EvaluatorUtil::loadNBest(const string& nBestFile) {
+  vector<ScoreStats> entries;
+
+  Data data(g_scorer);
+  data.loadNBest(nBestFile, true);
+  const ScoreDataHandle & score_data = data.getScoreData();
+  for (size_t i = 0; i != score_data->size(); i++) {
+    entries.push_back(score_data->get(i, 0));
+  }
+  return entries;
+}
+
+
+void EvaluatorUtil::evaluate(const string& candFile, int bootstrap, bool nbest_input)
+{
+
+  vector<ScoreStats> entries;
+
+  if (nbest_input) {
+    entries = loadNBest(candFile);
+  }
+  else {
+    entries = loadCand(candFile);
   }
 
   int n = entries.size();
@@ -131,6 +163,7 @@ void usage()
   cerr << "\tThis is of the form NAME1:VAL1,NAME2:VAL2 etc " << endl;
   cerr << "[--reference|-R] comma separated list of reference files" << endl;
   cerr << "[--candidate|-C] comma separated list of candidate files" << endl;
+  cerr << "[--nbest|-n] comma separated list of nbest files (only 1-best is evaluated)" << endl;
   cerr << "[--factors|-f] list of factors passed to the scorer (e.g. 0|2)" << endl;
   cerr << "[--filter|-l] filter command which will be used to preprocess the sentences" << endl;
   cerr << "[--bootstrap|-b] number of booststraped samples (default 0 - no bootstraping)" << endl;
@@ -162,6 +195,7 @@ static struct option long_options[] = {
   {"scconfig", required_argument, 0, 'c'},
   {"reference", required_argument, 0, 'R'},
   {"candidate", required_argument, 0, 'C'},
+  {"nbest", required_argument, 0, 'n'},
   {"bootstrap", required_argument, 0, 'b'},
   {"rseed", required_argument, 0, 'r'},
   {"factors", required_argument, 0, 'f'},
@@ -176,6 +210,7 @@ struct ProgramOption {
   vector<string> scorer_configs;
   string reference;
   string candidate;
+  string nbest;
   vector<string> scorer_factors;
   vector<string> scorer_filter;
   int bootstrap;
@@ -185,6 +220,7 @@ struct ProgramOption {
   ProgramOption()
     : reference(""),
       candidate(""),
+      nbest(""),
       bootstrap(0),
       seed(0),
       has_seed(false) { }
@@ -195,7 +231,7 @@ void ParseCommandOptions(int argc, char** argv, ProgramOption* opt)
   int c;
   int option_index;
   int last_scorer_index = -1;
-  while ((c = getopt_long(argc, argv, "s:c:R:C:b:r:f:l:h", long_options, &option_index)) != -1) {
+  while ((c = getopt_long(argc, argv, "s:c:R:C:n:b:r:f:l:h", long_options, &option_index)) != -1) {
     switch(c) {
     case 's':
       opt->scorer_types.push_back(string(optarg));
@@ -212,6 +248,9 @@ void ParseCommandOptions(int argc, char** argv, ProgramOption* opt)
       break;
     case 'C':
       opt->candidate = string(optarg);
+      break;
+    case 'n':
+      opt->nbest = string(optarg);
       break;
     case 'b':
       opt->bootstrap = atoi(optarg);
@@ -271,8 +310,13 @@ int main(int argc, char** argv)
     if (option.reference.length() == 0) throw runtime_error("You have to specify at least one reference file.");
     split(option.reference, ',', refFiles);
 
-    if (option.candidate.length() == 0) throw runtime_error("You have to specify at least one candidate file.");
-    split(option.candidate, ',', candFiles);
+    if (option.candidate.length() == 0 && option.nbest.length() == 0) throw runtime_error("You have to specify at least one candidate (or n-best) file.");
+    if (option.candidate.length() > 0 && option.nbest.length() > 0) throw runtime_error("You can either specify candidate files or n-best files, but not both.");
+    bool nbest_input = option.nbest.length() > 0;
+    if (nbest_input)
+      split(option.nbest, ',', candFiles);
+    else
+      split(option.candidate, ',', candFiles);
 
     if (candFiles.size() > 1) g_has_more_files = true;
     if (option.scorer_types.size() > 1) g_has_more_scorers = true;
@@ -283,7 +327,7 @@ int main(int argc, char** argv)
         g_scorer->setFactors(option.scorer_factors[i]);
         g_scorer->setFilter(option.scorer_filter[i]);
         g_scorer->setReferenceFiles(refFiles);
-        EvaluatorUtil::evaluate(*fileIt, option.bootstrap);
+        EvaluatorUtil::evaluate(*fileIt, option.bootstrap, nbest_input);
         delete g_scorer;
       }
     }

commit e2b1ac1e9d955c55cab5000eccc7b052f196a90b

fix option --return-best-dev with hypergraph MIRA (which I broke in commit d39cbca0b9)

scripts/training/mert-moses.pl
@@ -1127,7 +1127,16 @@ if($___RETURN_BEST_DEV) {
   my $bestbleu=0;
   my $evalout = "eval.out";
   for (my $i = 1; $i < $run; $i++) {
-    my $cmd = "$mert_eval_cmd --reference " . join(",", @references) . " $mert_extract_args --nbest run$i.best$___N_BEST_LIST_SIZE.out.gz";
+    my $candidate;
+    if ($___HG_MIRA) {
+      die "File not found: run$i.out" unless -r "run$i.out";
+      $candidate = "--candidate run$i.out";
+    }
+    else {
+      die "File not found: run$i.best$___N_BEST_LIST_SIZE.out.gz" unless -r "run$i.best$___N_BEST_LIST_SIZE.out.gz";
+      $candidate = "--nbest run$i.best$___N_BEST_LIST_SIZE.out.gz";
+    }
+    my $cmd = "$mert_eval_cmd --reference " . join(",", @references) . " $mert_extract_args $candidate";
     $cmd .= " -l $__REMOVE_SEGMENTATION" if defined( $__PROMIX_TRAINING);
     safesystem("$cmd 2> /dev/null 1> $evalout");
     open my $fh, '<', $evalout or die "Can't read $evalout : $!";