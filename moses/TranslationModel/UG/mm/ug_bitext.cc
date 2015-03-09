524376fad454c7f7570b3fc27f47b7d4f6d1028c
Ulrich Germann <ugermann@inf.ed.ac.uk>
2015-03-09 00:34:47 +0000

    Code cleanup.

--- moses/TranslationModel/UG/mm/ug_bitext.cc
+++ moses/TranslationModel/UG/mm/ug_bitext.cc
@@ -273,32 +273,29 @@ namespace Moses
     snt_adder<L2R_Token<SimpleWordId> >::
     operator()()
     {
-	vector<id_type> sids;
-	sids.reserve(snt.size());
-	BOOST_FOREACH(string const& foo, snt)
-	  {
-	    sids.push_back(track ? track->size() : 0);
-	    istringstream buf(foo);
-	    string w;
-	    vector<L2R_Token<SimpleWordId > > s;
-	    s.reserve(100);
-	    while (buf >> w) 
-	      s.push_back(L2R_Token<SimpleWordId>(V[w]));
-	    track = append(track,s);
-	  }
-	if (index)
-	  index.reset(new imTSA<L2R_Token<SimpleWordId> >(*index,track,sids,V.tsize()));
-	else
-	  index.reset(new imTSA<L2R_Token<SimpleWordId> >(track,NULL,NULL));
+      typedef L2R_Token<SimpleWordId> tkn;
+      vector<id_type> sids; sids.reserve(snt.size());
+      BOOST_FOREACH(string const& foo, snt)
+	{
+	  sids.push_back(track ? track->size() : 0);
+	  istringstream buf(foo);
+	  string w;
+	  vector<tkn> s; s.reserve(100);
+	  while (buf >> w) s.push_back(tkn(V[w]));
+	  track = append(track,s);
+	}
+      if (index)
+	index.reset(new imTSA<tkn>(*index,track,sids,V.tsize()));
+      else
+	index.reset(new imTSA<tkn>(track,NULL,NULL));
     }
-
+    
     snt_adder<L2R_Token<SimpleWordId> >::
     snt_adder(vector<string> const& s, TokenIndex& v, 
      	      sptr<imTtrack<L2R_Token<SimpleWordId> > >& t, 
 	      sptr<imTSA<L2R_Token<SimpleWordId> > >& i)
       : snt(s), V(v), track(t), index(i) 
     { }
-
 
     bool 
     expand_phrase_pair
@@ -354,7 +351,9 @@ namespace Moses
 	      done2.set(p);
 	      BOOST_FOREACH(ushort i, a2[p])
 		{
-		  if ((e1 < L1 && i >= L1) || (s1 >= R1 && i < R1) || (i >= L1 && i < R1))
+		  if ((e1 < L1 && i >= L1) || 
+		      (s1 >= R1 && i < R1) || 
+		      (i >= L1 && i < R1))
 		    {
 		      // cout << __FILE__ << ":" << __LINE__ << " " 
 		      // << L1 << "-" << R1 << " " << i << " " 
@@ -381,85 +380,6 @@ namespace Moses
       ++e2;
       return true;
     }
-    //   s1 = seed;
-    //   e1 = seed;
-    //   s2 = e2 = a1[seed].front();
-
-    //   BOOST_FOREACH(ushort k, a1[seed])
-    // 	{
-    // 	  if (s2 < k) s2 = k;
-    // 	  if (e2 > k) e2 = k;
-    // 	}
-
-    //   for (ushort j = s2; j <= e2; ++j)
-    // 	{
-    // 	  if (a2[j].size() == 0) continue;
-    // 	  done2.set(j);
-    // 	  agenda.push_back(pair<ushort,ushort>(j,1));
-    // 	}
-
-    //   while (agenda.size())
-    // 	{
-    // 	  ushort side = agenda[0].second;
-    // 	  ushort i    = agenda[0].first;
-    // 	  agenda.pop_back();
-    // 	  if (side)
-    // 	    {
-    // 	      BOOST_FOREACH(ushort k, a2[i])
-    // 		{
-    // 		  if (k < L1 || k > R1) 
-    // 		    return false;
-    // 		  if (done1[k]) 
-    // 		    continue;
-    // 		  while (s1 > k)
-    // 		    {
-    // 		      --s1;
-    // 		      if (done1[s1] || !a1[s1].size()) 
-    // 			continue;
-    // 		      done1.set(s1);
-    // 		      agenda.push_back(pair<ushort,ushort>(s1,0));
-    // 		    }
-    // 		  while (e1 < k)
-    // 		    {
-    // 		      ++e1;
-    // 		      if (done1[e1] || !a1[e1].size())
-    // 			continue;
-    // 		      done1.set(e1);
-    // 		      agenda.push_back(pair<ushort,ushort>(e1,0));
-    // 		    }
-    // 		}
-    // 	    }
-    // 	  else
-    // 	    {
-    // 	      BOOST_FOREACH(ushort k, a1[i])
-    // 		{
-    // 		  if (k < L2 || k > R2) 
-    // 		    return false;
-    // 		  if (done2[k]) 
-    // 		    continue;
-    // 		  while (s2 > k)
-    // 		    {
-    // 		      --s2;
-    // 		      if (done2[s2] || !a2[s2].size()) 
-    // 			continue;
-    // 		      done1.set(s2);
-    // 		      agenda.push_back(pair<ushort,ushort>(s2,1));
-    // 		    }
-    // 		  while (e2 < k)
-    // 		    {
-    // 		      ++e2;
-    // 		      if (done1[e2] || !a1[e2].size())
-    // 			continue;
-    // 		      done2.set(e2);
-    // 		      agenda.push_back(pair<ushort,ushort>(e2,1));
-    // 		    }
-    // 		}
-    // 	    }
-    // 	}
-    //   ++e1;
-    //   ++e2;
-    //   return true;
-    // }
 
     void 
     print_amatrix(vector<vector<ushort> > a1, uint32_t len2,
