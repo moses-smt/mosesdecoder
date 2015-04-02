5ef23b726b0eb31f33bfeeaefa742da7cdee8fe2
fethi bougares <fethi.bougares@lium.univ-lemans.fr>
2015-04-02 14:28:44 +0200

    Add Skeleton for Asynchronous feature functions

---
+++ moses/FF/SkeletonAsynchFF.cpp
@@ -0,0 +1,74 @@
+#include <vector>
+#include "SkeletonAsynchFF.h"
+#include "moses/ScoreComponentCollection.h"
+#include "moses/TargetPhrase.h"
+
+using namespace std;
+
+namespace Moses
+{
+SkeletonAsynchFF::SkeletonAsynchFF(const std::string &line)
+  :AsynchFeatureFunction(2, line)
+{
+  ReadParameters();
+}
+
+
+void SkeletonAsynchFF::EvaluateNbest(const InputType &input,
+		                     const TrellisPathList &Nbest) const 
+{}
+
+void SkeletonAsynchFF::EvaluateInIsolation(const Phrase &source
+    , const TargetPhrase &targetPhrase
+    , ScoreComponentCollection &scoreBreakdown
+    , ScoreComponentCollection &estimatedFutureScore) const
+{
+  // dense scores
+  vector<float> newScores(m_numScoreComponents);
+  newScores[0] = 0.0;
+  newScores[1] = 0.0;
+  scoreBreakdown.PlusEquals(this, newScores);
+
+  // sparse scores
+//   scoreBreakdown.PlusEquals(this, "sparse-name", );
+
+}
+
+void SkeletonAsynchFF::EvaluateWithSourceContext(const InputType &input
+    , const InputPath &inputPath
+    , const TargetPhrase &targetPhrase
+    , const StackVec *stackVec
+    , ScoreComponentCollection &scoreBreakdown
+    , ScoreComponentCollection *estimatedFutureScore) const
+{
+  if (targetPhrase.GetNumNonTerminals()) {
+    vector<float> newScores(m_numScoreComponents);
+    newScores[0] = - std::numeric_limits<float>::infinity();
+    scoreBreakdown.PlusEquals(this, newScores);
+  }
+}
+
+void SkeletonAsynchFF::EvaluateTranslationOptionListWithSourceContext(const InputType &input
+
+    , const TranslationOptionList &translationOptionList) const
+{}
+
+void SkeletonAsynchFF::EvaluateWhenApplied(const Hypothesis& hypo,
+    ScoreComponentCollection* accumulator) const
+{}
+
+void SkeletonAsynchFF::EvaluateWhenApplied(const ChartHypothesis &hypo,
+    ScoreComponentCollection* accumulator) const
+{}
+
+void SkeletonAsynchFF::SetParameter(const std::string& key, const std::string& value)
+{
+  if (key == "arg") {
+    // set value here
+  } else {
+    AsynchFeatureFunction::SetParameter(key, value);
+  }
+}
+
+}
+
---
+++ moses/FF/SkeletonAsynchFF.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <string>
+#include "moses/HypothesisStackNormal.h"
+#include "moses/TrellisPathList.h"
+#include "AsynchFeatureFunction.h"
+
+namespace Moses
+{
+
+class SkeletonAsynchFF : public AsynchFeatureFunction
+{
+public:
+  SkeletonAsynchFF(const std::string &line);
+
+  bool IsUseable(const FactorMask &mask) const {
+    return true;
+  }
+
+   /*
+    *  Evaluate functions for Asynch features
+    */
+
+  void EvaluateNbest(const InputType &input,
+	                     const TrellisPathList &Nbest) const;
+
+//  void EvaluateSearchGraph(const InputType &input,
+//		  	   const std::vector < HypothesisStack* > &hypoStackColl );	
+
+  /**
+   *
+   */
+
+  void EvaluateInIsolation(const Phrase &source
+                           , const TargetPhrase &targetPhrase
+                           , ScoreComponentCollection &scoreBreakdown
+                           , ScoreComponentCollection &estimatedFutureScore) const;
+  void EvaluateWithSourceContext(const InputType &input
+                                 , const InputPath &inputPath
+                                 , const TargetPhrase &targetPhrase
+                                 , const StackVec *stackVec
+                                 , ScoreComponentCollection &scoreBreakdown
+                                 , ScoreComponentCollection *estimatedFutureScore = NULL) const;
+
+  void EvaluateTranslationOptionListWithSourceContext(const InputType &input
+      , const TranslationOptionList &translationOptionList) const;
+
+  void EvaluateWhenApplied(const Hypothesis& hypo,
+                           ScoreComponentCollection* accumulator) const;
+  void EvaluateWhenApplied(const ChartHypothesis &hypo,
+                           ScoreComponentCollection* accumulator) const;
+
+
+  void SetParameter(const std::string& key, const std::string& value);
+
+};
+
+}
+
