8a3ae2fd5c4fc11fafe2ba64cf29a655688f043a
Jeroen Vermeulen <jtv@precisiontranslationtools.com>
2015-04-10 12:54:34 +0700

    Portability and include fixes.
    Add <cstdlib> include for srand()/rand(), and <unistd.h> for open() etc.
    Include <unistd.h> on Windows if using MinGW.  Disable MeteorScorer on
    Windows, since it doesn't have fork() and pipe().

--- mert/MeteorScorer.cpp
+++ mert/MeteorScorer.cpp
@@ -18,6 +18,7 @@
 
 #include "ScoreStats.h"
 #include "Util.h"
+#include "util/unistd.hh"
 
 using namespace std;
 
@@ -25,7 +26,7 @@ namespace MosesTuning
 {
 
 // Meteor supported
-#if defined(__GLIBCXX__) || defined(__GLIBCPP__)
+#if (defined(__GLIBCXX__) || defined(__GLIBCPP__)) && !defined(_WIN32)
 
 // for clarity
 #define CHILD_STDIN_READ pipefds_input[0]
--- mert/evaluator.cpp
+++ mert/evaluator.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <string>
