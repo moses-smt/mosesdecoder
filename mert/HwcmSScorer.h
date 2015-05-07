717b2e411f69d95a5a3d6fb12c1067ab2d34b8f8
maria nadejde <maria.nadejde@gmail.com>
2015-05-07 13:56:25 +0100

    add HWCMS

---
+++ mert/HwcmSScorer.h
@@ -0,0 +1,67 @@
+/*
+ * HwcmSScorer.h
+ *
+ *  Created on: May 5, 2015
+ *      Author: mnadejde
+ */
+
+#ifndef HWCMSSCORER_H_
+#define HWCMSSCORER_H_
+
+#include <string>
+#include <vector>
+
+#include "StatisticsBasedScorer.h"
+
+namespace MosesTuning
+{
+
+class ScoreStats;
+
+
+/**
+ * HWCM scoring (Liu and Gildea 2005), but F1 instead of precision.
+ * Rico's class but with no internal tree. Dependency tuples are given instead: dep_id head_id depRel -> 1 2 nsubj
+ */
+class HwcmSScorer: public StatisticsBasedScorer
+{
+public:
+  explicit HwcmSScorer(const std::string& config = "");
+  ~HwcmSScorer();
+
+  virtual void setReferenceFiles(const std::vector<std::string>& referenceFiles);
+  virtual void prepareStats(std::size_t sid, const std::string& text, ScoreStats& entry);
+
+  virtual std::size_t NumberOfScores() const {
+    return m_order*3; //HwcmOrder = 4
+  }
+
+  virtual float calculateScore(const std::vector<int>& comps) const;
+
+ std::vector <std::map <std::string,int> > MakeTuples(std::string ref, std::string dep, size_t order, std::vector<int> &totals);
+
+  //which means use field 5 or 6 where we extract the tuples from
+  bool useAlignment() const {
+    return true;
+  }
+
+private:
+
+  // data extracted from reference files
+  std::vector<std::vector<int> > m_ref_lengths;
+  std::vector< std::pair<std::string, std::string> > m_ref;
+  int m_currentRefId;
+  std::vector <std::map<std::string,int> > m_currentRefTuples;
+  bool m_includeRel;
+  std::vector<int> m_totalRef;
+  size_t m_order; //3 means: child + 3 ancestors
+
+  // no copying allowed
+  HwcmSScorer(const HwcmSScorer&);
+  HwcmSScorer& operator=(const HwcmSScorer&);
+};
+
+
+}
+
+#endif /* HWCMSSCORER_H_ */
