294dcbb7a164329493314652d1a8c4320335ce9f
Barry Haddow <barry.haddow@gmail.com>
2015-05-13 12:12:20 +0100

    format of output

--- mert/hgdecode.cpp
+++ mert/hgdecode.cpp
@@ -101,7 +101,7 @@ int main(int argc, char** argv)
 
   //write weights
   cerr << "WEIGHTS ";
-  bestHypo.featureVector.write(cerr);
+  bestHypo.featureVector.write(cerr, "=");
   cerr << endl;
 
 }
