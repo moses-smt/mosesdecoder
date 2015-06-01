f3ccd68bee73e2d8dfe8b7d57c9ea2b33d0d99ae
Phil Williams <philip.williams@mac.com>
2015-06-01 10:35:50 +0100

    Add ConstPreOrderIterator to MosesTraining::Syntax::Tree

--- phrase-extract/syntax-common/tree_test.cc
+++ phrase-extract/syntax-common/tree_test.cc
@@ -61,6 +61,46 @@ BOOST_AUTO_TEST_CASE(pre_order_2) {
   BOOST_REQUIRE(p == end);
 }
 
+// Test Tree<>::ConstPreOrderIterator on this tree: (1 (2 (3 (4 (5) (6)))) (7))
+BOOST_AUTO_TEST_CASE(const_pre_order_1) {
+  boost::scoped_ptr<Tree<int> > root(new Tree<int>(1));
+  root->children().push_back(new Tree<int>(2));
+  root->children()[0]->children().push_back(new Tree<int>(3));
+  root->children()[0]->children()[0]->children().push_back(new Tree<int>(4));
+  root->children()[0]->children()[0]->children()[0]->children().push_back(
+      new Tree<int>(5));
+  root->children()[0]->children()[0]->children()[0]->children().push_back(
+      new Tree<int>(6));
+  root->children().push_back(new Tree<int>(7));
+  root->SetParents();
+
+  Tree<int>::ConstPreOrderIterator p(*root);
+  Tree<int>::ConstPreOrderIterator end;
+
+  BOOST_REQUIRE(p != end);
+  BOOST_REQUIRE(p->value() == 1);
+  ++p;
+  BOOST_REQUIRE(p != end);
+  BOOST_REQUIRE(p->value() == 2);
+  ++p;
+  BOOST_REQUIRE(p != end);
+  BOOST_REQUIRE(p->value() == 3);
+  ++p;
+  BOOST_REQUIRE(p != end);
+  BOOST_REQUIRE(p->value() == 4);
+  ++p;
+  BOOST_REQUIRE(p != end);
+  BOOST_REQUIRE(p->value() == 5);
+  ++p;
+  BOOST_REQUIRE(p != end);
+  BOOST_REQUIRE(p->value() == 6);
+  ++p;
+  BOOST_REQUIRE(p != end);
+  BOOST_REQUIRE(p->value() == 7);
+  ++p;
+  BOOST_REQUIRE(p == end);
+}
+
 }  // namespace
 }  // namespace Syntax
 }  // namespace MosesTraining
