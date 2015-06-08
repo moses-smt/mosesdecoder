cd5530cb875cef5282f2f5bdee93b33327aaf5c0
Ulrich Germann <ugermann@inf.ed.ac.uk>
2015-06-08 12:10:48 +0100

    Added option to make moses consider the entire document as context.

--- moses/parameters/ContextParameters.cpp
+++ moses/parameters/ContextParameters.cpp
@@ -6,7 +6,8 @@ namespace Moses
 
 ContextParameters::
 ContextParameters()
-  : look_ahead(0), look_back(0) { }
+  : look_ahead(0), look_back(0) 
+{ }
 
 void
 ContextParameters::
@@ -19,11 +20,18 @@ init(Parameter& params)
 
   if (context_window == "")
     return;
-
+  
+  if (context_window.substr(0,3) == "all")
+    {
+      look_back = look_ahead = std::numeric_limits<size_t>::max();
+      return;
+    }
+ 
   size_t p = context_window.find_first_of("0123456789");
   if (p == 0)
     look_back = look_ahead = atoi(context_window.c_str());
-  if (p == 1) {
+ 
+ if (p == 1) {
     if (context_window[0] == '-')
       look_back  = atoi(context_window.substr(1).c_str());
     else if (context_window[0] == '+')
@@ -31,6 +39,7 @@ init(Parameter& params)
     else
       UTIL_THROW2("Invalid specification of context window.");
   }
+
   if (p == 2) {
     if (context_window.substr(0,2) == "+-" ||
         context_window.substr(0,2) == "-+")
