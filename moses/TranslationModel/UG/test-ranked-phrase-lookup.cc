78b0aab65ba80ebaee4e5308834b19f0307244d5
Ulrich Germann <ugermann@inf.ed.ac.uk>
2015-06-08 14:04:19 +0100

    Work in progress.

--- moses/TranslationModel/UG/test-ranked-phrase-lookup.cc
+++ moses/TranslationModel/UG/test-ranked-phrase-lookup.cc
@@ -8,10 +8,13 @@
 #include "mm/ug_bitext_sampler.h"
 #include "mm/ug_phrasepair.h"
 #include "mm/ug_lru_cache.h"
+// #include "mm/memtrack.h"
 #include "generic/sorting/VectorIndexSorter.h"
 #include "generic/sorting/NBestList.h"
 #include <string>
 #include <boost/unordered_map.hpp>
+#include "moses/thread_safe_container.h"
+#include "mm/ug_prep_phrases.h"
 
 using namespace std;
 using namespace Moses;
@@ -28,6 +31,7 @@ typedef imTSA<Token> imtsa;
 
 string bname, bname1, bname2, ifile, L1, L2, Q1, Q2;
 size_t maxhits;
+size_t cache_size;
 void interpret_args(int ac, char* av[]);
 
 sptr<imttrack>
@@ -52,22 +56,45 @@ read_input(TokenIndex& V)
   return ret;
 }
 
+typedef ThreadSafeContainer<uint64_t, sptr<pstats> > permacache_t;
+
+void dump(iter& m, TokenIndex& V)
+{
+  if (m.down())
+    {
+      do 
+        { 
+          // cout << m.str(&V) << endl;
+          dump(m,V); 
+        } 
+      while (m.over());
+      m.up();
+    }
+}
+
 int main(int argc, char* argv[])
 {
+  typedef vector<PhrasePair<Token> > pplist_t;
   interpret_args(argc, argv);
-  iptr<mmbitext> Bptr(new mmbitext); 
-  mmbitext& B = *Bptr;
+  iptr<mmbitext> Bptr(new mmbitext);
+  mmbitext& B = *Bptr;// static_cast<mmbitext*>(Bptr.get());
   B.open(bname, L1, L2);
+  B.V1->setDynamic(true);
   sptr<imttrack> icrp = read_input(*B.V1);
   imtsa newIdx(icrp,NULL);
-  sptr<SentenceBias> bias = prime_sampling1(*B.I1, newIdx, 5000);
+  sptr<SentenceBias> bias = prime_sampling1(*B.I1, newIdx, 5000, B.sid2did());
   cerr << "primed" << endl;
-  typedef vector<PhrasePair<Token> > pplist_t;
-#define WITH_CACHE 1
-#if WITH_CACHE
-  // map<uint64_t, sptr<pstats const> > CACHE;
-  lru_cache::LRU_Cache<uint64_t, pstats> CACHE(1000);
-#endif
+  ug::ThreadPool T(boost::thread::hardware_concurrency());
+  TSA<Token>::tree_iterator m(&newIdx);
+  // dump(m, *B.V1);
+  // exit(0);
+  TSA<Token>::tree_iterator r(B.I1.get());
+  StatsCollector<Token> collect(Bptr, bias);
+  collect.tpool = &T;
+  collect.process(m, r);
+
+  typedef PhrasePair<Token>::SortDescendingByJointCount sorter_t;
+  sorter_t sorter;
   for (size_t s = 0; s < icrp->size(); ++s)
     {
       size_t stop = icrp->sntLen(s);
@@ -78,47 +105,83 @@ int main(int argc, char* argv[])
           iter r(B.I1.get());
           for (size_t k = i; k < stop && r.extend(t[k].id()); ++k)
             {
-              // cerr << k << "/" << i << endl;
-              cout << "\n" << r.str(B.V1.get()) 
-                   << " [" << r.ca() << "]" << endl;
-              // sptr<pplist_t> pplist;
-              sptr<pstats> stats;
-#if WITH_CACHE
-              // if (r.ca() > 1000) pplist = CACHE.get(r.getPid()); 
-              stats = CACHE.get(r.getPid()); 
-#endif
-              vector<PhrasePair<Token> > pplist;
-              if (!stats)
-                {
-                  bitext::BitextSampler<Token> 
-                    sampler(&B, r, bias, 1000, ranked_sampling);
-                  sampler();
-                  stats = sampler.stats();
-                  // pplist->resize(pplist->size());
-#if WITH_CACHE
-
-                  // if (r.ca() > 1000) 
-                  CACHE.set(r.getPid(), stats); 
-#endif
-                }
+              sptr<pstats> stats = (*collect.lcache)[r.getPid()];
+              stats->wait();
+              pplist_t pplist;
               expand(r, B, *stats, pplist, NULL);
-              cout << pplist.size() << " " << sizeof(PhrasePair<Token>) << "; "
-                // << pstats::s_instance_count << " instances of pstats live. "
-                // << PhrasePair<Token>::s_instances << " instances of PhrasePair live."
-                   << endl;
-              // BOOST_FOREACH(PhrasePair<Token> const& pp, *pplist)
-              //   {
-              //     if (pp.joint == 1) continue;
-              //     cout << "   " << setw(6) << pp.joint << " " 
-              //          << toString(*B.V2, pp.start2, pp.len2) << endl;
-              //   }
-            }
+              if (pplist.empty()) continue;
+              cout << "\n" << r.str(B.V1.get()) << " [" << r.ca() << "]" << endl;
+              VectorIndexSorter<PhrasePair<Token>, sorter_t> viso(pplist, sorter);
+              sptr<vector<size_t> > ranked = viso.GetOrder();
+              size_t ctr=0;
+              BOOST_FOREACH(size_t const i, *ranked)
+                {
+                  PhrasePair<Token> const& pp = pplist[i];
+                  // if (pp.joint == 1) break;
+                  cout << boost::format("   %6d %.5f | ") % pp.joint % pp.cum_bias 
+                       << toString(*B.V2, pp.start2, pp.len2) << endl;
+                  if (++ctr == 5) break;
+                }
+           }
         }
-    }
-  // cout << pstats::s_instance_count << " instances of pstats live. "
-  //      << PhrasePair<Token>::s_instances << " instances of PhrasePair live."
-  //      << endl;
+    }  
 }
+
+
+//   permacache_t permacache;
+//   lru_cache::LRU_Cache<uint64_t, pstats> CACHE(10000);
+  
+  
+//   for (size_t s = 0; s < icrp->size(); ++s)
+//     {
+//       size_t stop = icrp->sntLen(s);
+//       Token const* t = icrp->sntStart(s);
+//       cout << string(80,'-') << "\n" << toString(*B.V1, t, stop) << endl;
+//       for (size_t i = 0; i < stop; ++i)
+//         {
+//           iter r(B.I1.get());
+//           for (size_t k = i; k < stop && r.extend(t[k].id()); ++k)
+//             {
+//               // cerr << k << "/" << i << endl;
+//               cout << "\n" << r.str(B.V1.get()) 
+//                    << " [" << r.ca() << "]" << endl;
+//               // sptr<pplist_t> pplist;
+//               sptr<pstats> stats;
+
+//               if (cache_size) stats = CACHE.get(r.getPid()); 
+
+//               vector<PhrasePair<Token> > pplist;
+//               if (!stats)
+//                 {
+//                   bitext::BitextSampler<Token> 
+//                     sampler(&B, r, bias, 1000, ranked_sampling);
+//                   sampler();
+//                   stats = sampler.stats();
+//                   if (cache_size) CACHE.set(r.getPid(), stats); 
+//                 }
+//               expand(r, B, *stats, pplist, NULL);
+//               typedef PhrasePair<Token>::SortDescendingByJointCount sorter_t;
+//               sorter_t sorter;
+//               VectorIndexSorter<PhrasePair<Token>, sorter_t> viso(pplist, sorter);
+//               sptr<vector<size_t> > ranked = viso.GetOrder();
+//               size_t ctr=0;
+//               BOOST_FOREACH(size_t const i, *ranked)
+//                 {
+//                   PhrasePair<Token> const& pp = pplist[i];
+//                   // if (pp.joint == 1) break;
+//                   cout << "   " << setw(6) << pp.joint << " " 
+//                        << boost::format("%.5f ") % pp.cum_bias 
+//                        << toString(*B.V2, pp.start2, pp.len2) << endl;
+//                   if (++ctr == 5) break;
+//                 }
+//             }
+//         }
+//     }
+//   // MemTrack::TrackListMemoryUsage();
+//   // cout << pstats::s_instance_count << " instances of pstats live. "
+//   //      << PhrasePair<Token>::s_instances << " instances of PhrasePair live."
+//   //      << endl;
+// }
 
   // vector<float> score(hits->size());
   // VectorIndexSorter<float> sorter(score);
@@ -159,10 +222,12 @@ interpret_args(int ac, char* av[])
   o.add_options()
 
     ("help,h",  "print this message")
-    ("maxhits,n", po::value<size_t>(&maxhits)->default_value(25),
-     "max. number of hits")
-    ("q1", po::value<string>(&Q1), "query in L1")
-    ("q2", po::value<string>(&Q2), "query in L2")
+    ("cache,C", po::value<size_t>(&cache_size)->default_value(0), 
+     "cache size")
+    // ("maxhits,n", po::value<size_t>(&maxhits)->default_value(25),
+    // "max. number of hits")
+    // ("q1", po::value<string>(&Q1), "query in L1")
+    // ("q2", po::value<string>(&Q2), "query in L2")
     ;
 
   po::options_description h("Hidden Options");
@@ -170,7 +235,7 @@ interpret_args(int ac, char* av[])
     ("bname", po::value<string>(&bname), "base name of corpus")
     ("L1", po::value<string>(&L1), "L1 tag")
     ("L2", po::value<string>(&L2), "L2 tag")
-    ("ifile", po::value<string>(&ifile), "input file")
+    ("ifile,i", po::value<string>(&ifile), "input file")
     ;
 
   h.add(o);
