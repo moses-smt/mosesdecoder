fba4a3e24da01a01088c95c8c85f71d551ba4634
MosesAdmin <moses-support-owner@mit.edu>
2015-06-29 00:00:54 +0100

    daily automatic beautifier

--- phrase-extract/ScoreFeatureTest.cpp
+++ phrase-extract/ScoreFeatureTest.cpp
@@ -83,12 +83,14 @@ static void checkDomainConfigured(
 }
 
 template<typename T>
-T adder(T v) {
+T adder(T v)
+{
   return v;
 }
 
 template<typename T, typename... Args>
-T adder(T first, Args... args) {
+T adder(T first, Args... args)
+{
   return first + adder(args...);
 }
 
@@ -113,7 +115,7 @@ BOOST_AUTO_TEST_CASE(manager_config_domain)
   s.insert(4);
   s.insert(1);
 
-	for (auto i: s) {
+for (auto i: s) {
     cerr << i << " ";
   }
 
@@ -121,15 +123,15 @@ BOOST_AUTO_TEST_CASE(manager_config_domain)
   m["a"] = 4;
   m["ba"] = 6;
   m["aabc"] = 7;
-  
-	for (auto i: m) {
+
+for (auto i: m) {
     cerr << i.first << "=" << i.second << " ";
   }
-  
+
   long sum = adder(1, 2, 3, 8, 7);
 
-	std::string s1 = "x", s2 = "aa", s3 = "bb", s4 = "yy";
-	std::string ssum = adder(s1, s2, s3, s4);
+  std::string s1 = "x", s2 = "aa", s3 = "bb", s4 = "yy";
+  std::string ssum = adder(s1, s2, s3, s4);
 
 }
 
