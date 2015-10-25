c879cd1d203f7a201d26a1141eb59f29cf5c04e3
Ulrich Germann <ugermann@inf.ed.ac.uk>
2015-10-25 19:26:48 +0000

    Missing variable initialization.

--- biconcor/Mismatch.cpp
+++ biconcor/Mismatch.cpp
@@ -216,7 +216,7 @@ void Mismatch::PrintClippedHTML( ostream* out, int width )
   // print target
   // shorten target context if too long
   int target_start = -1;
-  int target_end;
+  int target_end=0;
   for(int i=0; i<m_target_length; i++)
     if (target_annotation[i] != UNANNOTATED) {
       if (target_start == -1)
