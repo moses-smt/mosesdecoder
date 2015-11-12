a575cd14dbaa5278ae4eacb369412ac1e9b06af5
Hieu Hoang <hieuhoang@gmail.com>
2015-11-12 22:52:59 +0000

    import classes into moses2

--- contrib/other-builds/moses2/legacy/Factor.h
+++ contrib/other-builds/moses2/legacy/Factor.h
@@ -23,7 +23,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include <ostream>
 #include <string>
-#include "Util.h"
 #include "util/string_piece.hh"
 
 struct FactorFriend;
