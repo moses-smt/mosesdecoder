3ce2f6a55dc41bd56b73b53f66110de9b3ebde0b
Ulrich Germann <Ulrich.Germann@gmail.com>
2015-11-20 13:54:18 +0000

    Use absolute path for external dependencies.

--- run-regtests.sh
+++ run-regtests.sh
@@ -2,13 +2,14 @@
 # this script assumes that all 3rd-party dependencies are installed under ./opt
 # you can install all 3rd-party dependencies by running make -f contrib/Makefiles/install-dependencies.gmake
 
-set -e -o pipefail
+set -e -o pipefail -x
 
-git submodule init
-git submodule update regtest
+opt=$(pwd)/opt
+# git submodule init
+# git submodule update regtest
 
 # test compilation without xmlrpc-c
-./bjam -j$(nproc) --with-irstlm=./opt --with-boost=./opt --with-cmph=./opt --no-xmlrpc-c --with-regtest=./regtest -a -q $@ || exit $?
+./bjam -j$(nproc) --with-irstlm=$opt --with-boost=$opt --with-cmph=$opt --no-xmlrpc-c --with-regtest=./regtest -a -q $@ || exit $?
 
 # test compilation with xmlrpc-c
-./bjam -j$(nproc) --with-irstlm=./opt --with-boost=./opt --with-cmph=./opt --with-xmlrpc-c=./opt --with-regtest=./regtest -a -q $@
+./bjam -j$(nproc) --with-irstlm=$opt --with-boost=$opt --with-cmph=$opt --with-xmlrpc-c=$opt --with-regtest=./regtest -a -q $@
