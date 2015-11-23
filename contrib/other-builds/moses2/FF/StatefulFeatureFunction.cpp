b0697f55c2341f9dd131cd9e62b83da87b484f1c
Hieu Hoang <hieuhoang@gmail.com>
2015-11-23 18:08:03 +0000

    run LM on differe cpu

--- contrib/other-builds/moses2/FF/StatefulFeatureFunction.cpp
+++ contrib/other-builds/moses2/FF/StatefulFeatureFunction.cpp
@@ -4,6 +4,10 @@
  *  Created on: 24 Oct 2015
  *      Author: hieu
  */
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 
 #include <boost/foreach.hpp>
 #include "StatefulFeatureFunction.h"
@@ -20,8 +24,31 @@ StatefulFeatureFunction::~StatefulFeatureFunction() {
 
 void StatefulFeatureFunction::EvaluateWhenApplied(const ObjectPoolContiguous<Hypothesis*> &hypos) const
 {
+#ifdef __linux
+	pthread_t thread;
+	handle = pthread_self();
+
+    int s;
+    cpu_set_t cpusetOrig, cpuset;
+    s = pthread_getaffinity_np(handle, sizeof(cpu_set_t), &cpusetOrig);
+
+    CPU_ZERO(&cpuset);
+
+    int core = handle % 8;
+    core += 24;
+    CPU_SET(core, &cpuset);
+
+    s = pthread_setaffinity_np(handle, sizeof(cpu_set_t), &cpuset);
+
+#endif
+
 	for (size_t i = 0; i < hypos.size(); ++i) {
 		Hypothesis *hypo = hypos.get(i);
 		 hypo->EvaluateWhenApplied(*this);
 	 }
+
+#ifdef __linux
+
+    s = pthread_setaffinity_np(handle, sizeof(cpu_set_t), &cpusetOrig);
+#endif
 }
