cf0270788be5b2b18e2aaa85c45a0a51410c5171
Hieu Hoang <hieuhoang@gmail.com>
2015-12-09 11:06:08 +0000

    probing pt vocab id stored with factor

--- contrib/other-builds/moses2/TranslationModel/ProbingPT.cpp
+++ contrib/other-builds/moses2/TranslationModel/ProbingPT.cpp
@@ -31,7 +31,7 @@ void ProbingPT::Load(System &system)
 {
   m_engine = new QueryEngine(m_path.c_str());
 
-  m_unkId = 456456546456;
+  m_unkId = 0;
 
   FactorCollection &vocab = system.GetVocab();
 
@@ -43,12 +43,8 @@ void ProbingPT::Load(System &system)
 	const Factor *factor = vocab.AddFactor(wordStr, system);
 
 	uint64_t probingId = iterSource->first;
-	size_t factorId = factor->GetId();
 
-	if (factorId >= m_sourceVocab.size()) {
-		m_sourceVocab.resize(factorId + 1, m_unkId);
-	}
-	m_sourceVocab[factorId] = probingId;
+	factor->ffData[m_vocabInd] = (void*) probingId;
   }
 
   // target vocab
@@ -69,11 +65,7 @@ void ProbingPT::Load(System &system)
 
 uint64_t ProbingPT::GetSourceProbingId(const Factor *factor) const
 {
-  size_t factorId = factor->GetId();
-  if (factorId >= m_sourceVocab.size()) {
-	  return m_unkId;
-  }
-  return m_sourceVocab[factorId];
+	return (uint64_t) factor->ffData[m_vocabInd];
 }
 
 void ProbingPT::Lookup(const Manager &mgr, InputPaths &inputPaths) const
