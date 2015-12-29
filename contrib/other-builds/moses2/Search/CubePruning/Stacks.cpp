99bd5c1267c0bbcd979aa9b0523d16a2f472ef6f
Hieu Hoang <hieuhoang@gmail.com>
2015-12-29 09:27:50 -0500

    Revert "don't have Stack*"
    This reverts commit 75d63e44500e14fee3f667bc21fb8cf4e9dd38ce.

--- contrib/other-builds/moses2/Search/CubePruning/Stacks.cpp
+++ contrib/other-builds/moses2/Search/CubePruning/Stacks.cpp
@@ -24,18 +24,22 @@ Stacks::Stacks(const Manager &mgr)
 
 Stacks::~Stacks()
 {
+	RemoveAllInColl(m_stacks);
 }
 
 void Stacks::Init(size_t numStacks)
 {
-	m_stacks.resize(numStacks, Stack(m_mgr));
+	m_stacks.resize(numStacks);
+	for (size_t i = 0; i < m_stacks.size(); ++i) {
+		m_stacks[i] = new Stack(m_mgr);
+	}
 }
 
 
 std::ostream& operator<<(std::ostream &out, const Stacks &obj)
 {
   for (size_t i = 0; i < obj.GetSize(); ++i) {
-	  const Stack &stack = obj.m_stacks[i];
+	  const Stack &stack = *obj.m_stacks[i];
 	  out << stack.GetHypoSize() << " ";
   }
 
@@ -46,7 +50,7 @@ void Stacks::Add(const Hypothesis *hypo, Recycler<Hypothesis*> &hypoRecycle)
 {
 	size_t numWordsCovered = hypo->GetBitmap().GetNumWordsCovered();
 	//cerr << "numWordsCovered=" << numWordsCovered << endl;
-	Stack &stack = m_stacks[numWordsCovered];
+	Stack &stack = *m_stacks[numWordsCovered];
 	stack.Add(hypo, hypoRecycle);
 
 }
