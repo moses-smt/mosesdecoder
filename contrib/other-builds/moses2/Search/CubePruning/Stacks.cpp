95d45dd7f3a548fa4ed6099f10e76afcabe7f569
Hieu Hoang <hieuhoang@gmail.com>
2015-12-29 17:23:21 +0000

    use pointers for stack object

--- contrib/other-builds/moses2/Search/CubePruning/Stacks.cpp
+++ contrib/other-builds/moses2/Search/CubePruning/Stacks.cpp
@@ -28,14 +28,17 @@ Stacks::~Stacks()
 
 void Stacks::Init(size_t numStacks)
 {
-	m_stacks.resize(numStacks, Stack(m_mgr));
+	m_stacks.resize(numStacks);
+	for (size_t i = 0; i < m_stacks.size(); ++i) {
+	  m_stacks[i] = new (m_mgr.GetPool().Allocate<Stack>()) Stack(m_mgr);
+	}
 }
 
 
 std::ostream& operator<<(std::ostream &out, const Stacks &obj)
 {
   for (size_t i = 0; i < obj.GetSize(); ++i) {
-	  const Stack &stack = obj.m_stacks[i];
+	  const Stack &stack = *obj.m_stacks[i];
 	  out << stack.GetHypoSize() << " ";
   }
 
@@ -46,7 +49,7 @@ void Stacks::Add(const Hypothesis *hypo, Recycler<Hypothesis*> &hypoRecycle)
 {
 	size_t numWordsCovered = hypo->GetBitmap().GetNumWordsCovered();
 	//cerr << "numWordsCovered=" << numWordsCovered << endl;
-	Stack &stack = m_stacks[numWordsCovered];
+	Stack &stack = *m_stacks[numWordsCovered];
 	stack.Add(hypo, hypoRecycle);
 
 }
