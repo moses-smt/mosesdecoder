45f59cf97375270bc5c8c64609e91d2053c3d17b
Hieu Hoang <hieuhoang@gmail.com>
2016-01-12 12:10:56 +0000

    use std::vector

--- contrib/other-builds/moses2/Vector.h
+++ contrib/other-builds/moses2/Vector.h
@@ -13,60 +13,18 @@ namespace Moses2
 {
 
 template <typename T>
-class Vector {
+class Vector : public std::vector<T, MemPoolAllocator<T> >
+{
+  typedef std::vector<T, MemPoolAllocator<T> > Parent;
+
 public:
-  typedef T* iterator;
-  typedef const T* const_iterator;
-
-  T *begin()
-  { return m_arr; }
-  const T *begin() const
-  { return m_arr; }
-
-  T *end()
-  { return m_arr + m_size; }
-  const T *end() const
-  { return m_arr + m_size; }
-
   Vector(MemPool &pool, size_t size)
-  :m_size(size)
-  ,m_maxSize(size)
+  :Parent(size, NULL, MemPoolAllocator<T>(pool) )
   {
-	  m_arr = pool.Allocate<T>(size);
   }
 
-  Vector(MemPool &pool, const std::vector<T> &vec)
-  :m_size(vec.size())
-  ,m_maxSize(vec.size())
-  {
-	  m_arr = pool.Allocate<T>(m_size);
-	  for (size_t i = 0; i < m_size; ++i) {
-		  m_arr[i] = vec[i];
-	  }
-  }
-
-  virtual ~Vector()
-  {
-
-  }
-
-  T& operator[](size_t ind)
-  { return m_arr[ind]; }
-  const T& operator[](size_t ind) const
-  { return m_arr[ind]; }
-
-  size_t size() const
-  { return m_size; }
-
-  void resize(size_t newSize)
-  {
-	  assert(newSize <= m_maxSize);
-	  m_size = newSize;
-  }
 
 protected:
-  size_t m_size, m_maxSize;
-  T *m_arr;
 };
 
 }
