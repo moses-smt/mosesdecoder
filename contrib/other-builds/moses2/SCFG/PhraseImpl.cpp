199ca502da5481855cf53502fcd97fc5842b9b20
Hieu Hoang <hieuhoang@gmail.com>
2016-02-22 19:32:46 +0000

    add SCFG::PhraseImpl

---
+++ contrib/other-builds/moses2/SCFG/PhraseImpl.cpp
@@ -0,0 +1,16 @@
+/*
+ * PhraseImpl.cpp
+ *
+ *  Created on: 19 Feb 2016
+ *      Author: hieu
+ */
+#include "PhraseImpl.h"
+
+using namespace std;
+
+namespace Moses2
+{
+
+
+}
+
---
+++ contrib/other-builds/moses2/SCFG/PhraseImpl.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "../PhraseImplTemplate.h"
+
+namespace Moses2
+{
+namespace SCFG
+{
+
+class PhraseImpl : public PhraseImplTemplate<Word>
+{
+public:
+  static PhraseImpl *CreateFromString(MemPool &pool, FactorCollection &vocab, const System &system, const std::string &str)
+  {
+	std::vector<std::string> toks = Moses2::Tokenize(str);
+	size_t size = toks.size();
+	PhraseImpl *ret;
+
+	ret = new (pool.Allocate<PhraseImpl>()) PhraseImpl(pool, size);
+
+	ret->PhraseImplTemplate<Word>::CreateFromString(vocab, system, toks);
+	return ret;
+  }
+
+  PhraseImpl(MemPool &pool, size_t size)
+  :PhraseImplTemplate(pool, size)
+  {}
+
+};
+
+
+}
+
+}
