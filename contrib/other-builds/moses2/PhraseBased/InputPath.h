ff17678195b95a02c8665ebf7061ed53f3533a6e
Hieu Hoang <hieuhoang@gmail.com>
2016-02-29 21:41:52 +0000

    separate out InputPath

---
+++ contrib/other-builds/moses2/PhraseBased/InputPath.h
@@ -0,0 +1,38 @@
+/*
+ * InputPath.h
+ *
+ *  Created on: 23 Oct 2015
+ *      Author: hieu
+ */
+
+#ifndef INPUTPATH_H_
+#define INPUTPATH_H_
+#include <iostream>
+#include <vector>
+#include "../InputPathBase.h"
+
+namespace Moses2
+{
+
+class InputPath : public InputPathBase
+{
+	  friend std::ostream& operator<<(std::ostream &, const InputPath &);
+public:
+	const TargetPhrases** targetPhrases;
+
+	InputPath(MemPool &pool, const SubPhrase &subPhrase, const Range &range, size_t numPt, const InputPath *prefixPath);
+	virtual ~InputPath();
+
+	void AddTargetPhrases(const PhraseTable &pt, const TargetPhrases *tps);
+	const TargetPhrases *GetTargetPhrases(const PhraseTable &pt) const;
+
+	inline bool IsUsed() const
+	{ return m_isUsed; }
+
+protected:
+	bool m_isUsed;
+};
+
+}
+
+#endif /* INPUTPATH_H_ */
