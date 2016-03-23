105faa5673567f8ed4ffee5857b59715511a6a2d
Hieu Hoang <hieuhoang@gmail.com>
2016-03-23 13:57:25 +0000

    separate BidirectionalReorderingState into its own file

--- moses/FF/LexicalReordering/LexicalReorderingState.h
+++ moses/FF/LexicalReordering/LexicalReorderingState.h
@@ -195,36 +195,6 @@ protected:
   ComparePrevScores(const TranslationOption *other) const;
 };
 
-//! @todo what is this?
-class BidirectionalReorderingState
-  : public LRState
-{
-private:
-  const LRState *m_backward;
-  const LRState *m_forward;
-public:
-  BidirectionalReorderingState(const LRModel &config,
-                               const LRState *bw,
-                               const LRState *fw, size_t offset)
-    : LRState(config,
-              LRModel::Bidirectional,
-              offset)
-    , m_backward(bw)
-    , m_forward(fw)
-  { }
-
-  ~BidirectionalReorderingState() {
-    delete m_backward;
-    delete m_forward;
-  }
-
-  virtual size_t hash() const;
-  virtual bool operator==(const FFState& other) const;
-
-  LRState*
-  Expand(const TranslationOption& topt, const InputType& input,
-         ScoreComponentCollection*  scores) const;
-};
 
 
 
