46a94f22586bcfed47a51877cb83bd0df1683162
Hieu Hoang <hieuhoang@gmail.com>
2016-03-23 10:38:42 +0000

    separate HReorderingForwardState into its own file

--- moses/FF/LexicalReordering/LexicalReorderingState.h
+++ moses/FF/LexicalReordering/LexicalReorderingState.h
@@ -283,26 +283,5 @@ private:
 };
 
 
-//!forward state (conditioned on the next phrase)
-class HReorderingForwardState : public LRState
-{
-private:
-  bool m_first;
-  Range m_prevRange;
-  Bitmap m_coverage;
-
-public:
-  HReorderingForwardState(const LRModel &config, size_t sentenceLength,
-                          size_t offset);
-  HReorderingForwardState(const HReorderingForwardState *prev,
-                          const TranslationOption &topt);
-
-  virtual size_t hash() const;
-  virtual bool operator==(const FFState& other) const;
-
-  virtual LRState* Expand(const TranslationOption& hypo,
-                          const InputType& input,
-                          ScoreComponentCollection* scores) const;
-};
 }
 
