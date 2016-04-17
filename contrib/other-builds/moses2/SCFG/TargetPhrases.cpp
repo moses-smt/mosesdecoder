08ddcd2c77ca70ff07e984815b90346df52ddc86
Hieu Hoang <hieuhoang@gmail.com>
2016-04-17 14:02:05 +0400

    TargetPhrases

---
+++ contrib/other-builds/moses2/SCFG/TargetPhrases.cpp
@@ -0,0 +1,27 @@
+/*
+ * TargetPhrases.cpp
+ *
+ *  Created on: 15 Apr 2016
+ *      Author: hieu
+ */
+
+#include "TargetPhrases.h"
+
+namespace Moses2
+{
+namespace SCFG
+{
+
+TargetPhrases::TargetPhrases()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+TargetPhrases::~TargetPhrases()
+{
+  // TODO Auto-generated destructor stub
+}
+
+}
+} /* namespace Moses2 */
---
+++ contrib/other-builds/moses2/SCFG/TargetPhrases.h
@@ -0,0 +1,52 @@
+/*
+ * TargetPhrases.h
+ *
+ *  Created on: 15 Apr 2016
+ *      Author: hieu
+ */
+
+#pragma once
+#include <vector>
+
+namespace Moses2
+{
+namespace SCFG
+{
+class TargetPhraseImpl;
+
+class TargetPhrases
+{
+  typedef std::vector<const SCFG::TargetPhraseImpl*> Coll;
+
+public:
+  typedef Coll::iterator iterator;
+  typedef Coll::const_iterator const_iterator;
+  //! iterators
+  const_iterator begin() const
+  {
+    return m_coll.begin();
+  }
+  const_iterator end() const
+  {
+    return m_coll.end();
+  }
+
+  TargetPhrases();
+  virtual ~TargetPhrases();
+
+  size_t GetSize() const
+  { return m_coll.size(); }
+
+  void AddTargetPhrase(const SCFG::TargetPhraseImpl &targetPhrase)
+  {
+    m_coll.push_back(&targetPhrase);
+  }
+
+protected:
+  Coll m_coll;
+
+};
+
+}
+} /* namespace Moses2 */
+
