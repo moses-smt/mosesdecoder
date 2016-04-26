d06bdecbd3a9e8e0750ce85782c86fb1610212b5
Hieu Hoang <hieuhoang@gmail.com>
2016-04-27 00:44:15 +0400

    templatize SubPhrase class

--- contrib/other-builds/moses2/SubPhrase.cpp
+++ contrib/other-builds/moses2/SubPhrase.cpp
@@ -11,28 +11,7 @@ using namespace std;
 namespace Moses2
 {
 
-const Word &SubPhrase::operator[](size_t pos) const
-{
-  return (*m_origPhrase)[pos + m_start];
-}
 
-std::ostream& operator<<(std::ostream &out, const SubPhrase &obj)
-{
-  if (obj.GetSize()) {
-    out << obj[0];
-    for (size_t i = 1; i < obj.GetSize(); ++i) {
-      const Word &word = obj[i];
-      out << " " << word;
-    }
-  }
-  return out;
-}
-
-SubPhrase SubPhrase::GetSubPhrase(size_t start, size_t size) const
-{
-  SubPhrase ret(*m_origPhrase, m_start + start, m_size);
-  return ret;
-}
 
 }
 
--- contrib/other-builds/moses2/SubPhrase.h
+++ contrib/other-builds/moses2/SubPhrase.h
@@ -14,19 +14,35 @@ public:
   ,m_size(size)
   {}
 
-  virtual const Word& operator[](size_t pos) const;
+  virtual const Word& operator[](size_t pos) const
+  {  return (*m_origPhrase)[pos + m_start]; }
 
   virtual size_t GetSize() const
-  {
-    return m_size;
-  }
+  {  return m_size; }
 
-  SubPhrase GetSubPhrase(size_t start, size_t size) const;
+  SubPhrase GetSubPhrase(size_t start, size_t size) const
+  {
+    SubPhrase ret(*m_origPhrase, m_start + start, m_size);
+    return ret;
+  }
 
 protected:
   const Phrase *m_origPhrase;
   size_t m_start, m_size;
 };
+
+///////////////////////////////////////////////////////
+std::ostream& operator<<(std::ostream &out, const SubPhrase &obj)
+{
+  if (obj.GetSize()) {
+    out << obj[0];
+    for (size_t i = 1; i < obj.GetSize(); ++i) {
+      const Word &word = obj[i];
+      out << " " << word;
+    }
+  }
+  return out;
+}
 
 }
 
