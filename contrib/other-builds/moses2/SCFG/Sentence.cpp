a8ce541adde47e006d6dda44cc68b22457634c09
Hieu Hoang <hieuhoang@gmail.com>
2016-04-27 12:42:07 +0400

    separate Sentence class for SCFG

---
+++ contrib/other-builds/moses2/SCFG/Sentence.cpp
@@ -0,0 +1,19 @@
+/*
+ * Sentence.cpp
+ *
+ *  Created on: 14 Dec 2015
+ *      Author: hieu
+ */
+
+#include "Sentence.h"
+
+using namespace std;
+
+namespace Moses2
+{
+namespace SCFG
+{
+
+}
+} /* namespace Moses2 */
+
---
+++ contrib/other-builds/moses2/SCFG/Sentence.h
@@ -0,0 +1,59 @@
+/*
+ * Sentence.h
+ *
+ *  Created on: 14 Dec 2015
+ *      Author: hieu
+ */
+#pragma once
+
+#include <string>
+#include "PhraseImpl.h"
+#include "../InputType.h"
+#include "../MemPool.h"
+#include "../legacy/Util2.h"
+
+namespace Moses2
+{
+class FactorCollection;
+class System;
+
+namespace SCFG
+{
+
+class Sentence: public InputType, public PhraseImpl
+{
+public:
+  static Sentence *CreateFromString(MemPool &pool, FactorCollection &vocab,
+      const System &system, const std::string &str, long translationId,
+      bool addBOSEOS)
+  {
+    std::vector<std::string> toks = Tokenize(str);
+    size_t size = toks.size();
+
+     if (addBOSEOS) {
+       size += 2;
+     }
+
+    Sentence *ret;
+
+    ret = new (pool.Allocate<Sentence>()) Sentence(translationId, pool, size);
+
+    ret->PhraseImplTemplate<Word>::CreateFromString(vocab, system, toks,
+        addBOSEOS);
+
+    return ret;
+  }
+
+  Sentence(long translationId, MemPool &pool, size_t size)
+  :InputType(translationId)
+  ,PhraseImpl(pool, size)
+  {}
+
+  virtual ~Sentence()
+  {}
+
+};
+
+}
+} /* namespace Moses2 */
+
