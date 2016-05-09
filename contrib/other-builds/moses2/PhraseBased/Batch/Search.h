a0e00ded87c6c62dfb5d849e995af651abb2d0a5
Hieu Hoang <hieuhoang@gmail.com>
2016-05-09 18:00:15 +0100

    add batch search

---
+++ contrib/other-builds/moses2/PhraseBased/Batch/Search.h
@@ -0,0 +1,53 @@
+/*
+ * SearchNormal.h
+ *
+ *  Created on: 25 Oct 2015
+ *      Author: hieu
+ */
+#pragma once
+
+#include <vector>
+#include "../../legacy/Range.h"
+#include "../../legacy/Bitmap.h"
+#include "../../TypeDef.h"
+#include "../Search.h"
+#include "Stacks.h"
+
+namespace Moses2
+{
+template<typename WORD>
+class TargetPhrase;
+
+class Hypothesis;
+class InputPath;
+class TargetPhrases;
+
+namespace NSBatch
+{
+class Stacks;
+
+class Search: public Moses2::Search
+{
+public:
+  Search(Manager &mgr);
+  virtual ~Search();
+
+  virtual void Decode();
+  const Hypothesis *GetBestHypothesis() const;
+
+  void AddInitialTrellisPaths(TrellisPaths &paths) const;
+
+protected:
+  Stacks m_stacks;
+
+  void Decode(size_t stackInd);
+  void Extend(const Hypothesis &hypo, const InputPath &path);
+  void Extend(const Hypothesis &hypo, const TargetPhrases &tps,
+      const InputPath &path, const Bitmap &newBitmap, SCORE estimatedScore);
+  void Extend(const Hypothesis &hypo, const TargetPhrase<Moses2::Word> &tp,
+      const InputPath &path, const Bitmap &newBitmap, SCORE estimatedScore);
+
+};
+
+}
+}
---
+++ contrib/other-builds/moses2/PhraseBased/Batch/Stacks.cpp
@@ -0,0 +1,80 @@
+/*
+ * Stacks.cpp
+ *
+ *  Created on: 6 Nov 2015
+ *      Author: hieu
+ */
+
+#include "Stacks.h"
+#include "../Manager.h"
+#include "../../System.h"
+
+using namespace std;
+
+namespace Moses2
+{
+
+namespace NSBatch
+{
+
+Stacks::Stacks(const Manager &mgr) :
+    m_mgr(mgr)
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+Stacks::~Stacks()
+{
+  for (size_t i = 0; i < m_stacks.size(); ++i) {
+    delete m_stacks[i];
+  }
+}
+
+void Stacks::Init(const Manager &mgr, size_t numStacks)
+{
+  m_stacks.resize(numStacks);
+  for (size_t i = 0; i < m_stacks.size(); ++i) {
+    m_stacks[i] = new Stack(mgr);
+  }
+}
+
+std::ostream& operator<<(std::ostream &out, const Stacks &obj)
+{
+  for (size_t i = 0; i < obj.GetSize(); ++i) {
+    const Stack *stack = obj.m_stacks[i];
+    if (stack) {
+      out << stack->GetSize() << " ";
+    }
+    else {
+      out << "N ";
+    }
+  }
+
+  return out;
+}
+
+void Stacks::Add(Hypothesis *hypo, Recycler<HypothesisBase*> &hypoRecycle,
+    ArcLists &arcLists)
+{
+  size_t numWordsCovered = hypo->GetBitmap().GetNumWordsCovered();
+  //cerr << "numWordsCovered=" << numWordsCovered << endl;
+  Stack &stack = *m_stacks[numWordsCovered];
+  StackAdd added = stack.Add(hypo);
+
+  size_t nbestSize = m_mgr.system.options.nbest.nbest_size;
+  if (nbestSize) {
+    arcLists.AddArc(added.added, hypo, added.other);
+  }
+  else {
+    if (!added.added) {
+      hypoRecycle.Recycle(hypo);
+    }
+    else if (added.other) {
+      hypoRecycle.Recycle(added.other);
+    }
+  }
+}
+
+}
+}
