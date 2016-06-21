6570fa969e46bee71add35c2c9beda4b8d8c7f35
Hieu Hoang <hieuhoang@gmail.com>
2016-06-21 15:37:31 +0100

    set m_xmlCoverageMap

--- contrib/other-builds/moses2/InputType.h
+++ contrib/other-builds/moses2/InputType.h
@@ -39,7 +39,7 @@ public:
   InputType(long translationId, MemPool &pool);
   virtual ~InputType();
 
-  virtual void Init(size_t size, int max_distortion);
+  virtual void Init(const System &system, size_t size, int max_distortion);
 
   long GetTranslationId() const
   {
@@ -55,7 +55,10 @@ public:
   const std::vector<const XMLOption*> &GetXMLOptions() const
   { return m_xmlOptions; }
 
-  void AddXMLOption(const XMLOption *xmlOption);
+  void AddXMLOption(const System &system, const XMLOption *xmlOption);
+
+  //! Returns true if there were any XML tags parsed that at least partially covered the range passed
+  bool XmlOverlap(size_t startPos, size_t endPos) const;
 
 protected:
   long m_translationId; 	//< contiguous Id
