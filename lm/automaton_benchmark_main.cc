fef16b852b98308d50b7a50dcaba13d56da7290c
Jozef Mokry <jozef.mokry@gmail.com>
2016-06-28 20:50:30 +0100

    Use binary file with vocab IDs instead of text file

--- lm/automaton_benchmark_main.cc
+++ lm/automaton_benchmark_main.cc
@@ -18,103 +18,126 @@ void CheckEqual(const lm::FullScoreReturn& lhs, const lm::FullScoreReturn& rhs) 
 }
 }
 
-template <typename Callback>
-void PipelineScore(lm::Pipeline<Callback>& pipeline, const lm::ngram::ProbingModel& model, char* test_file) {
-    util::FilePiece in(test_file);
-    StringPiece word;
-    auto time = util::CPUTime();
+struct Config {
+    int pipeline_size_start;
+    int pipeline_size_end;
+    char* model_file;
+    std::string type;
+    int fd_in;
+};
+
+template <typename Callback, typename Width>
+void PipelineScore(lm::Pipeline<Callback>& pipeline, const lm::ngram::ProbingModel& model, const Config& options){
+    const Width kEOS = model.GetVocabulary().EndSentence();
+    const lm::ngram::State begin_state = model.BeginSentenceState();
+    std::array<Width, 1024> buff;
+    util::SeekOrThrow(options.fd_in, 0);
 
     //start timer
+    auto time = util::CPUTime();
+    bool new_sentence = true;
     while (true) {
-        if (in.ReadWordSameLine(word)) {
-            lm::WordIndex vocab = model.GetVocabulary().Index(word);
-            pipeline.FullScore(model.BeginSentenceState(), vocab);
-        }
-
-        while(in.ReadWordSameLine(word)) {
-            lm::WordIndex vocab = model.GetVocabulary().Index(word);
-            pipeline.AppendWord(vocab);
-        }
-
-        try {
-            UTIL_THROW_IF('\n' != in.get(), util::Exception, "FilePiece is confused.");
-        } catch (const util::EndOfFileException &e) { break; }
-        
-        pipeline.AppendWord(model.GetVocabulary().EndSentence());
+      std::size_t got = util::ReadOrEOF(options.fd_in, buff.begin(), buff.size() * sizeof(Width));
+      if (!got) break;
+      UTIL_THROW_IF2(got % sizeof(Width), "File size not a multiple of vocab id size " << sizeof(Width));
+      auto end = buff.begin() + got / sizeof(Width);
+      auto curr = buff.begin();
+      while(curr != end) {
+          if (new_sentence) pipeline.FullScore(begin_state, *curr);
+          else pipeline.AppendWord(*curr);
+          new_sentence = *curr++ == kEOS;
+      }
     }
     pipeline.Drain();
     //stop timer
     time = util::CPUTime() - time;
-    std::cout << time << " ";
+    std::cout << time << '\n';
 }
 
-void ModelScore(const lm::ngram::ProbingModel& model, char * test_file){
-    util::FilePiece in(test_file);
-    StringPiece word;
-    lm::ngram::State in_state, out_state;
+template<typename Width>
+void ModelScore(const lm::ngram::ProbingModel& model, const Config& options){
+    const Width kEOS = model.GetVocabulary().EndSentence();
+    const lm::ngram::State* const begin_state = &model.BeginSentenceState();
+    const lm::ngram::State *in_state = begin_state;
+    std::array<Width, 1024> buff;
+    lm::ngram::State states[3];
     auto score = 0.0;
-    auto time = util::CPUTime();
 
     //start timer
+    auto time = util::CPUTime();
+    bool new_sentence = true;
     while (true) {
-        if (in.ReadWordSameLine(word)) {
-            lm::WordIndex vocab = model.GetVocabulary().Index(word);
-            auto ret = model.FullScore(model.BeginSentenceState(), vocab, out_state); 
-            score += ret.prob;
-            in_state = out_state;
-        }
-
-        while(in.ReadWordSameLine(word)) {
-            lm::WordIndex vocab = model.GetVocabulary().Index(word);
-            auto ret = model.FullScore(in_state, vocab, out_state);
-            score += ret.prob;
-            in_state = out_state;
-        }
-
-        try {
-            UTIL_THROW_IF('\n' != in.get(), util::Exception, "FilePiece is confused.");
-        } catch (const util::EndOfFileException &e) { break; }
-
-        auto ret = model.FullScore(in_state, model.GetVocabulary().EndSentence(), out_state);
-        score += ret.prob;
+      std::size_t got = util::ReadOrEOF(options.fd_in, buff.begin(), buff.size() * sizeof(Width));
+      if (!got) break;
+      UTIL_THROW_IF2(got % sizeof(Width), "File size not a multiple of vocab id size " << sizeof(Width));
+      auto even_end = buff.begin() + ((got / sizeof(Width)) & ~1);
+      auto curr = buff.begin();
+      while(curr != even_end) {
+        score += model.FullScore(*in_state, *curr, states[1]).prob;
+        in_state = (*curr++ == kEOS) ? begin_state : &states[1];
+        score += model.FullScore(*in_state, *curr, states[0]).prob;
+        in_state = (*curr++ == kEOS) ? begin_state : &states[0];
+      }
+      if (got & 1) {
+          score += model.FullScore(*in_state, *curr, states[2]).prob;
+          in_state = *curr == kEOS ? begin_state : &states[2];
+      }
     }
     //stop timer
     time = util::CPUTime() - time;
-    std::cerr << "Score (model): " << score << std::endl;
     std::cout << time << " ";
+    std::cerr << "Score(model) : " << score << std::endl;
 }
+
+template<typename Width>
+void DispatchFunction(lm::ngram::ProbingModel& model, const Config& options){
+    if (options.type == "probing") ModelScore<Width>(model, options);
+    else if (options.type == "pipeline") {
+        auto score = 0.0;
+        const auto callback = [&score](const lm::FullScoreReturn& r){score += r.prob;};
+        typename lm::ngram::NGramAutomaton<lm::ngram::BackoffValue, decltype(callback)>::Construct construct{model.GetSearch(), callback};
+        for (std::size_t pipeline_size = options.pipeline_size_start; pipeline_size <= options.pipeline_size_end; ++pipeline_size) {
+            score = 0.0;
+            lm::Pipeline<decltype(callback)> pipeline(pipeline_size, construct);
+            PipelineScore<decltype(callback), Width>(pipeline, model, options);
+            std::cerr << "Score(pipeline): " << score << std::endl;
+        }
+    }
+    std::cout << '\n';
+}
+
+void DispatchWidth(lm::ngram::ProbingModel& model, const Config& options) {
+  uint64_t bound = model.GetVocabulary().Bound();
+  if (bound <= 256) {
+    DispatchFunction<uint8_t>(model, options);
+  } else if (bound <= 65536) {
+    DispatchFunction<uint16_t>(model, options);
+  } else if (bound <= (1ULL << 32)) {
+    DispatchFunction<uint32_t>(model, options);
+  } else {
+    DispatchFunction<uint64_t>(model, options);
+  }
+}
+
 
 int main(int argc, char* argv[]){
     if (argc < 6) {
-        std::cerr << argv[0] <<" pipeline_size_start pipeline_size_end model_file test_file <probing|pipeline>" << std::endl;
+        std::cerr << argv[0] <<" pipeline_size_start pipeline_size_end model_file query_file {probing|pipeline}" << std::endl;
         return 1;
     }
     int pipeline_size_start = std::stoi(std::string(argv[1]));
     int pipeline_size_end = std::stoi(std::string(argv[2]));
-    char* arpa_file(argv[3]);
-    char* test_file(argv[4]);
+    char* model_file(argv[3]);
+    util::scoped_fd in_fd(util::OpenReadOrThrow(argv[4]));
     std::string type(argv[5]);
+    Config options{pipeline_size_start, pipeline_size_end, model_file, type, in_fd.get()};
 
     lm::ngram::Config config;
     config.arpa_complain = lm::ngram::Config::ALL;
     config.messages = &std::cout;
     config.positive_log_probability = lm::SILENT;
     config.probing_multiplier = 1.5;
-    lm::ngram::ProbingModel model(arpa_file, config);
+    lm::ngram::ProbingModel model(options.model_file, config);
 
-    if (type == "probing") ModelScore(model, test_file);
-    else if (type == "pipeline") {
-        auto score = 0.0;
-        const auto callback = [&score](const lm::FullScoreReturn& r){score += r.prob;};
-        typename lm::ngram::NGramAutomaton<lm::ngram::BackoffValue, decltype(callback)>::Construct construct{model.GetSearch(), callback};
-        for (std::size_t pipeline_size = pipeline_size_start; pipeline_size <= pipeline_size_end; ++pipeline_size) {
-            score = 0.0;
-            lm::Pipeline<decltype(callback)> pipeline(pipeline_size, construct);
-            PipelineScore(pipeline, model, test_file);
-            std::cerr << "Score (pipeline): " << score << std::endl;
-        }
-    }
-    std::cout << std::endl;
-
-    
+    DispatchWidth(model, options);
 }
