dffe9c2cca142c701029ccbc3130920004fd4cd7
Hieu Hoang <hieuhoang@gmail.com>
2016-08-13 15:16:09 +0100

    delete AllocateWithAlign(). Always align

--- contrib/moses2/MemPool.h
+++ contrib/moses2/MemPool.h
@@ -70,40 +70,6 @@ public:
     return (T*) ret;
   }
 
-  // when allocating small objects (bool. char etc). There is a segfault when mem isn't aligned to 64-bit mem
-  uint8_t *AllocateWithAlign(std::size_t size)
-  {
-    size_t remainder = size % 4;
-    size = remainder ? size + 4 - remainder : size;
-
-    uint8_t *ret = current_;
-    current_ += size;
-
-    Page &page = *m_pages[m_currPage];
-    if (current_ <= page.end) {
-      // return what we got
-    }
-    else {
-      ret = More(size);
-    }
-    return ret;
-
-  }
-
-  template<typename T>
-  T *AllocateWithAlign()
-  {
-    uint8_t *ret = AllocateWithAlign(sizeof(T));
-    return (T*) ret;
-  }
-
-  template<typename T>
-  T *AllocateWithAlign(size_t num)
-  {
-    uint8_t *ret = AllocateWithAlign(sizeof(T) * num);
-    return (T*) ret;
-  }
-
   // re-use pool
   void Reset();
 
