8e619c67fa13de2032d60800905699dc3ad21a01
Hieu Hoang <hieuhoang@gmail.com>
2016-09-29 10:38:01 +0100

    begin import of Compact pt

--- contrib/moses2/FF/FeatureRegistry.cpp
+++ contrib/moses2/FF/FeatureRegistry.cpp
@@ -1,6 +1,7 @@
 #include "FeatureRegistry.h"
 
 #include "../TranslationModel/Memory/PhraseTableMemory.h"
+#include "../TranslationModel/PhraseTableCompact.h"
 #include "../TranslationModel/ProbingPT.h"
 #include "../TranslationModel/UnknownWordPenalty.h"
 #include "../TranslationModel/Transliteration.h"
@@ -51,6 +52,7 @@ FeatureRegistry::FeatureRegistry()
   // Feature with different name than class.
 #define MOSES_FNAME2(name, type) Add(name, new DefaultFeatureFactory< type >());
 
+  MOSES_FNAME2("PhraseDictionaryCompact", PhraseTableCompact);
   MOSES_FNAME2("PhraseDictionaryMemory", PhraseTableMemory);
   MOSES_FNAME(ProbingPT);
   MOSES_FNAME2("PhraseDictionaryTransliteration", Transliteration);
---
+++ contrib/moses2/TranslationModel/PhraseTableCompact.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "PhraseTable.h"
+
+namespace Moses2
+{
+class PhraseTableCompact: public PhraseTable
+{
+public:
+  PhraseTableCompact(size_t startInd, const std::string &line);
+  virtual ~PhraseTableCompact();
+  void Load(System &system);
+  virtual void SetParameter(const std::string& key, const std::string& value);
+
+
+  // scfg
+  virtual void InitActiveChart(
+      MemPool &pool,
+      const SCFG::Manager &mgr,
+      SCFG::InputPath &path) const;
+
+  virtual void Lookup(
+      MemPool &pool,
+      const SCFG::Manager &mgr,
+      size_t maxChartSpan,
+      const SCFG::Stacks &stacks,
+      SCFG::InputPath &path) const;
+
+protected:
+  virtual void LookupGivenNode(
+      MemPool &pool,
+      const SCFG::Manager &mgr,
+      const SCFG::ActiveChartEntry &prevEntry,
+      const SCFG::Word &wordSought,
+      const Moses2::Hypotheses *hypos,
+      const Moses2::Range &subPhraseRange,
+      SCFG::InputPath &outPath) const;
+
+};
+
+}
