f7cf9a84edc8a3a17e1eabe32f0bc8f3097ba97c
Hieu Hoang <hieuhoang@gmail.com>
2016-12-05 17:29:52 +0000

    use GetBestHypo()

--- contrib/moses2/PhraseBased/Normal/Search.cpp
+++ contrib/moses2/PhraseBased/Normal/Search.cpp
@@ -140,13 +140,7 @@ void Search::Extend(const Hypothesis &hypo, const TargetPhraseImpl &tp,
 const Hypothesis *Search::GetBestHypo() const
 {
 	const Stack &lastStack = m_stacks.Back();
-	const Hypotheses &sortedHypos = lastStack.GetSortedAndPruneHypos(mgr,
-			mgr.arcLists);
-
-	const Hypothesis *best = NULL;
-	if (sortedHypos.size()) {
-		best = static_cast<const Hypothesis*>(sortedHypos[0]);
-	}
+	const Hypothesis *best = lastStack.GetBestHypo<Hypothesis>();
 	return best;
 }
 
